87cb2ebf1c208f3221856312bc97a8f4940e5143
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2021-07-12 10:52:57 +0200

    chore: format

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,21 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
 const config = {
-  mode: "jit",
-  purge: ["./src/**/*.{html,js,svelte,ts}"],
-  theme: {
-    extend: {},
-    fontFamily: {
-      sans: ["Marianne"],
-    },
-    colors: {
-      white: colors.white,
-      orange: colors.orange,
-      "blue-dora": "#2B5787",
-      gray: colors.gray,
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
+	mode: 'jit',
+	purge: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {},
+		fontFamily: {
+			sans: ['Marianne']
+		},
+		colors: {
+			white: colors.white,
+			orange: colors.orange,
+			'blue-dora': '#2B5787',
+			gray: colors.gray
+		}
+	},
+	plugins: [require('@tailwindcss/typography')]
 };
 
 module.exports = config;
