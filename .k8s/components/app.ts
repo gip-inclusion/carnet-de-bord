32ec6c2e2d33c64d026eb6d01bbd2d6cfe3ea65f
Lionel <lionel.breduillieard@beta.gouv.fr>
2021-07-28 16:07:21 +0200

    feat: add review workflow
    * parent 38b6dcab23c73d38c43453c5edeaf4c6bbd41374
    author gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr> 1626183953 +0200
    committer gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr> 1627423107 +0200
    
    parent 38b6dcab23c73d38c43453c5edeaf4c6bbd41374
    author gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr> 1626183953 +0200
    committer gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr> 1627422760 +0200
    
    fix: Add review workflow.
    
    Add k8s files.
    
    Generate manifests.
    
    Deploy.
    
    Upgrade kosko-charts.
    
    Add pg admin secret.
    
    Add yaml component.
    
    Fix typo.
    
    Create healthz route.
    
    Use /login as probes.
    
    Change port.
    
    Allow /healthz route.
    
    Add Hasura component.
    
    fix: Add review workflow.
    
    Add k8s files.
    
    Generate manifests.
    
    Deploy.
    
    Create healthz route.
    
    Use /login as probes.
    
    Add Hasura registration.
    
    Change Hasura register job name.
    
    Set proper Hasura image address.
    
    Use port 80 for Hasura.
    
    Build Hasura image with proper context.
    
    Change BASE_DOMAIN.
    
    Change Hasura image cache.
    
    Change Hasura image cache, again.
    
    Remove Hasura image cache.
    
    Use proper Hasura image build context.
    
    Add release workflow.
    
    Use GITHUB_TOKEN for image registration.
    
    Add Rancher ID.
    
    Fix conflicts in review workflow.
    
    Remove pg-admin sealed secret.
    
    Add k8s files.
    
    Upgrade kosko-charts.
    
    Add pg admin secret.
    
    Add yaml component.
    
    Fix typo.
    
    Create healthz route.
    
    Use /login as probes.
    
    Change port.
    
    Allow /healthz route.
    
    fix: Add review workflow.
    
    Add k8s files.
    
    Generate manifests.
    
    Deploy.
    
    Create healthz route.
    
    Use /login as probes.
    
    Add Hasura registration.
    
    Change Hasura register job name.
    
    Set proper Hasura image address.
    
    Use port 80 for Hasura.
    
    Build Hasura image with proper context.
    
    Change BASE_DOMAIN.
    
    Change Hasura image cache, again.
    
    Remove Hasura image cache.
    
    Use proper Hasura image build context.
    
    Add release workflow.
    
    Use GITHUB_TOKEN for image registration.
    
    Add Rancher ID.
    
    Fix conflicts in review workflow.
    
    Remove pg-admin sealed secret.
    
    Fix app configmap.
    
    Fix Hasura configmap.
    
    Fix Hasura configmap.
    
    Use HASURA_GRAPHQL_DATABASE_URL when VITE_DATABASE_URL not available.
    
    Use key within HASURA_GRAPHQL_JWT_SECRET.
    
    Make package.json as private.
    
    * fix: Rename hasura configmap.
    
    * Rename hasura configmap.
    
    * chore(release): 1.0.0-alpha.1
    
    * chore(release): 1.0.0-beta.1
    
    * Add secrets for Hasura and app.
    
    * feat(hasura): update schema and metadata
    
    * chore(release): 1.0.0-beta.2
    
    * Fix sealed secrets.
    
    * Fix sealed secrets, again.
    
    * pass url to frontend
    
    * fix api endpoint url
    
    * test(k8s): update snap
    
    * update seed
    
    Co-authored-by: gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>
    Co-authored-by: Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>

---
+++ .k8s/components/app.ts
@@ -0,0 +1,183 @@
+import env from '@kosko/env';
+import { create } from '@socialgouv/kosko-charts/components/app';
+import { addEnv } from '@socialgouv/kosko-charts/utils/addEnv';
+import { getIngressHost } from '@socialgouv/kosko-charts/utils/getIngressHost';
+import { getManifestByKind } from '@socialgouv/kosko-charts/utils/getManifestByKind';
+import { ok } from 'assert';
+import { Deployment } from 'kubernetes-models/apps/v1/Deployment';
+import { EnvVar } from 'kubernetes-models/v1/EnvVar';
+import { getManifests as getHasuraManifests } from './hasura';
+import environments from '@socialgouv/kosko-charts/environments';
+// import { azureProjectVolume } from "@socialgouv/kosko-charts/components/azure-storage/azureProjectVolume";
+// import { VolumeMount, Volume } from "kubernetes-models/v1";
+
+// type AnyObject = {
+//   [any: string]: any;
+// };
+
+// interface AddEnvsParams {
+//   deployment: Deployment;
+//   data: AnyObject;
+//   containerIndex?: number;
+// }
+
+// const addEnvs = ({ deployment, data, containerIndex = 0 }: AddEnvsParams) => {
+//   Object.keys(data).forEach((key) => {
+//     addEnv({
+//       deployment,
+//       data: new EnvVar({ name: key, value: data[key] }),
+//       containerIndex,
+//     });
+//   });
+// };
+
+export const getManifests = async () => {
+	const name = 'app';
+	const probesPath = '/healthz';
+	const subdomain = 'carnet-de-bord';
+
+	const ciEnv = environments(process.env);
+
+	const isDev = !(ciEnv.isPreProduction || ciEnv.isProduction);
+
+	// const tag = process.env.CI_COMMIT_TAG
+	//   ? process.env.CI_COMMIT_TAG.slice(1)
+	//   : process.env.CI_COMMIT_SHA
+	//   ? process.env.CI_COMMIT_SHA
+	//   : process.env.GITHUB_SHA;
+
+	const podProbes = ['livenessProbe', 'readinessProbe', 'startupProbe'].reduce(
+		(probes, probe) => ({
+			...probes,
+			[probe]: {
+				httpGet: {
+					path: probesPath,
+					port: 3000
+				},
+				initialDelaySeconds: 30,
+				periodSeconds: 15
+			}
+		}),
+		{}
+	);
+
+	// const [persistentVolumeClaim, persistentVolume] = azureProjectVolume("files", {
+	//   storage: "5Gi",
+	// });
+
+	// const uploadsVolume = new Volume({
+	//   name: "files",
+	//   persistentVolumeClaim: { claimName: persistentVolumeClaim.metadata!.name! },
+	// });
+
+	// const uploadsVolumeMount = new VolumeMount({
+	//   mountPath: "/mnt/files",
+	//   name: "files",
+	// });
+
+	// const emptyDir = new Volume({ name: "files", emptyDir: {} });
+
+	const manifests = await create(name, {
+		env,
+		config: {
+			subdomain,
+			ingress: true,
+			withPostgres: true,
+			containerPort: 3000
+			// subDomainPrefix: ciEnv.isProduction ? `fake-` : `${subdomain}-`,
+		},
+		deployment: {
+			image: `ghcr.io/socialgouv/carnet-de-bord:sha-${ciEnv.sha}`,
+			// volumes: [isDev ? emptyDir : uploadsVolume],
+			container: {
+				// volumeMounts: [uploadsVolumeMount],
+				// volumeMounts: [
+				//   {
+				//     mountPath: "/mnt/files",
+				//     name: "domifa-volume",
+				//   },
+				// ],
+				resources: {
+					requests: {
+						cpu: '50m',
+						memory: '128Mi'
+					},
+					limits: {
+						cpu: '200m',
+						memory: '256Mi'
+					}
+				},
+				...podProbes
+			}
+		}
+	});
+
+	return manifests;
+	// return manifests.concat(
+	//   isDev
+	//     ? []
+	//     : [persistentVolumeClaim, persistentVolume]
+	// );
+};
+
+export default async () => {
+	// const { env } = process;
+	// const ciEnv = environments(env);
+	// const { CI_ENVIRONMENT_NAME, PRODUCTION } = env;
+	// const isProductionCluster = Boolean(PRODUCTION);
+	// const isPreProduction = CI_ENVIRONMENT_NAME === "preprod-dev";
+	// const isDev = !isProductionCluster && !isPreProduction;
+
+	const manifests = await getManifests();
+
+	/* pass dynamic deployment URL as env var to the container */
+	//@ts-expect-error
+	const deployment = getManifestByKind(manifests, Deployment) as Deployment;
+
+	ok(deployment);
+
+	const hasuraManifests = await getHasuraManifests();
+
+	const hasuraUrl = new EnvVar({
+		name: 'VITE_GRAPHQL_API_URL',
+		value: `https://${getIngressHost(hasuraManifests)}/v1/graphql`
+	});
+
+	addEnv({ deployment, data: hasuraUrl });
+	// addEnvs({ deployment,
+	//     POSTGRES_HOST: "$(PGHOST)",
+	//     POSTGRES_USERNAME: "$(PGUSER)",
+	//     POSTGRES_PASSWORD: "$(PGPASSWORD)",
+	//     POSTGRES_DATABASE: "$(PGDATABASE)",
+	//     DOMIFA_BACKEND_URL: `https://${getIngressHost(manifests)}`,
+	//     DOMIFA_FRONTEND_URL: `https://${getIngressHost(frontendManifests)}`,
+	//   },
+	// });
+
+	// const volumes = [
+	//   ciEnv.isPreProduction || ciEnv.isProduction
+	//     ? {
+	//         name: "domifa-volume",
+	//         azureFile: {
+	//           readOnly: false,
+	//           shareName: "files",
+	//           secretName: "azure-storage",
+	//           secretNamespace: "domifa-preprod",
+	//         },
+	//       }
+	//     : {
+	//       name: "domifa-volume",
+	//       emptyDir: {},
+	//     }
+	// ];
+
+	// assert.object(deployment.spec);
+	// assert.object(deployment.spec.template.spec);
+
+	// deployment.spec.template.spec.volumes = [
+	//   ...(deployment.spec.template.spec.volumes || []),
+	//   ...volumes,
+	// ];
+
+	return manifests;
+};
---
+++ .k8s/components/hasura.ts
@@ -0,0 +1,54 @@
+import env from '@kosko/env';
+import { create } from '@socialgouv/kosko-charts/components/hasura';
+// import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath";
+import environments from '@socialgouv/kosko-charts/environments';
+// import type { Manifests } from "../types/config";
+// import type { IIoK8sApimachineryPkgApisMetaV1ObjectMeta } from "kubernetes-models/_definitions/IoK8sApimachineryPkgApisMetaV1ObjectMeta";
+
+declare type Manifests = Promise<{ kind: string }[] | []>;
+
+// declare type Manifest =
+//   | {
+//       metadata?: IIoK8sApimachineryPkgApisMetaV1ObjectMeta | undefined;
+//       spec?: unknown;
+//     }
+//   | undefined;
+
+// import Config from "../utils/config";
+
+export default async (): Manifests => {
+	// const { name, hasura } = await Config();
+	const hasura = 'exposed';
+
+	const ciEnv = environments(process.env);
+
+	const config = {
+		config: { ingress: hasura === 'exposed' },
+		deployment: {
+			image: `ghcr.io/socialgouv/carnet-de-bord/hasura:sha-${ciEnv.sha}`
+		},
+		env
+	};
+
+	if (hasura) {
+		const manifests = await getManifests();
+		return manifests;
+	} else {
+		return Promise.resolve([]);
+	}
+};
+
+export async function getManifests() {
+	const hasura = 'exposed';
+
+	const ciEnv = environments(process.env);
+
+	const config = {
+		config: { ingress: hasura === 'exposed' },
+		deployment: {
+			image: `ghcr.io/socialgouv/carnet-de-bord/hasura:sha-${ciEnv.sha}`
+		},
+		env
+	};
+	return await create('hasura', config);
+}
