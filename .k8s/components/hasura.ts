58fd29351237a2fc727a05c22849aeba879a28d5
LionelB <lionel@lumographe.fr>
2021-07-28 14:46:34 +0200

    pass url to frontend

--- .k8s/components/hasura.ts
+++ .k8s/components/hasura.ts
@@ -1,7 +1,7 @@
-import env from "@kosko/env";
-import { create } from "@socialgouv/kosko-charts/components/hasura";
+import env from '@kosko/env';
+import { create } from '@socialgouv/kosko-charts/components/hasura';
 // import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath";
-import environments from "@socialgouv/kosko-charts/environments";
+import environments from '@socialgouv/kosko-charts/environments';
 // import type { Manifests } from "../types/config";
 // import type { IIoK8sApimachineryPkgApisMetaV1ObjectMeta } from "kubernetes-models/_definitions/IoK8sApimachineryPkgApisMetaV1ObjectMeta";
 
@@ -17,23 +17,38 @@ declare type Manifests = Promise<{ kind: string }[] | []>;
 // import Config from "../utils/config";
 
 export default async (): Manifests => {
-  // const { name, hasura } = await Config();
-  const hasura = "exposed";
+	// const { name, hasura } = await Config();
+	const hasura = 'exposed';
 
-  const ciEnv = environments(process.env);
+	const ciEnv = environments(process.env);
 
-  const config = {
-    config: { ingress: hasura === "exposed" },
-    deployment: {
-      image: `ghcr.io/socialgouv/carnet-de-bord/hasura:sha-${ciEnv.sha}`,
-    },
-    env,
-  };
+	const config = {
+		config: { ingress: hasura === 'exposed' },
+		deployment: {
+			image: `ghcr.io/socialgouv/carnet-de-bord/hasura:sha-${ciEnv.sha}`
+		},
+		env
+	};
 
-  if (hasura) {
-    const manifests = await create("hasura", config);
-    return manifests;
-  } else {
-    return Promise.resolve([]);
-  }
+	if (hasura) {
+		const manifests = await getManifests();
+		return manifests;
+	} else {
+		return Promise.resolve([]);
+	}
 };
+
+export async function getManifests() {
+	const hasura = 'exposed';
+
+	const ciEnv = environments(process.env);
+
+	const config = {
+		config: { ingress: hasura === 'exposed' },
+		deployment: {
+			image: `ghcr.io/socialgouv/carnet-de-bord/hasura:sha-${ciEnv.sha}`
+		},
+		env
+	};
+	return await create('hasura', config);
+}
