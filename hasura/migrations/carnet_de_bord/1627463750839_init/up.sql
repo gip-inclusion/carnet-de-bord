62ccc488a8db63664020964c8315c5ee8dff7bdd
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2021-07-28 11:48:05 +0200

    feat(hasura): update schema and metadata

--- hasura/migrations/carnet_de_bord/1627463750839_init/up.sql
+++ hasura/migrations/carnet_de_bord/1627463750839_init/up.sql
@@ -28,6 +28,26 @@ CREATE TABLE public.beneficiary (
     mobile_number character varying(255),
     date_of_birth date NOT NULL
 );
+CREATE TABLE public.notebook (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    beneficiary_id uuid NOT NULL,
+    creation_date timestamp with time zone DEFAULT now() NOT NULL
+);
+CREATE TABLE public.notebook_event (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    notebook_id uuid NOT NULL,
+    creation_date timestamp with time zone DEFAULT now() NOT NULL,
+    event_date date NOT NULL,
+    data jsonb NOT NULL,
+    professional_id uuid NOT NULL
+);
+CREATE TABLE public.notebook_member (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    notebook_id uuid NOT NULL,
+    professional_id uuid NOT NULL,
+    last_visit_date timestamp with time zone NOT NULL,
+    member_type character varying NOT NULL
+);
 CREATE TABLE public.professional (
     id uuid DEFAULT public.gen_random_uuid() NOT NULL,
     structure_id uuid NOT NULL,
@@ -49,14 +69,6 @@ CREATE TABLE public.structure (
     creation_date timestamp with time zone,
     modification_date timestamp with time zone
 );
-CREATE TABLE public.team_member (
-    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
-    beneficiary_id uuid NOT NULL,
-    professional_id uuid DEFAULT public.gen_random_uuid() NOT NULL,
-    member_type character varying NOT NULL,
-    last_visit_date timestamp with time zone,
-    modification_date timestamp with time zone
-);
 ALTER TABLE ONLY public.account
     ADD CONSTRAINT account_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.account
@@ -69,23 +81,35 @@ ALTER TABLE ONLY public.beneficiary
     ADD CONSTRAINT beneficiary_email_unique UNIQUE (email);
 ALTER TABLE ONLY public.beneficiary
     ADD CONSTRAINT beneficiary_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.notebook
+    ADD CONSTRAINT notebook_beneficiary_id_key UNIQUE (beneficiary_id);
+ALTER TABLE ONLY public.notebook_event
+    ADD CONSTRAINT notebook_event_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.notebook_member
+    ADD CONSTRAINT notebook_member_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.notebook
+    ADD CONSTRAINT notebook_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.professional
     ADD CONSTRAINT professional_email_unique UNIQUE (email);
 ALTER TABLE ONLY public.professional
     ADD CONSTRAINT professional_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.structure
     ADD CONSTRAINT structure_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY public.team_member
-    ADD CONSTRAINT team_member_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.account
     ADD CONSTRAINT account_admin_id_foreign FOREIGN KEY (admin_id) REFERENCES public.admin(id);
 ALTER TABLE ONLY public.account
     ADD CONSTRAINT account_beneficiary_id_foreign FOREIGN KEY (beneficiary_id) REFERENCES public.beneficiary(id);
 ALTER TABLE ONLY public.account
     ADD CONSTRAINT account_professional_id_foreign FOREIGN KEY (professional_id) REFERENCES public.professional(id);
+ALTER TABLE ONLY public.notebook
+    ADD CONSTRAINT notebook_beneficiary_id_fkey FOREIGN KEY (beneficiary_id) REFERENCES public.beneficiary(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE ONLY public.notebook_event
+    ADD CONSTRAINT notebook_event_notebook_id_fkey FOREIGN KEY (notebook_id) REFERENCES public.notebook(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+ALTER TABLE ONLY public.notebook_event
+    ADD CONSTRAINT notebook_event_professional_id_fkey FOREIGN KEY (professional_id) REFERENCES public.professional(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE ONLY public.notebook_member
+    ADD CONSTRAINT notebook_member_notebook_id_fkey FOREIGN KEY (notebook_id) REFERENCES public.notebook(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+ALTER TABLE ONLY public.notebook_member
+    ADD CONSTRAINT notebook_member_professional_id_fkey FOREIGN KEY (professional_id) REFERENCES public.professional(id) ON UPDATE RESTRICT ON DELETE CASCADE;
 ALTER TABLE ONLY public.professional
     ADD CONSTRAINT professional_structure_id_foreign FOREIGN KEY (structure_id) REFERENCES public.structure(id);
-ALTER TABLE ONLY public.team_member
-    ADD CONSTRAINT team_member_beneficiary_id_fkey FOREIGN KEY (beneficiary_id) REFERENCES public.beneficiary(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
-ALTER TABLE ONLY public.team_member
-    ADD CONSTRAINT team_member_professional_id_fkey FOREIGN KEY (professional_id) REFERENCES public.professional(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
