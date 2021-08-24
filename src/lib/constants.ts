b889374093a190fec9d6d92041ab726955b8c29a
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2021-08-24 11:12:03 +0200

    chore: change support email

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const orgName = "Ministère<br />du Travail,<br />de l'Emploi<br />et de l'Insertion";
 
-export const contactEmail = 'contact@cdb.fr';
+export const contactEmail = 'support.cdb@fabrique.social.gouv.fr';
