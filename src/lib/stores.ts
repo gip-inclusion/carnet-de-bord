3b0a94237613e84503e13e1963853c32438280fa
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2021-09-07 09:05:02 +0200

    refactor: use svelte component to open modal

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,7 +1,9 @@
 import type { Account } from '$lib/types';
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
+import type { SvelteComponent } from 'svelte';
 
 export const account: Writable<null | Account> = writable(null);
 
 export const offCanvas: Writable<boolean> = writable(false);
+export const openComponent: Writable<typeof SvelteComponent> = writable(null);
