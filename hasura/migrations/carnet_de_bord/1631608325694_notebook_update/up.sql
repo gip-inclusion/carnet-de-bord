65cae8d5e7b7347c125b664bf0ddfaeea74d17ce
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2021-09-14 10:34:12 +0200

    feat(notebook): update schema

---
+++ hasura/migrations/carnet_de_bord/1631608325694_notebook_update/up.sql
@@ -0,0 +1,41 @@
+
+
+alter table "public"."notebook_target" add column "linked_to" varchar
+ null;
+
+alter table "public"."notebook" drop column "rights" cascade;
+
+alter table "public"."notebook" add column "right_rsa" varchar
+ null;
+
+alter table "public"."notebook" add column "right_rqth" boolean
+ not null default 'false';
+
+alter table "public"."notebook" add column "right_are" boolean
+ not null default 'false';
+
+alter table "public"."notebook" add column "right_ass" boolean
+ null default 'false';
+
+alter table "public"."notebook" add column "right_bonus" boolean
+ not null default 'false';
+
+alter table "public"."notebook" add column "geographical_area" varchar
+ null;
+
+alter table "public"."notebook" add column "education_level" varchar
+ null;
+
+alter table "public"."notebook" add column "job" varchar
+ null;
+
+alter table "public"."notebook" add column "work_situation_date" date
+ null;
+
+alter table "public"."notebook" drop column "cer_objects" cascade;
+
+alter table "public"."notebook" add column "contract_type" varchar
+ null;
+
+alter table "public"."notebook" add column "contract_sign_date" date
+ null;
--- src/lib/constants/keys.ts
+++ src/lib/constants/keys.ts
@@ -1,5 +1,61 @@
 import { buildKeys } from '$lib/utils/keysBuilder';
 
+// NOTEBOOK KEYS
+/* ----------------------------------------------- */
+
+export const workSituationKeys = buildKeys({
+	recherche_emploi: "En recherche d'emploi",
+	recherche_formation: 'En recherche de formation',
+	recherche_alternance: 'En recherche d’alternance',
+	recherche_stage: 'En recherche de stage',
+	emploi: 'En emploi',
+	formation: 'En formation',
+	stage: 'En stage',
+	alternance: 'En alternance',
+	service_civique: 'En service civique',
+	cdd: 'En CDD',
+	cdi: 'En CDI',
+	iae: 'En IAE',
+	interim: 'En interim',
+	construction_projet: 'En construction de projet',
+	projet_entrepreneurial: 'En projet entrepreneurial',
+	entrepreneur: 'Entrepreneur'
+});
+
+export const rsaRightKeys = buildKeys({
+	rsa_droit_ouvert_et_suspendu: 'RSA_ Droit ouvert et suspendu',
+	rsa_droit_ouvert_versable: 'RSA_Droit ouvert et versable',
+	rsa_droit_ouvert_versement_suspendu: 'RSA_Droit ouvert mais versement suspendu'
+});
+
+export const geographicalAreaKeys = buildKeys({
+	less_10: 'Mois de 10 km',
+	between_10_20: 'Entre  10 et 20 km',
+	between_20_30: 'Entre  20 et 30 km',
+	plus_30: 'Plus de 30 km'
+});
+
+export const contractTypeKeys = buildKeys({
+	cer: 'CER',
+	ppae: 'PPAE',
+	pacea: 'PACEA'
+});
+
+export const educationLevelKeys = buildKeys({
+	level_1: 'Niveau I : enseignement supérieur',
+	level_2: 'Niveau II : BAC +3 / +4',
+	level_3: 'Niveau III : BAC +2',
+	level_4: 'Niveau IV : BAC ou équivalent',
+	level_5: 'Niveau V : CAP/BEP',
+	level_6: 'Formation courte max 1 mois',
+	level_7: 'Pas de formation / Pas de diplôme'
+});
+
+/* ----------------------------------------------- */
+/* ----------------------------------------------- */
+
+// FOCUS KEYS
+
 export const focusThemeKeys = buildKeys({
 	logement: 'Logement',
 	sante: 'Santé',
@@ -26,32 +82,4 @@ export const cerObjectKeys = buildKeys({
 	logement: 'Logement',
 	autonomie_sociale: 'Autonomie sociale',
 	sante: 'Santé'
-});
-
-export const rightKeys = buildKeys({
-	rqth: 'RQTH',
-	are: 'ARE',
-	ass: 'ASS',
-	rsa_droit_ouvert_et_suspendu: 'RSA_ Droit ouvert et suspendu',
-	rsa_droit_ouvert_versable: 'RSA_Droit ouvert et versable',
-	rsa_droit_ouvert_versement_suspendu: 'RSA_Droit ouvert mais versement suspendu'
-});
-
-export const workSituationKeys = buildKeys({
-	recherche_emploi: "En recherche d'emploi",
-	recherche_formation: 'En recherche de formation',
-	recherche_alternance: 'En recherche d’alternance',
-	recherche_stage: 'En recherche de stage',
-	emploi: 'En emploi',
-	formation: 'En formation',
-	stage: 'En stage',
-	alternance: 'En alternance',
-	service_civique: 'En service civique',
-	cdd: 'En CDD',
-	cdi: 'En CDI',
-	iae: 'En IAE',
-	interim: 'En interim',
-	construction_projet: 'En construction de projet',
-	projet_entrepreneurial: 'En projet entrepreneurial',
-	entrepreneur: 'Entrepreneur'
 });
