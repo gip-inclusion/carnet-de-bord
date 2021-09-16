6348803156a2c66a8fc74fb3b84b400b401cf859
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2021-09-16 15:03:51 +0200

    chore: fix focuses layout

--- src/lib/ui/ProNotebookFocus/ProNotebookFocusView.svelte
+++ src/lib/ui/ProNotebookFocus/ProNotebookFocusView.svelte
@@ -14,16 +14,18 @@
 	};
 </script>
 
-<div class="flex flex-row flex-wrap gap-4">
-	{#each focuses as focus (focus.id)}
-		<div class="w-5/12 box-border">
-			<Card hideArrow={false}>
-				<span slot="title">{focusThemeKeys.byKey[focus.theme]}</span>
-				<span slot="description">
-					<Text value={contractTypeKeys.byKey[focus.linkedTo]} />
-				</span>
-			</Card>
-		</div>
-	{/each}
-	<div><Button on:click={addFocus}>Ajouter un axe de travail</Button></div>
+<div>
+	<div class="flex flex-row flex-wrap">
+		{#each focuses as focus, i}
+			<div class={`w-1/2 py-1 box-border ${i % 2 ? 'pl-1' : 'pr-1'}`}>
+				<Card hideArrow={false}>
+					<span slot="title">{focusThemeKeys.byKey[focus.theme]}</span>
+					<span slot="description">
+						<Text value={contractTypeKeys.byKey[focus.linkedTo]} />
+					</span>
+				</Card>
+			</div>
+		{/each}
+	</div>
+	<div class="py-4"><Button on:click={addFocus}>Ajouter un axe de travail</Button></div>
 </div>
