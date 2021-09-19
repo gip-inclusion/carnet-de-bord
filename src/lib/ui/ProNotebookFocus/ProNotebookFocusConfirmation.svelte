ea505e6f1cba3e286fcc6f23106668079ace143a
Augustin82 <augustin.ragon@gmail.com>
2021-09-19 20:49:54 +0200

    chore: fix typo

--- src/lib/ui/ProNotebookFocus/ProNotebookFocusConfirmation.svelte
+++ src/lib/ui/ProNotebookFocus/ProNotebookFocusConfirmation.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="flex flex-col gap-6">
-	<h1>Nouvelle axe de travail créé !</h1>
+	<h1>Nouvel axe de travail créé !</h1>
 	<p>Le nouvel axe de travail a été créé avec succès.</p>
 	<div>
 		<Button on:click={openComponent.close}>J'ai compris</Button>
