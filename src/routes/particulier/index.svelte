8235d865cd3195983045250d4dceec72721c248c
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2021-09-20 12:04:14 +0200

    chore(part): remove obsolete sections

--- src/routes/particulier/index.svelte
+++ src/routes/particulier/index.svelte
@@ -4,8 +4,7 @@
 		GetNotebookByBeneficiaryIdDocument,
 		GetNotebookByBeneficiaryIdQueryStore,
 	} from '$lib/graphql/_gen/typed-document-nodes';
-	import type { Option } from '$lib/types';
-	import { Accordion, Accordions, Button, Select } from '$lib/ui/base';
+	import { Accordion, Accordions } from '$lib/ui/base';
 	import { PartNotebookPersonalInfoView } from '$lib/ui/PartNotebookPersonalInfo';
 	import LoaderIndicator from '$lib/ui/utils/LoaderIndicator.svelte';
 	import Text from '$lib/ui/utils/Text.svelte';
@@ -32,14 +31,10 @@
 	$: notebook = $getNotebookResult.data?.notebook[0];
 	$: beneficiary = notebook?.beneficiary as Beneficiary;
 	$: members = notebook?.members;
-	let selectedPeriod: Option | null;
-	let periodOptions = [];
-	let selectedOrder: Option | null;
-	let orderOptions = [];
 </script>
 
 <LoaderIndicator result={getNotebookResult}>
-	<div class="flex flex-col space-y-8 px-40">
+	<div class="flex flex-col px-40 space-y-8">
 		<PartNotebookPersonalInfoView
 			{beneficiary}
 			on:edit={() => alert('Not implemented!')}
@@ -53,7 +48,7 @@
 				{#each members as member, i}
 					<div
 						class:bg-gray-100={i % 2 === 0}
-						class="flex hover:ml-2 cursor-pointer gap-2 p-2 mb-2 w-full border-l-2 border-france-blue"
+						class="flex w-full gap-2 p-2 mb-2 border-l-2 cursor-pointer hover:ml-2 border-france-blue"
 					>
 						<div class="flex flex-col w-1/2 min-w-0">
 							<div class="text-gray-text-alt">Structure</div>
@@ -65,7 +60,7 @@
 						<div class="flex flex-col w-1/4 min-w-0">
 							<div class="text-gray-text-alt">Accompagnateur</div>
 							<div
-								class="flex flex-row gap-2 font-bold overflow-ellipsis overflow-hidden whitespace-nowrap"
+								class="flex flex-row gap-2 overflow-hidden font-bold overflow-ellipsis whitespace-nowrap"
 							>
 								<Text classNames="font-bold" value={member.professional.firstname} />
 								<Text classNames="font-bold" value={member.professional.lastname} />
@@ -80,31 +75,6 @@
 						</div>
 					</div>
 				{/each}
-			</Accordion>
-			<Accordion title="Historique des démarches">
-				<div class="flex flex-col">
-					<div class="flex flex-row w-full justify-between">
-						<Button disabled={true} on:click={() => alert('Not implemented!')}
-							>Ajouter une étape
-						</Button>
-						<div class="flex flex-row justify-between space-x-4">
-							<Select
-								disabled={true}
-								selected={selectedPeriod}
-								options={periodOptions}
-								selectLabel=""
-								selectHint="Sélectionner une période"
-							/>
-							<Select
-								disabled={true}
-								selected={selectedOrder}
-								options={orderOptions}
-								selectLabel=""
-								selectHint="Sélectionner un tri"
-							/>
-						</div>
-					</div>
-				</div>
 			</Accordion>
 		</Accordions>
 	</div>
