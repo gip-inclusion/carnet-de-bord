d96d57f601ec19a4f83daff67e5e24abdb37fbaf
Augustin <Augustin82@users.noreply.github.com>
2021-09-27 16:38:06 +0200

    feat(ui): bring Create Beneficiary closer to the wireframe (#289)
    * feat(ui): bring Create Beneficiary closer to the wireframe
    
    Clsoes #262
    
    * fix(form): set input width
    
    Co-authored-by: LionelB <lionel@lumographe.fr>

--- src/lib/ui/base/Radio.svelte
+++ src/lib/ui/base/Radio.svelte
@@ -8,7 +8,7 @@
 
 	export let selected: string | null;
 	export let options: Option[];
-	export let caption: string;
+	export let caption: string | null = null;
 	export let legendClass = '';
 
 	const dispatch = createEventDispatcher();
@@ -27,9 +27,11 @@
 <div class="fr-form-group">
 	<fieldset class="fr-fieldset">
 		<legend class="fr-fieldset__legend fr-text--regular" id="radio-legend">
-			<span class={legendClass}>
-				{caption}
-			</span>
+			{#if caption}
+				<span class={legendClass}>
+					{caption}
+				</span>
+			{/if}
 		</legend>
 		<div class="fr-fieldset__content my-4">
 			{#each options as option (option.name)}
