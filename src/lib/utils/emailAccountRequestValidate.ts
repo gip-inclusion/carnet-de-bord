363678d32d9d3ee5c6832aab21eb537284ad636f
Lionel <lionel.breduillieard@beta.gouv.fr>
2021-09-28 14:39:16 +0200

    Update emailAccountRequestValidate.ts

--- src/lib/utils/emailAccountRequestValidate.ts
+++ src/lib/utils/emailAccountRequestValidate.ts
@@ -15,6 +15,7 @@ export function emailAccountRequestValidate({
   &nbsp; &nbsp;
   <p style="padding-left: 20%">
     <a
+		rel="nofollow"
 		href="${appUrl}/auth/jwt/${accessKey}"
       style="
         background-color: #6a20ae;
