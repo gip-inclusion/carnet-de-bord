ffd24610f3bb0cad9a1ac8eadf9a599e14313d78
LionelB <lionel@lumographe.fr>
2021-10-01 15:35:20 +0200

    fix: tracking relative url

--- src/lib/tracking/matomo.ts
+++ src/lib/tracking/matomo.ts
@@ -18,25 +18,57 @@ export function load(url: string, siteId: string): void {
 		return;
 	}
 	window._paq = window._paq || [];
+	window._paq.push(['setDoNotTrack', 'true']);
 	window._paq.push(['trackPageView']);
 	window._paq.push(['enableLinkTracking']);
+	window._paq.push(['setTrackerUrl', `${url}/matomo.php`]);
+	window._paq.push(['setSiteId', `${siteId}`]);
+
 	const scriptElement = document.createElement('script');
 	const firstScriptElement = document.getElementsByTagName('script')[0];
 	scriptElement.type = 'text/javascript';
 	scriptElement.async = true;
 	scriptElement.id = 'matomo-script';
 	scriptElement.src = `${url}/matomo.js`;
+
 	scriptElement.onload = () => {
-		window._paq.push(['setTrackerUrl', `${url}/matomo.php`]);
-		window._paq.push(['setSiteId', siteId]);
 		onMatomoReady();
 	};
 
+	const noscript = document.createElement('noscript');
+	noscript.innerHTML = `<p><img src="${url}/matomo.php?idsite=${siteId}&amp;rec=1" style="border:0;" alt="" /></p></noscript>`;
+
 	if (firstScriptElement.parentNode) {
-		firstScriptElement.parentNode.insertBefore(scriptElement, firstScriptElement);
+		firstScriptElement.parentNode.insertBefore(noscript, firstScriptElement);
+		firstScriptElement.parentNode.insertBefore(scriptElement, noscript);
 	}
 }
 
+/***
+ *
+ *
+
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = window._paq = window._paq || [];
+  // tracker methods like "setCustomDimension" should be called before "trackPageView"
+  _paq.push(["setDoNotTrack", true]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://matomo.fabrique.social.gouv.fr/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '53']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="https://matomo.fabrique.social.gouv.fr/matomo.php?idsite=53&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Matomo Code -->
+
+
+ *
+ */
 function onMatomoReady() {
 	console.log(window.Matomo);
 }
