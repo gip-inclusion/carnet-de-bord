1f43a347a982e1f1f950f9b50a3a75d66e3439a8
Augustin82 <augustin.ragon@gmail.com>
2021-10-21 08:59:47 +0200

    wip

---
+++ tests/parser/cd51.test.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+describe('Options tests', () => {
+	// the tests container
+	it('checking default options', () => {
+		// the single test
+		expect(true).to.be.false;
+	});
+});
