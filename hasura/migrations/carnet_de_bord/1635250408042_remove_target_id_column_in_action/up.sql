5974612a2d1c2113a470786a71f6b7384cefb4e7
LionelB <lionel@lumographe.fr>
2021-10-26 21:03:04 +0200

    fix: update target column migration

--- hasura/migrations/carnet_de_bord/1635250408042_remove_target_id_column_in_action/up.sql
+++ hasura/migrations/carnet_de_bord/1635250408042_remove_target_id_column_in_action/up.sql
@@ -1,5 +1,6 @@
 alter table "public"."ref_action" drop constraint "ref_action_target_id_fkey";
+alter table "public"."ref_action" alter column "target_id" drop not null;
 alter table "public"."ref_action" drop column "target_id" cascade;
-alter table "public"."ref_action" add column "theme" varchar not null;
+alter table "public"."ref_action" add column "theme" varchar not null default 'none'; 
 
 alter table "public"."ref_target" alter column "theme" set not null;
