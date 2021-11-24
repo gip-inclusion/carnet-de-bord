4875a64717cc0f4333e4c2e7b057d6ec14fc9d54
LionelB <lionel@lumographe.fr>
2021-11-24 16:06:30 +0100

    fix: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,4 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs', 'build', '.svelte-kit', '**/_gen/**', 'e2e'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript'),
-	},
-	rules: {
-		'@typescript-eslint/no-unused-vars': [
-			'error',
-			{ argsIgnorePattern: '^_', varsIgnorePattern: '^_', ignoreRestSiblings: true },
-		],
-	},
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2019,
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true,
-	},
+	root: true,
+	extends: ['eslint:recommended', 'prettier'],
 };
