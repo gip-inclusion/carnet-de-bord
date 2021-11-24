f17df60035b016f9e7a98e39127da16c0e787726
LionelB <lionel@lumographe.fr>
2021-11-24 15:38:11 +0100

    test: add e2e tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,25 @@
 module.exports = {
-	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs', 'build', '.svelte-kit', '**/_gen/**'],
+	ignorePatterns: ['*.cjs', 'build', '.svelte-kit', '**/_gen/**', 'e2e'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
-		'svelte3/typescript': () => require('typescript')
+		'svelte3/typescript': () => require('typescript'),
 	},
 	rules: {
 		'@typescript-eslint/no-unused-vars': [
 			'error',
-			{ "argsIgnorePattern": "^_", varsIgnorePattern: '^_', ignoreRestSiblings: true }
-		]
+			{ argsIgnorePattern: '^_', varsIgnorePattern: '^_', ignoreRestSiblings: true },
+		],
 	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2019
+		ecmaVersion: 2019,
 	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
-	}
+		node: true,
+	},
 };
