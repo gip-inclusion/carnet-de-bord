05d5f2796a28531eb96b71f8c3b0237dbe0bc3b5
LionelB <lionel@lumographe.fr>
2021-11-29 17:14:03 +0100

    feat: add creator_id for target

---
+++ hasura/migrations/carnet_de_bord/1638200411278_target_creator_id/up.sql
@@ -0,0 +1,19 @@
+
+alter table "public"."notebook_target" add column "creator_id" uuid
+ null;
+
+alter table "public"."notebook_target"
+  add constraint "notebook_target_creator_id_fkey"
+  foreign key ("creator_id")
+  references "public"."professional"
+  ("id") on update restrict on delete restrict;
+
+update notebook_target 
+set creator_id 
+= (
+    SELECT creator_id "creator"
+    FROM notebook_focus "focus" 
+    WHERE id = notebook_target.focus_id
+);
+
+alter table "public"."notebook_target" alter column "creator_id" set not null;
