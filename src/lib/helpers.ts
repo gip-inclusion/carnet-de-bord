8646d811412324a8c42b72ffe483e265b49d8542
Augustin82 <augustin.ragon@gmail.com>
2021-12-07 13:46:16 +0100

    feat: show manager stats since beginning and start of month

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -3,7 +3,7 @@ export const stringsMatch =
 	(haystack?: string | null): boolean =>
 		haystack && haystack.toLocaleLowerCase().includes(needle.toLocaleLowerCase());
 
-export function filterFalsyProps(obj: Record<string, unknown>): Record<string, unknonw> {
+export function filterFalsyProps(obj: Record<string, unknown>): Record<string, unknown> {
 	const result = {};
 	for (const [k, v] of Object.entries(obj)) {
 		if (v) {
@@ -12,3 +12,13 @@ export function filterFalsyProps(obj: Record<string, unknown>): Record<string, u
 	}
 	return result;
 }
+
+export const by = <T>(sorter: (data: T) => number, order = 'ASC'): ((d1: T, d2: T) => number) => {
+	let a = 1;
+	let b = -1;
+	if (order === 'DESC') {
+		a = -1;
+		b = 1;
+	}
+	return (d1: T, d2: T) => a * sorter(d1) + b * sorter(d2);
+};
