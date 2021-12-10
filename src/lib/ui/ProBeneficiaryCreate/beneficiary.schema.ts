7ec543d53586308684842106ee0bcbf9b341b6cb
LionelB <lionel@lumographe.fr>
2021-12-10 10:38:27 +0100

    fix: handle update beneficiary info

---
+++ src/lib/ui/ProBeneficiaryCreate/beneficiary.schema.ts
@@ -0,0 +1,41 @@
+import isDate from 'date-fns/isDate';
+import * as yup from 'yup';
+
+export const beneficiaryAccountSchema = yup.object().shape({
+	firstname: yup.string().required(),
+	lastname: yup.string().required(),
+	dateOfBirth: yup.string().test('is-date-valid', 'Le format de la date est incorrect', (value) => {
+		if (!/[0-9]{4}-[0-9]{2}-[0-9]{2}/.test(value)) {
+			return false;
+		}
+		const date = new Date(value);
+		return isDate(date);
+	}),
+
+	mobileNumber: yup
+		.string()
+		.test('is-phone-valid', 'Le format du téléphone est incorrect', (value) => {
+			return /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/.test(value.trim());
+		})
+		.nullable(),
+	email: yup.string().email().nullable(),
+	address1: yup.string().nullable(),
+	address2: yup.string().nullable(),
+	postalCode: yup
+		.string()
+		.nullable()
+		.test('is-cp-valid', 'Le code postal doit être composé de cinq chiffres', (value) => {
+			if (value) {
+				// le code postal est composé de 5 chiffres mais
+				// ne peut pas commencer par 00 sinon il est pas valide
+				if (value.slice(0, 2) === '00') return false;
+				return /[0-9]{5}/.test(value);
+			}
+			return true;
+		}),
+	city: yup.string().nullable(),
+	peNumber: yup.string().nullable(),
+	cafNumber: yup.string().nullable(),
+});
+
+export type BeneficiaryAccountInput = yup.InferType<typeof beneficiaryAccountSchema>;
--- src/lib/ui/ProNotebookPersonalInfo/ProNotebookPersonalInfoUpdate.svelte
+++ src/lib/ui/ProNotebookPersonalInfo/ProNotebookPersonalInfoUpdate.svelte
@@ -1,6 +1,3 @@
-<script context="module" lang="ts">
-</script>
-
 <script lang="ts">
 	import {
 		Beneficiary,
@@ -8,7 +5,14 @@
 	} from '$lib/graphql/_gen/typed-document-nodes';
 	import { openComponent } from '$lib/stores';
 	import { mutation, operationStore } from '@urql/svelte';
-	import { Button, Input } from '$lib/ui/base';
+	import { Button } from '$lib/ui/base';
+	import {
+		BeneficiaryAccountInput,
+		beneficiaryAccountSchema,
+	} from '../ProBeneficiaryCreate/beneficiary.schema';
+	import Form from '../forms/Form.svelte';
+	import ProBeneficiaryCreateFields from '../ProBeneficiaryCreate/ProBeneficiaryCreateFields.svelte';
+	import Input from '../forms/Input.svelte';
 
 	export let beneficiary: Pick<
 		Beneficiary,
@@ -28,31 +32,27 @@
 
 	const updateStore = operationStore(UpdateBeneficiaryPersonalInfoDocument);
 	const update = mutation(updateStore);
+	console.log(beneficiary);
 
-	function initFormData(): typeof beneficiary {
-		return {
-			id: beneficiary.id,
-			firstname: beneficiary.firstname,
-			lastname: beneficiary.lastname,
-			dateOfBirth: beneficiary.dateOfBirth,
-			mobileNumber: beneficiary.mobileNumber,
-			email: beneficiary.email,
-			address1: beneficiary.address1,
-			address2: beneficiary.address2,
-			postalCode: beneficiary.postalCode,
-			city: beneficiary.city,
-			peNumber: beneficiary.peNumber,
-			cafNumber: beneficiary.cafNumber,
-		};
-	}
+	const initialValues = {
+		firstname: beneficiary.firstname,
+		lastname: beneficiary.lastname,
+		dateOfBirth: beneficiary.dateOfBirth,
+		mobileNumber: beneficiary.mobileNumber,
+		email: beneficiary.email,
+		address1: beneficiary.address1,
+		address2: beneficiary.address2,
+		postalCode: beneficiary.postalCode,
+		city: beneficiary.city,
+		peNumber: beneficiary.peNumber,
+		cafNumber: beneficiary.cafNumber,
+	};
 
-	const formData = initFormData();
-	const fieldErrors: Partial<typeof beneficiary> = {};
-
-	async function handleSubmit() {
+	async function updateBeneficiary(values: BeneficiaryAccountInput) {
 		await update({
 			id: beneficiary.id,
-			...formData,
+			...values,
+			dateOfBirth: new Date(values.dateOfBirth),
 		});
 		openComponent.close();
 	}
@@ -67,81 +67,20 @@
 		<h1>Informations personnelles</h1>
 		<p class="mb-0">Veuillez cliquer sur un champ pour le modifier.</p>
 	</div>
-	<form on:submit|preventDefault={handleSubmit}>
-		<Input
-			bind:value={formData['firstname']}
-			placeholder={'Ex : Poquelin'}
-			inputLabel={'Nom'}
-			error={fieldErrors['firstname']}
-			required={true}
-		/>
-		<Input
-			bind:value={formData['lastname']}
-			placeholder={'Ex : Jean-Baptiste'}
-			inputLabel={'Prénom'}
-			error={fieldErrors['lastname']}
-			required={true}
-		/>
-		<!-- <Input
-			bind:value={formData['dateOfBirth']}
-			placeholder={'Ex : 21/12/1977'}
-			inputLabel={'Date de naissance (JJ/MM/AAAA)'}
-			error={fieldErrors['dateOfBirth']}
-			type={'text'}
-			required={true}
-		/> -->
-		<Input
-			bind:value={formData['mobileNumber']}
-			placeholder={'Ex : 0123456789'}
-			inputLabel={'Téléphone'}
-			error={fieldErrors['mobileNumber']}
-		/>
-		<Input
-			bind:value={formData['email']}
-			placeholder={'Ex : jb@poquelin.fr'}
-			inputLabel={'Courriel'}
-			error={fieldErrors['email']}
-			type={'email'}
-		/>
-		<Input
-			bind:value={formData['address1']}
-			placeholder={'Ex : 55-57 rue du Faubourg Saint-Honoré'}
-			inputLabel={'Adresse'}
-			error={fieldErrors['address1']}
-		/>
-		<Input
-			bind:value={formData['address2']}
-			placeholder={'Ex : 1er étage'}
-			inputLabel={'Adresse (complément)'}
-			error={fieldErrors['address2']}
-		/>
-		<Input
-			bind:value={formData['postalCode']}
-			placeholder={'Ex : 75 008'}
-			inputLabel={'Code postal'}
-			error={fieldErrors['postalCode']}
-		/>
-		<Input
-			bind:value={formData['city']}
-			placeholder={'Ex : Paris'}
-			inputLabel={'Ville'}
-			error={fieldErrors['city']}
-		/>
-		<Input
-			bind:value={formData['peNumber']}
-			placeholder={'Ex : 123456789A'}
-			inputLabel={'Identifiant Pôle emploi'}
-			error={fieldErrors['peNumber']}
-		/>
-		<Input
-			bind:value={formData['cafNumber']}
-			placeholder={'Ex : 123456789A'}
-			inputLabel={'Identifiant CAF/MSA'}
-			error={fieldErrors['cafNumber']}
-		/>
+	<Form
+		{initialValues}
+		validationSchema={beneficiaryAccountSchema}
+		onSubmit={updateBeneficiary}
+		let:isSubmitting
+		let:isSubmitted
+		let:isValid
+	>
+		<ProBeneficiaryCreateFields />
+		<Input name="peNumber" placeholder={'123456789A'} inputLabel={'Identifiant Pôle emploi'} />
+		<Input name="cafNumber" placeholder={'123456789A'} inputLabel={'Identifiant CAF/MSA'} />
 		<div class="flex flex-row gap-6 pt-4 pb-12">
 			<Button type="submit">Enregistrer</Button>
 			<Button outline on:click={onCancel}>Annuler</Button>
 		</div>
-	</form>
+	</Form>
 </section>
