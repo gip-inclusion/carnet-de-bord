e8a6869749c8d9c0a0c92016a5a848241f4dcd50
Augustin82 <augustin.ragon@gmail.com>
2021-12-23 10:03:53 +0100

    chore: update sender for CdB emails (#425)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 export const orgName = "Ministère<br />du Travail,<br />de l'Emploi<br />et de l'Insertion";
 
+// TODO(augustin): move this to an environment variable (distinct from SMTP_FROM)
 export const contactEmail = 'support.carnet-de-bord@fabrique.social.gouv.fr';
