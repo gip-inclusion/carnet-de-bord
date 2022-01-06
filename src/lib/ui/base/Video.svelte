27b1d129d52bf61d40a2f2918a5e5990cc8fc894
Augustin <Augustin82@users.noreply.github.com>
2022-01-06 17:23:06 +0100

    feat: update landing to add Video tag (#488)
    * feat: update landing to add Video tag
    
    * fix: updates from Jonathan
    
    * chore: use video without embedded subtitles
    
    * fix: add missing container to login body
    
    * feat: add voiceover video

---
+++ src/lib/ui/base/Video.svelte
@@ -0,0 +1,49 @@
+<script lang="ts" context="module">
+	let counter = 0;
+</script>
+
+<script lang="ts">
+	type Source = {
+		url: string;
+		type: string;
+	};
+	type Track = {
+		url: string;
+		label: string;
+		lang: string;
+	};
+	export let sources: Source[];
+	export let tracks: Track[];
+	export let title = '';
+	export let preload = 'metadata';
+	export let poster = '';
+	export let hideControls = false;
+	const videoId = `video-${counter++}`;
+
+	let hovered: boolean;
+	$: controls = !hideControls || hovered;
+</script>
+
+<div class="flex flex-col">
+	{#if title}
+		<h4 class="!text-france-blue">{title}</h4>
+	{/if}
+	<!-- svelte-ignore a11y-media-has-caption -->
+	<!-- see https://github.com/sveltejs/svelte/issues/5967 -->
+	<video
+		class="!cursor-pointer"
+		id={videoId}
+		{controls}
+		{preload}
+		{poster}
+		on:mouseenter={() => (hovered = true)}
+		on:mouseleave={() => (hovered = false)}
+	>
+		{#each sources as source}
+			<source src={source.url} type={source.type} />
+		{/each}
+		{#each tracks as track}
+			<track label={track.label} kind="captions" srclang={track.lang} src={track.url} default />
+		{/each}
+	</video>
+</div>
--- src/routes/auth/__layout.svelte
+++ src/routes/auth/__layout.svelte
@@ -1,15 +1,55 @@
 <script lang="ts">
 	import { HeaderCDB, FooterCDB } from '$lib/ui/index';
+	import { Video } from '$lib/ui/base';
+
+	const videoOptions = {
+		sources: [
+			/**
+			 * Video with neither subtitles nor voiceover
+			 * {
+			 *   url: 'https://nextcloud.fabrique.social.gouv.fr/s/TtM2A7KkD55MqZ3/download/intro-cdb.mp4',
+			 *   type: 'video/mp4',
+			 * },
+			 */
+
+			/**
+			 * Video with voiceover
+			 */
+			{
+				url: 'https://nextcloud.fabrique.social.gouv.fr/s/ZymqnnGR8LcALFf/download/intro-cdb-voiceover.mp4',
+				type: 'video/mp4',
+			},
+
+			/**
+			 * Video with embedded subtitles
+			 * {
+			 *   url: 'https://nextcloud.fabrique.social.gouv.fr/s/Nx3wbt6LTMrNqGE/download/intro-cdb-with-subs.mp4',
+			 *   type: 'video/mp4',
+			 * },
+			 */
+		],
+		tracks: [
+			{
+				url: '/videos/intro-cdb.websrt',
+				label: 'Français',
+				lang: 'fr',
+			},
+		],
+		title: 'Découvrez Carnet de bord en vidéo !',
+		preload: 'none',
+		hideControls: true,
+		poster: '/videos/intro-cdb.png',
+	};
 </script>
 
 <HeaderCDB menuItems={[]} />
 
-<div style="min-height: calc(100vh - 142.5px) flex">
+<div class="fr-container" style="min-height: calc(100vh - 142.5px)">
 	<div class="flex flex-row">
-		<div class="w-1/2 bg-gray-bg flex items-center justify-center">
-			<img class="h-full max-h-96" src="/logo-cdb.svg" alt="" aria-hidden />
+		<div class="w-1/2 py-20 pr-10">
+			<Video {...videoOptions} />
 		</div>
-		<div class="w-1/2 p-20">
+		<div class="w-1/2 py-20 pl-10">
 			<slot />
 		</div>
 	</div>
