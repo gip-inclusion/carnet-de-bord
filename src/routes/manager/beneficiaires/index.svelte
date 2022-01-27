10c3eea34aee657c5307e6d29079153ec765b2b4
Augustin82 <augustin.ragon@gmail.com>
2022-01-27 11:49:18 +0100

    wip

--- src/routes/manager/beneficiaires/index.svelte
+++ src/routes/manager/beneficiaires/index.svelte
@@ -4,12 +4,19 @@
 	import Dialog from '$lib/ui/Dialog.svelte';
 </script>
 
-<Dialog
-	label="Importer des bénéficiaires"
-	buttonLabel="Importer des bénéficiaires"
-	title="Importer des bénéficiaires"
-	size={'large'}
-	showButtons={false}
->
-	<AdminDeploymentBeneficiariesImport />
-</Dialog>
+<svelte:head>
+	<title>Gestion des bénéficiaires - Carnet de bord</title>
+</svelte:head>
+
+<div>
+	<p>Cliquez sur le bouton ci-dessous pour procéder à l'import des bénéficiaires du RSA.</p>
+	<Dialog
+		label="Importer des bénéficiaires"
+		buttonLabel="Importer des bénéficiaires"
+		title="Importer des bénéficiaires"
+		size={'large'}
+		showButtons={false}
+	>
+		<AdminDeploymentBeneficiariesImport />
+	</Dialog>
+</div>
