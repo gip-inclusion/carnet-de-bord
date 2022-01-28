7dfc9ed48e16aab250c7df6d98fd1aa9a48f7b8c
Augustin82 <augustin.ragon@gmail.com>
2022-01-28 20:48:46 +0100

    feat: email is now the default way to log in

--- src/routes/auth/login/index.ts
+++ src/routes/auth/login/index.ts
@@ -51,35 +51,30 @@ export const post: RequestHandler<Record<string, unknown>, Record<string, unknow
 
 	const { username } = body;
 
-	const usernameResult = await client
-		.query<GetAccountByUsernameQuery>(GetAccountByUsernameDocument, { comp: { _eq: username } })
-		.toPromise();
-	if (usernameResult.error) {
-		return {
-			status: 401,
-			body: {
-				errors: 'USER_NOT_FOUND',
+	const emailResult = await client
+		.query<GetAccountByEmailQuery>(GetAccountByEmailDocument, {
+			criteria: {
+				_or: [
+					{ beneficiary: { email: { _eq: username } } },
+					{ professional: { email: { _eq: username } } },
+					{ manager: { email: { _eq: username } } },
+					{ admin: { email: { _eq: username } } },
+				],
 			},
-		};
-	}
+		})
+		.toPromise();
+	let account: typeof emailResult.data.account[0];
 
-	let account: typeof usernameResult.data.account[0];
-
-	if (!usernameResult.data || usernameResult.data.account.length === 0) {
-		const emailResult = await client
-			.query<GetAccountByEmailQuery>(GetAccountByEmailDocument, {
-				criteria: {
-					_or: [
-						{ beneficiary: { email: { _eq: username } } },
-						{ professional: { email: { _eq: username } } },
-						{ manager: { email: { _eq: username } } },
-						{ admin: { email: { _eq: username } } },
-					],
-				},
-			})
+	if (emailResult.error || emailResult.data.account.length === 0) {
+		// if we fail to find an account for the given email address,
+		// we try searching by the now-deprecated username
+		const usernameResult = await client
+			.query<GetAccountByUsernameQuery>(GetAccountByUsernameDocument, { comp: { _eq: username } })
 			.toPromise();
 
-		if (emailResult.error || emailResult.data.account.length === 0) {
+		if (usernameResult.error) {
+			// something went wrong but we're not saying what at the moment
+			console.info('Could not find account with email', { username });
 			return {
 				status: 401,
 				body: {
@@ -87,11 +82,28 @@ export const post: RequestHandler<Record<string, unknown>, Record<string, unknow
 				},
 			};
 		}
-		account = emailResult.data.account[0];
-	} else {
+
+		if (!usernameResult.data || usernameResult.data.account.length === 0) {
+			// it went fine but we still found no user, time to give up
+			console.info('Could not find username', { username });
+			return {
+				status: 401,
+				body: {
+					errors: 'USER_NOT_FOUND',
+				},
+			};
+		}
+
+		// otherwise, we have a winner
 		account = usernameResult.data.account[0];
+	} else {
+		// we did get an answer,let's use it!
+		account = emailResult.data.account[0];
 	}
+
 	if (!account.confirmed) {
+		// OK, you do have an account, but it's not confirmed/enabled yet, so no dice!
+		console.info('Refused log-in for unconfirmed account', { username });
 		return {
 			status: 403,
 			body: {
@@ -99,12 +111,18 @@ export const post: RequestHandler<Record<string, unknown>, Record<string, unknow
 			},
 		};
 	}
+
 	const { id, beneficiary, manager, admin, professional } = account;
 	const user = beneficiary || manager || admin || professional;
 
 	const result = await updateAccessKey(client, id);
+
 	if (result.error) {
-		console.error('login', result.error);
+		console.error('Could not update access key when logging in', {
+			error: result.error,
+			id,
+			username,
+		});
 		return {
 			status: 500,
 			body: {
@@ -138,8 +156,8 @@ export const post: RequestHandler<Record<string, unknown>, Record<string, unknow
 				},
 			],
 		});
-	} catch (e) {
-		console.log(e);
+	} catch (error) {
+		console.error('Failed sending login email', { error, id, username, email });
 	}
 
 	return {
