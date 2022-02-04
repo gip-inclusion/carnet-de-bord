a11aac238cdf86ab13fb2b99638c8363b8976769
Augustin82 <augustin.ragon@gmail.com>
2022-02-04 09:27:02 +0100

    fix: missing admin structure unicity

---
+++ hasura/migrations/carnet_de_bord/1643963150726_adm_struct_fixes/down.sql
@@ -0,0 +1,7 @@
+
+
+alter table "public"."account" drop constraint "Account must be linked to a profile of the right type";
+alter table "public"."account" add constraint "Account must be linked to a profile of the right type" check (CHECK (type::text = 'manager'::text AND manager_id IS NOT NULL OR type::text = 'beneficiary'::text AND beneficiary_id IS NOT NULL OR type::text = 'professional'::text AND professional_id IS NOT NULL OR type::text = 'admin_cdb'::text AND admin_id IS NOT NULL));
+
+
+alter table "public"."account" drop constraint "account_admin_structure_id_key";
