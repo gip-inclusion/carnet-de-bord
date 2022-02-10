5d96cecaa65f4ddfd5cf5075a2f7f874b1a20817
Augustin <Augustin82@users.noreply.github.com>
2022-02-10 20:58:06 +0100

    feat: education level (#610)
    * feat: education level
    
    * Apply suggestions from code review

---
+++ hasura/migrations/carnet_de_bord/1644337776226_education_level/up.sql
@@ -0,0 +1,6 @@
+UPDATE notebook SET education_level='NV5' where education_level='level_3';
+UPDATE notebook SET education_level='NV4' where education_level='level_4';
+UPDATE notebook SET education_level='NV3' where education_level='level_5';
+UPDATE notebook SET education_level='NV2' where education_level='level_6';
+UPDATE notebook SET education_level='NV1' where education_level='level_7';
+UPDATE notebook SET education_level='NV1' where education_level='level_8';
