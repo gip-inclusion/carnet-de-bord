afd019c2dde54a82c7e81b10265a1236cb1c074c
Vincent Jousse <vincent@jousse.org>
2022-02-16 17:24:35 +0000

    chore: clean-up migrations

---
+++ hasura/migrations/carnet_de_bord/1645031738892_add_search_beneficiaries_function/down.sql
@@ -0,0 +1,14 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE FUNCTION search_beneficiaries(search text)
+-- RETURNS SETOF notebook_member AS $$
+--   SELECT notebook_member.*
+--   FROM notebook_member
+--   JOIN notebook ON notebook.id = notebook_member.notebook_id
+--   JOIN beneficiary ON beneficiary.id = notebook.beneficiary_id
+--   WHERE
+--       unaccent(search) <% beneficiary.lastname
+--       OR search <% beneficiary.pe_number
+--       OR search <% beneficiary.caf_number
+--       OR search <% beneficiary.mobile_number
+-- $$ LANGUAGE sql STABLE;
---
+++ hasura/migrations/carnet_de_bord/1645031738892_add_search_beneficiaries_function/up.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION search_beneficiaries(search text)
+RETURNS SETOF notebook_member AS $$
+  SELECT notebook_member.*
+  FROM notebook_member
+  JOIN notebook ON notebook.id = notebook_member.notebook_id
+  JOIN beneficiary ON beneficiary.id = notebook.beneficiary_id
+  WHERE
+      unaccent(search) <% beneficiary.lastname
+      OR search <% beneficiary.pe_number
+      OR search <% beneficiary.caf_number
+      OR search <% beneficiary.mobile_number
+$$ LANGUAGE sql STABLE;
