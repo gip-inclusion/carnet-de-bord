40cfff8084d76713164cd2523925235df2a9129b
Vincent Jousse <vincent@jousse.org>
2022-02-16 17:39:38 +0000

    fix: down migrations

--- hasura/migrations/carnet_de_bord/1645026702022_add_pg_trgm_extension/down.sql
+++ hasura/migrations/carnet_de_bord/1645026702022_add_pg_trgm_extension/down.sql
@@ -1,3 +1,1 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- CREATE EXTENSION pg_trgm;
+DROP EXTENSION pg_trgm;
--- hasura/migrations/carnet_de_bord/1645032690130_add_search_notebook_members_function/down.sql
+++ hasura/migrations/carnet_de_bord/1645032690130_add_search_notebook_members_function/down.sql
@@ -1,14 +1,1 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- CREATE FUNCTION search_notebook_members(search text)
--- RETURNS SETOF notebook_member AS $$
---   SELECT notebook_member.*
---   FROM notebook_member
---   JOIN notebook ON notebook.id = notebook_member.notebook_id
---   JOIN beneficiary ON beneficiary.id = notebook.beneficiary_id
---   WHERE
---       unaccent(search) <% beneficiary.lastname
---       OR search <% beneficiary.pe_number
---       OR search <% beneficiary.caf_number
---       OR search <% beneficiary.mobile_number
--- $$ LANGUAGE sql STABLE;
+DROP FUNCTION IF EXISTS search_notebook_members;
