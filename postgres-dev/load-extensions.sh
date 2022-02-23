dacfd644085eafcd766bde6bebdcaace228bcb5c
Lionel <lionel@lumographe.fr>
2022-02-23 20:52:54 +0100

    Update postgres-dev/load-extensions.sh
    Co-authored-by: Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>

--- postgres-dev/load-extensions.sh
+++ postgres-dev/load-extensions.sh
@@ -4,7 +4,7 @@
 # to create, but this is mostly an illustration of what can be done
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d"$POSTGRES_DB" <<EOF
-create extension pg_trgm;
-create extension unaccent;
+create extension if not exists pg_trgm;
+create extension if not exists unaccent;
 select * FROM pg_extension;
 EOF
