43a45e2d2f2e36ecd04d1d545a015db9b1d4fedc
Vincent Jousse <vincent@jousse.org>
2022-03-01 11:03:22 +0000

    feat: add migrations

---
+++ hasura/migrations/carnet_de_bord/1646132441749_alter_table_public_notebook_target_add_column_status/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."notebook_target" add column "status" varchar
+--  not null default 'in_progress';
