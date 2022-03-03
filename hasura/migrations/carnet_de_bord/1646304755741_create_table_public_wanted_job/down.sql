1d6d29524afefa0b44a9e1619b02789e3e2ea808
Augustin82 <augustin.ragon@gmail.com>
2022-03-03 11:54:52 +0100

    feat: create wanted_job table and relationships

---
+++ hasura/migrations/carnet_de_bord/1646304755741_create_table_public_wanted_job/down.sql
@@ -0,0 +1,1 @@
+DROP TABLE "public"."wanted_job";
---
+++ hasura/migrations/carnet_de_bord/1646304755741_create_table_public_wanted_job/up.sql
@@ -0,0 +1,2 @@
+CREATE TABLE "public"."wanted_job" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "notebook_id" UUID NOT NULL, "rome_code_id" UUID NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("notebook_id") REFERENCES "public"."notebook"("id") ON UPDATE restrict ON DELETE restrict, FOREIGN KEY ("rome_code_id") REFERENCES "public"."rome_code"("id") ON UPDATE restrict ON DELETE restrict, UNIQUE ("id"), UNIQUE ("notebook_id", "rome_code_id"));COMMENT ON TABLE "public"."wanted_job" IS E'Stores the jobs wanted for a notebook beneficiary';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
