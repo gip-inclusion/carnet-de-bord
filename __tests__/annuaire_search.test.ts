34aaa2e57aaf06c9987ea09b8492029203d362dc
Vincent Jousse <vincent@jousse.org>
2022-03-09 17:10:41 +0100

    chore: read GraphQL file directly

--- __tests__/annuaire_search.test.ts
+++ __tests__/annuaire_search.test.ts
@@ -1,38 +1,22 @@
 require('isomorphic-fetch');
+import fs from 'fs';
+import path from 'path';
 
-it('should check health point', async () => {
+it('GraphQL query should return all user with empty filter', async () => {
 	try {
+		const query = fs.readFileSync(
+			path.join(__dirname, '../src/routes/pro/annuaire/', '_searchNotebookMember.gql'),
+			'utf8'
+		);
 		const response = await fetch(process.env.VITE_GRAPHQL_API_URL, {
 			method: 'POST',
 			headers: {
 				'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET || 'admin',
 				'x-hasura-role': 'professional',
-				'x-hasura-user-id': '1a5b817b-6b81-4a4d-9953-26707a54e0e9',
+				'x-hasura-professional-id': '1a5b817b-6b81-4a4d-9953-26707a54e0e9',
 			},
 			body: JSON.stringify({
-				query: `query SearchNotebookMember($professionalId: uuid!, $filter: String, $visitDate: timestamptz_comparison_exp!) {
-  search_notebook_members(
-    args: {search: $filter}
-    where: {professionalId: {_eq: $professionalId}, lastVisitedAt: $visitDate}
-    order_by: {lastModifiedAt: desc_nulls_first}
-  ) {
-    id
-    notebook {
-      id
-      beneficiary {
-        dateOfBirth
-        firstname
-        id
-        lastname
-        mobileNumber
-        __typename
-      }
-      __typename
-    }
-    __typename
-  }
-}
-`,
+				query: query,
 				variables: {
 					filter: '',
 					professionalId: '1a5b817b-6b81-4a4d-9953-26707a54e0e9',
@@ -41,9 +25,8 @@ it('should check health point', async () => {
 			}),
 		});
 		const { data } = await response.json();
-		console.log(response);
-		console.log(data);
-		expect(response.ok).toBeTruthy();
+		expect(data.search_notebook_members.length).toEqual(1);
+		expect(data.search_notebook_members[0].id).toEqual('14c147d0-f94b-4708-be90-0227efc70db7');
 	} catch (error) {
 		console.log(error);
 	}
