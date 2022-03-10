0655934d0d2c928ae2411fe864115129b4683b7a
LionelB <lionel@lumographe.fr>
2022-03-10 17:38:41 +0100

    refactor: squahs migrations

--- hasura/migrations/carnet_de_bord/1646930043914_multi_rome_code/down.sql
+++ hasura/migrations/carnet_de_bord/1646930043914_multi_rome_code/down.sql
@@ -1,4 +1,5 @@
 
+
 alter table "public"."notebook" alter column "job" drop not null;
 alter table "public"."notebook" add column "job" varchar;
 
@@ -15,3 +16,11 @@ alter table "public"."notebook" add column "job" varchar;
 -- SELECT nb.id nbid, rc.id rcid
 -- FROM notebook nb
 -- LEFT JOIN rome_code rc ON nb.job = rc.label;
+
+DROP TABLE "public"."wanted_job";
+
+DROP FUNCTION IF EXISTS public.search_rome_codes; 
+
+alter table "public"."rome_code" drop constraint "rome_code_label_key";
+
+DROP TABLE "public"."rome_code";
