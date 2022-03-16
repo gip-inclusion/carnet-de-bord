08547dc7c7801552e044be484024b4adc406aaa4
Vincent Jousse <vincent@jousse.org>
2022-03-16 10:32:49 +0100

    fix: remove debug

--- __tests__/health.test.ts
+++ __tests__/health.test.ts
@@ -2,7 +2,6 @@ require('isomorphic-fetch');
 
 it('should check health point', async () => {
 	try {
-		console.log(process.env.HASURA_GRAPHQL_ENDPOINT + '/healthz');
 		const response = await fetch(process.env.HASURA_GRAPHQL_ENDPOINT + '/healthz', {
 			method: 'GET',
 		});
