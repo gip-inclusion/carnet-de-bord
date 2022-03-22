1e8bba7cdc000a64aab1d0e07889afb1785b269b
Lotfi <boutchacha.lotfi@gmail.com>
2022-03-22 15:46:24 +0100

    test: remove unused fixtures

--- e2e/fixtures/fixtures.js
+++ e2e/fixtures/fixtures.js
@@ -2,28 +2,10 @@ class CdbFixtures {
 	constructor() {}
 
 	static setupByTags(I, tags) {
-		if (tags.indexOf('@_loginStructureAdmin') >= 0) {
-			CdbFixtures.createStructureAdmin(I);
+		if (tags.indexOf('@_tagName') >= 0) {
+			console.log('Do something');
 		}
 	}
-
-	static cleanUpByTags(tags) {
-		if (tags.indexOf('@_loginStructureAdmin') >= 0) {
-			CdbFixtures.deleteStructureAdmin();
-		}
-	}
-
-	static createStructureAdmin(I) {
-		I.sendMutation(
-			`mutation insert_admin_structure {
-			  insert_admin_structure(objects: {firstname: "Vincent", lastname: "Tim", email: "vincent.tim@beta.gouv.fr", deploymentId: "4dab8036-a86e-4d5f-9bd4-6ce88c1940d0"}) {
-			    affected_rows
-			  }
-			}`
-		);
-	}
-
-	static deleteStructureAdmin() {}
 }
 
 module.exports = {
