bf13f149899222f7acb09bbc3d64c80e1a4617d8
Vincent Jousse <vincent@jousse.org>
2022-03-23 16:13:28 +0000

    feat: add trigger for target events

---
+++ hasura/migrations/carnet_de_bord/1648051501156_add_event_target_trigger/up.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION public.record_notebook_target_event() RETURNS trigger AS
+$$
+DECLARE
+  new_notebook_id uuid;
+BEGIN
+
+  SELECT notebook_focus.notebook_id
+  INTO new_notebook_id
+  FROM notebook_focus
+  WHERE notebook_focus.id = NEW.focus_id;
+
+  INSERT INTO notebook_event
+  (notebook_id, event_date, professional_id, event)
+  VALUES
+  (new_notebook_id, now(), NEW.creator_id, 'target.status.' || NEW.status);
+  RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER record_notebook_target_event_update_trigger
+AFTER UPDATE ON public.notebook_target
+    FOR EACH ROW
+    WHEN ((OLD.status)
+       IS DISTINCT FROM
+      (NEW.status))
+    EXECUTE PROCEDURE public.record_notebook_target_event()
+;
