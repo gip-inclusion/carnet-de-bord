e41fd1ef9017e56b77f161273365a0512b2db8dd
Vincent Jousse <vincent@jousse.org>
2022-03-28 12:31:24 +0100

    fix: add event types to migrations

--- hasura/migrations/carnet_de_bord/1648464159795_squashed/up.sql
+++ hasura/migrations/carnet_de_bord/1648464159795_squashed/up.sql
@@ -1,14 +1,17 @@
-alter table "public"."notebook_event" drop column "structure" cascade;
-alter table "public"."notebook_event" drop column "event" cascade;
+ALTER TABLE "public"."notebook_event" DROP COLUMN "structure" cascade;
+ALTER TABLE "public"."notebook_event" DROP COLUMN "event" cascade;
 ALTER TABLE "public"."notebook_event" ALTER COLUMN "event_date" TYPE timestamptz;
-alter table "public"."notebook_event" add column "event" jsonb not null;
+ALTER TABLE "public"."notebook_event" add column "event" jsonb not null;
 
 CREATE TABLE "public"."notebook_event_type" ("value" text NOT NULL, "comment" text NOT NULL, PRIMARY KEY ("value") , UNIQUE ("value"));
 
-alter table "public"."notebook_event" add column "event_type" text not null;
+ALTER TABLE "public"."notebook_event" ADD COLUMN "event_type" text NOT NULL;
 
-alter table "public"."notebook_event"
-  add constraint "notebook_event_event_type_fkey"
-  foreign key ("event_type")
-  references "public"."notebook_event_type"
-  ("value") on update restrict on delete restrict;
+ALTER TABLE "public"."notebook_event"
+  ADD CONSTRAINT "notebook_event_event_type_fkey"
+  FOREIGN KEY ("event_type")
+  REFERENCES "public"."notebook_event_type"
+  ("value") ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+INSERT INTO "public"."notebook_event_type" (value, comment) VALUES ('action','Action d''un objectif');
+INSERT INTO "public"."notebook_event_type" (value, comment) VALUES ('target','Objectif d''un parcours');
