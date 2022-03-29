5266e740c2ea1cf5351315f924d3d6f904bc6f62
Lotfi <boutchacha.lotfi@gmail.com>
2022-03-29 18:09:09 +0400

    test: add onboarding mutation and step

--- e2e/fixtures/fixtures.js
+++ e2e/fixtures/fixtures.js
@@ -1,10 +1,25 @@
 class CdbFixtures {
-	constructor() {}
+	constructor(I) {
+		this.I = I;
+	}
 
-	static setupByTags(I, tags) {
-		if (tags.indexOf('@_tagName') >= 0) {
-			console.log('Do something');
+	setupByTags(tags, queryParams) {
+		if (tags.indexOf('@onboarding_admin_structure') >= 0) {
+			this.onboardingSetup(queryParams['email'], 'admin_structure', false);
 		}
+		if (tags.indexOf('@onboarding_admin_structure_done') >= 0) {
+			this.onboardingSetup('admin_structure', true);
+		}
+	}
+
+	onboardingSetup(email, userType, onboardingDone) {
+		this.I.sendMutation(
+			`mutation AdminStructureByEmail {
+			  update_account(where: {${userType}: {email: {_eq: "${email}"}}}, _set: {onboardingDone: ${onboardingDone}) {
+			    affected_rows
+			  }
+			}`
+		);
 	}
 }
 
