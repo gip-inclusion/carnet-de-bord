0dad32f99239d2f995e1adeea95a591f88cb0b58
LionelB <lionel@lumographe.fr>
2022-04-07 15:19:04 +0200

    feat: download files

--- backend/scripts/pe-parse.sh
+++ backend/scripts/pe-parse.sh
@@ -6,4 +6,17 @@ set -eo pipefail
 trap "exit" INT TERM
 trap "kill 0" EXIT
 
+echo $PE_SERVER_KEY > pr_server.pem
+echo $PE_FILE_KEY > pe_file.pem
+
 echo "PE FETCH AND PARSE env:$hello"
+
+
+sftp  -i ./pe_server.pem $PE_URL:/OI33SPIE/principal .
+sftp  -i ./pe_server.pem $PE_URL:/OI33SPIE/actions .
+
+openssl smime -decrypt -in principal -binary -inform DEM -inkey pe_file.pem -out principal.csv
+openssl smime -decrypt -in actions -binary -inform DEM -inkey pe_file.pem -out actions.csv
+
+echo "fichier principal: $(wc -l principal.csv) lignes"
+echo "fichier actions: $(wc -l actions.csv) lignes"
