d47ba54975eb0abe493f3f6792a3fa5a6024d8de
LionelB <lionel@lumographe.fr>
2022-04-07 21:51:53 +0200

    fix: update script

--- backend/scripts/pe-parse.sh
+++ backend/scripts/pe-parse.sh
@@ -10,14 +10,17 @@ echo "dump private keys to files."
 
 echo -e "$PE_SERVER_KEY" > pe_server.pem
 echo -e "$PE_FILE_KEY" > pe_file.pem
+chmod 600 *.pem
 
 echo "download files."
-
-sftp  -i ./pe_server.pem $PE_SERVER_URL:/OI33SPIE/principal .
-sftp  -i ./pe_server.pem $PE_SERVER_URL:/OI33SPIE/actions .
+sftp -o "StrictHostKeyChecking accept-new"  -i ./pe_server.pem $PE_SERVER_URL:/OI33SPIE/principal .
+sftp -i ./pe_server.pem $PE_SERVER_URL:/OI33SPIE/actions .
 
 openssl smime -decrypt -in principal -binary -inform DEM -inkey pe_file.pem -out principal.csv
 openssl smime -decrypt -in actions -binary -inform DEM -inkey pe_file.pem -out actions.csv
 
 echo "fichier principal: $(wc -l principal.csv) lignes"
 echo "fichier actions: $(wc -l actions.csv) lignes"
+
+rm principal.csv
+rm actions.csv
