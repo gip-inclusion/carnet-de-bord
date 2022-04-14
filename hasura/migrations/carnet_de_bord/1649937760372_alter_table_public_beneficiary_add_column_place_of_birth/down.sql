2fb7f0be02fcc4dea693370427eca0c5a60ebdc1
LionelB <lionel@lumographe.fr>
2022-04-14 14:51:12 +0200

    feat: add placeOfBirth in data model

---
+++ hasura/migrations/carnet_de_bord/1649937760372_alter_table_public_beneficiary_add_column_place_of_birth/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."beneficiary" add column "place_of_birth" varchar
+--  null;
---
+++ hasura/migrations/carnet_de_bord/1649937760372_alter_table_public_beneficiary_add_column_place_of_birth/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."beneficiary" add column "place_of_birth" varchar
+ null;
