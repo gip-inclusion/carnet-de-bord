b2b6467b87583f22c0c2bc7886d76289fef83816
Vincent Jousse <vincent@jousse.org>
2022-04-25 16:31:15 +0200

    test: drop and recreate db for every connection

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -4,11 +4,16 @@ import os
 import dask.dataframe as dd
 import pytest
 from api.core.db import get_connection_pool
-from asyncpg.connection import Connection
-from asyncpg.pool import Pool
 from dask.dataframe.core import DataFrame
 
 test_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+@pytest.fixture(scope="session")
+def seed_filepath() -> str:
+    return os.path.join(
+        test_dir, "..", "..", "hasura", "seeds", "carnet_de_bord", "seed-data.sql"
+    )
 
 
 @pytest.fixture
@@ -28,21 +33,29 @@ def pe_principal_csv_series(pe_principal_csv_filepath) -> DataFrame:
 
 @pytest.fixture(scope="session")
 @pytest.mark.asyncio
-async def db_connection(db_pool):
+async def db_connection(db_pool, seed_filepath):
     # Take a connection from the pool.
     if db_pool:
         async with db_pool.acquire() as connection:
             # Open a transaction.
+            await connection.execute(f"DROP DATABASE IF EXISTS carnet_de_bord_test;")
+            await connection.execute(f"CREATE DATABASE carnet_de_bord_test;")
+
+            # Load the seeds
+            with open(seed_filepath, "r") as file:
+                data = file.read()
+                await connection.execute(data)
+
             yield connection
 
 
 @pytest.fixture(scope="session")
 @pytest.mark.asyncio
-async def db_pool() -> Pool | None:
-    return await get_connection_pool(
+async def db_pool():
+    yield await get_connection_pool(
         os.getenv(
             "HASURA_GRAPHQL_DATABASE_URL",
-            "postgres://cdb:test@localhost:5432/carnet_de_bord",
+            "postgres://cdb:test@localhost:5432/carnet_de_bord_test",
         )
     )
 
