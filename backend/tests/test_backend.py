1df3368fed517dafb769ff60b1984efeef023d77
Vincent Jousse <vincent@jousse.org>
2022-04-27 17:46:43 +0200

    fix: csv content for test

--- backend/tests/test_backend.py
+++ backend/tests/test_backend.py
@@ -14,7 +14,7 @@ async def test_csv_row(pe_principal_csv_series):
     assert res.first_name == "SOPHIE"
     assert res.place_of_birth == "LIEU NAISSANCE 1"
     assert res.date_of_birth == date(1982, 2, 1)
-    assert res.rome_1 == "G1605"
-    assert res.rome_1_label == "Aide de cuisine de collectivité"
+    assert res.rome_1 == "A1101"
+    assert res.rome_1_label == "Conducteur / Conductrice de tracto-benne"
     assert res.rome_2 == "K2204"
     assert res.rome_2_label == "Agent / Agente de nettoyage en collectivité"
