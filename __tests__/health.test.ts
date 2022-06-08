810e8b344e2048ea29f85859c5c595146aeb0cb5
LionelB <lionel@lumographe.fr>
2022-06-08 16:25:59 +0200

    ci: clean env for functionnal test

--- __tests__/health.test.ts
+++ __tests__/health.test.ts
@@ -1,10 +1,13 @@
 require('isomorphic-fetch');
 
-it('should check health point', async () => {
+it('should check hasura health point', async () => {
 	try {
-		const response = await fetch(process.env.HASURA_GRAPHQL_ENDPOINT + '/healthz', {
-			method: 'GET',
-		});
+		const response = await fetch(
+			process.env.VITE_GRAPHQL_API_URL.replace('/v1/graphql', '/healthz'),
+			{
+				method: 'GET',
+			}
+		);
 
 		expect(response.ok).toBeTruthy();
 	} catch (error) {
