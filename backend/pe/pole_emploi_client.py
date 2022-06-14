e1ab948ad3c2163138759f451e293d8c3d3aee2a
Vincent Jousse <vincent@jousse.org>
2022-06-14 15:35:54 +0200

    feat: add agence endpoint

--- backend/pe/pole_emploi_client.py
+++ backend/pe/pole_emploi_client.py
@@ -50,12 +50,8 @@ class PoleEmploiApiClient:
         return f"{self.auth_base_url}/connexion/oauth2/access_token"
 
     @property
-    def recherche_individu_url(self):
-        return f"{self.base_url}/rechercheindividucertifie/v1/rechercheIndividuCertifie"
-
-    @property
-    def mise_a_jour_url(self):
-        return f"{self.base_url}/maj-pass-iae/v1/passIAE/miseAjour"
+    def agences_url(self):
+        return f"{self.base_url}/partenaire/referentielagences/v1/agences"
 
     def _refresh_token(self, at=None):
         if not at:
@@ -83,11 +79,11 @@ class PoleEmploiApiClient:
     def _headers(self):
         return {"Authorization": self.token, "Content-Type": "application/json"}
 
-    def _request(self, url, data):
+    def _get_request(self, url: str, params: dict):
         try:
             self._refresh_token()
-            response = httpx.post(
-                url, json=data, headers=self._headers, timeout=API_TIMEOUT_SECONDS
+            response = httpx.get(
+                url, params=params, headers=self._headers, timeout=API_TIMEOUT_SECONDS
             )
             data = response.json()
             if response.status_code != 200:
@@ -95,3 +91,107 @@ class PoleEmploiApiClient:
             return data
         except httpx.RequestError as exc:
             raise PoleEmploiAPIException(API_CLIENT_HTTP_ERROR_CODE) from exc
+
+    def recherche_agences(
+        self, commune: str, horaires: bool = False, zonecompetence: bool = False
+    ):
+        """Example data:
+        {
+            "commune":"67",
+            "horaires":True,
+            "zonecompetence":True
+        }
+
+        Example response:
+        [
+
+            {
+                "code": "HDF0170",
+                "codeSafir":"62512",
+                "libelle": "ARRAS",
+                "libelle": "Agence Pôle emploi ARRAS",
+                "type": "APE",
+                "typeAccueil": "3",
+                "codeRegionINSEE": "32",
+                "dispositifADEDA": true,
+                "contact": {
+                    "telephonePublic": "39-49",
+                    "email": "ape.62512@pole-emploi.fr"
+                },
+                "siret": "13000548123525",
+                "adressePrincipale": {
+                    "ligne4": "2 RUE DE LA SYMPHORINE",
+                    "ligne5": "",
+                    "ligne6": "62000 ARRAS",
+                    "gpsLon": 2.7354,
+                    "gpsLat": 50.302027,
+                    "communeImplantation": "62041",
+                    "bureauDistributeur": "62000"
+                },
+                "horaires": [
+                    {
+                        "jour": 1,
+                        "ouvertureMatin": "08:30",
+                        "fermetureMatin": "12:45",
+                        "horaireFerme": "N",
+                        "horaireEnContinu": "N",
+                        "ouvertureApresMidiRDV": "12:45",
+                        "fermetureApresMidiRDV": "16:30",
+                        "horaireFermeRDV": "N",
+                        "horaireEnContinuRDV": "N"
+                    },
+                    {
+                        "jour": 2,
+                        "ouvertureMatin": "08:30",
+                        "fermetureMatin": "12:45",
+                        "horaireFerme": "N",
+                        "horaireEnContinu": "N",
+                        "ouvertureApresMidiRDV": "12:45",
+                        "fermetureApresMidiRDV": "16:30",
+                        "horaireFermeRDV": "N",
+                        "horaireEnContinuRDV": "N"
+                    },
+                    {
+                        "jour": 3,
+                        "ouvertureMatin": "08:30",
+                        "fermetureMatin": "12:45",
+                        "horaireFerme": "N",
+                        "horaireEnContinu": "N",
+                        "ouvertureApresMidiRDV": "12:45",
+                        "fermetureApresMidiRDV": "16:30",
+                        "horaireFermeRDV": "N",
+                        "horaireEnContinuRDV": "N"
+                    },
+                    {
+                        "jour": 4,
+                        "ouvertureMatin": "08:30",
+                        "fermetureMatin": "12:45",
+                        "horaireFerme": "N",
+                        "horaireEnContinu": "N",
+                        "ouvertureApresMidiRDV": "12:45",
+                        "fermetureApresMidiRDV": "16:30",
+                        "horaireFermeRDV": "N",
+                        "horaireEnContinuRDV": "N"
+                    },
+                    {
+                        "jour": 5,
+                        "ouvertureMatin": "09:00",
+                        "fermetureMatin": "12:00",
+                        "horaireFerme": "N",
+                        "horaireEnContinu": "N",
+                        "horaireFermeRDV": "O",
+                        "horaireEnContinuRDV": "N"
+                    }
+                ]
+            }
+        ]
+        """
+        data = self._get_request(
+            self.agences_url,
+            params={
+                "horaires": horaires,
+                "commune": commune,
+                "zonecompetence": zonecompetence,
+            },
+        )
+        return data
