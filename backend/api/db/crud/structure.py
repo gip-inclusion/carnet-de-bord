211f02397971e8e741c94447db99bfabc35b1950
Vincent Jousse <vincent@jousse.org>
2022-06-15 16:40:41 +0200

    feat: get Structure by name

--- backend/api/db/crud/structure.py
+++ backend/api/db/crud/structure.py
@@ -1,7 +1,13 @@
+from typing import List
+
 from asyncpg import Record
 from asyncpg.connection import Connection
 
 from api.db.models.structure import Structure, StructureInsert
+
+
+def parse_structure_from_record(record: Record) -> Structure:
+    return Structure.parse_obj(record)
 
 
 async def insert_structure(
@@ -18,8 +24,30 @@ async def insert_structure(
     )
 
     if record:
-        return await parse_structure_from_record(record)
+        return parse_structure_from_record(record)
 
 
-async def parse_structure_from_record(record: Record) -> Structure:
-    return Structure.parse_obj(record)
+async def get_structure_by_name(connection: Connection, name: str) -> Structure | None:
+    return await get_structure_with_query(
+        connection,
+        """
+        WHERE name=$1
+        """,
+        name,
+    )
+
+
+async def get_structure_with_query(
+    connection: Connection, query: str, *args
+) -> Structure | None:
+
+    record: Record | None = await connection.fetchrow(
+        """
+        SELECT structure.* FROM public.structure {query}
+        """.format(
+            query=query
+        ),
+        *args,
+    )
+
+    return parse_structure_from_record(record)
--- backend/tests/test_structure.py
+++ backend/tests/test_structure.py
@@ -1,8 +1,8 @@
-from api.db.crud.structure import insert_structure
+from api.db.crud.structure import get_structure_by_name, insert_structure
 from api.db.models.structure import Structure, StructureInsert
 
 
-async def test_get_insert_structure(pe_principal_csv_series, db_connection):
+async def test_insert_structure(db_connection):
 
     structure_insert: StructureInsert = StructureInsert(
         siret="test siret",
@@ -19,6 +19,15 @@ async def test_get_insert_structure(pe_principal_csv_series, db_connection):
     )
     structure: Structure | None = await insert_structure(
         db_connection, structure_insert=structure_insert
+    )
+
+    assert structure is not None
+
+
+async def test_get_structure_by_name(db_connection):
+
+    structure: Structure | None = await get_structure_by_name(
+        db_connection, "Pole Emploi Agence Livry-Gargnan"
     )
 
     assert structure is not None
