340511cad718be5e6b8aa4ccde84e721abecd655
Vincent Jousse <vincent@jousse.org>
2022-07-18 10:17:41 +0100

    feat: add deployment and Rome Code

--- backend/api/db/models/out.py
+++ backend/api/db/models/out.py
@@ -1,10 +1,13 @@
 from datetime import date, datetime
+from uuid import UUID
 
 from pydantic import BaseModel
 
+from api.db.models.rome_code import RomeCode
+
 
 class DeploymentOut(BaseModel):
-    pass
+    label: str
 
 
 class BeneficiaryOut(BaseModel):
@@ -26,5 +29,18 @@ class BeneficiaryOut(BaseModel):
     internal_id: str | None
 
 
+class RomeCodeOut(BaseModel):
+    code: str
+    description: str
+    # This is just a string representation of description and code
+    # Format is 'description (code)'
+    label: str
+
+
+class WantedJobOut(BaseModel):
+    rome_code: RomeCodeOut
+
+
 class NotebookOut(BaseModel):
     beneficiary: BeneficiaryOut
+    wanted_jobs: list[WantedJobOut]
