45c8d289dbb5528316b05a319f03fffd75d59004
Jonathan Perret <jonathan.perret@beta.gouv.fr>
2022-09-08 19:10:41 +0200

    ci: fix simple eslint warnings

--- src/.eslintrc.cjs
+++ src/.eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs', 'build', '.svelte-kit', '**/_gen/**'],
+	ignorePatterns: ['build', '.svelte-kit', '**/_gen/**'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
 		'svelte3/typescript': () => require('typescript'),
