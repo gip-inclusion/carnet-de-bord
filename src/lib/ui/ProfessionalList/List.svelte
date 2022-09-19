18e098ced578f8ea040f78ebf1373c15f0d9dae1
Guillaume Lagorce <guillaume@hey-gul.com>
2022-09-19 11:53:25 -0400

    refactor: use class directive to style the tag

--- src/lib/ui/ProfessionalList/List.svelte
+++ src/lib/ui/ProfessionalList/List.svelte
@@ -4,16 +4,6 @@
 	type Professional = GetProfessionalsForStructureQuery['professional'][0];
 
 	export let professionals: Professional[];
-
-	function beneficiariesCount(professional: Professional) {
-		return professional.account.notebooksWhereMember_aggregate.aggregate.count;
-	}
-
-	function tagClasses(professional: Professional) {
-		const tagColor = beneficiariesCount(professional) === 0 ? 'fr-tag--purple-glycine' : '';
-		// remove the `cursor-default` class when click action is impletemented
-		return `fr-tag fr-tag-sm ${tagColor} cursor-default`;
-	}
 </script>
 
 <table class="w-full fr-table fr-table--layout-fixed">
@@ -29,20 +19,26 @@
 	</thead>
 	<tbody>
 		{#each professionals as professional}
+			{@const beneficiariesCount =
+				professional.account.notebooksWhereMember_aggregate.aggregate.count}
+			{@const hasNoBeneficiaries = beneficiariesCount === 0}
 			<tr>
 				<td>{displayFullName(professional)}</td>
 				<td>{professional.mobileNumber ?? '--'}</td>
 				<td>{professional.email}</td>
 				<td>{professional.account.onboardingDone ? 'Oui' : 'Non'}</td>
 				<td class="flex justify-end">
-					<button class={tagClasses(professional)}>
-						{beneficiariesCount(professional)}
+					<button
+						class="fr-tag fr-tag-sm cursor-default"
+						class:fr-tag--purple-glycine={hasNoBeneficiaries}
+					>
+						{beneficiariesCount}
 					</button>
 				</td>
 			</tr>
 		{/each}
 		{#if professionals.length === 0}
-			<tr><td colspan="10">Aucun professionel.</td></tr>
+			<tr><td colspan="5">Aucun professionel.</td></tr>
 		{/if}
 	</tbody>
 </table>
