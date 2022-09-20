89ab55d6ab7ada8917d3333e0a828df085386a8c
Jonathan Perret <jonathan.perret@beta.gouv.fr>
2022-09-20 21:22:40 +0200

    fix: pe-fetch: disable downloading and decrypting of actions.csv (#1103)

--- backend/scripts/pe-fetch.sh
+++ backend/scripts/pe-fetch.sh
@@ -14,13 +14,17 @@ chmod 600 *.pem
 
 echo "download files."
 sftp -o "StrictHostKeyChecking accept-new"  -i ./pe_server.pem $PE_SERVER_URL:/OI33SPIE/principal .
-sftp -i ./pe_server.pem $PE_SERVER_URL:/OI33SPIE/actions .
+
+# Processing of the "actions" file is disabled for now because it is too large to be decrypted
+# by "openssl smime". See https://marc.info/?l=openssl-users&m=138545785012939
+
+# sftp -i ./pe_server.pem $PE_SERVER_URL:/OI33SPIE/actions .
 
 openssl smime -decrypt -in principal -binary -inform DEM -inkey pe_file.pem -out principal.csv
-openssl smime -decrypt -in actions -binary -inform DEM -inkey pe_file.pem -out actions.csv
+# openssl smime -decrypt -in actions -binary -inform DEM -inkey pe_file.pem -out actions.csv
 
 echo "fichier principal: $(wc -l principal.csv) lignes"
-echo "fichier actions: $(wc -l actions.csv) lignes"
+# echo "fichier actions: $(wc -l actions.csv) lignes"
 
 cp principal.csv /mnt/pefiles
-cp actions.csv /mnt/pefiles
+# cp actions.csv /mnt/pefiles
