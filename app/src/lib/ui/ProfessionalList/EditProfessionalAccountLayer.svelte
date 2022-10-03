650f2f1b64336a08d3eec9d414da70a8569b8325
Guillaume Lagorce <guillaume@hey-gul.com>
2022-10-03 16:11:24 -0400

    feat: allow update of a professional
    We take advantage of this commit to adjust the gql request to retrieve
    professionals to sort them and to include the position

---
+++ app/src/lib/ui/ProfessionalList/EditProfessionalAccountLayer.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import { openComponent } from '$lib/stores';
+	import type { Professional, ProfessionalSetInput } from '$lib/graphql/_gen/typed-document-nodes';
+	import { operationStore, mutation } from '@urql/svelte';
+	import { UpdateProfessionalAccountDocument } from '$lib/graphql/_gen/typed-document-nodes';
+	import { Form, Input } from '$lib/ui/forms';
+	import { Alert, Button } from '../base/';
+	import { proAccountSchema } from '$lib/ui/ProCreationForm/pro.schema';
+
+	export let professional: Pick<
+		Professional,
+		'id' | 'firstname' | 'lastname' | 'mobileNumber' | 'email' | 'position'
+	>;
+
+	let errorMessage = '';
+
+	function closeLayer() {
+		openComponent.close();
+	}
+
+	const updateProfessionalAccountStore = operationStore(UpdateProfessionalAccountDocument);
+	const updateProfessionalAccount = mutation(updateProfessionalAccountStore);
+
+	async function editProfessionalAccountSubmitHandler(payload: ProfessionalSetInput) {
+		try {
+			await updateProfessionalAccount({ id: professional.id, payload });
+			closeLayer();
+		} catch (error) {
+			console.error(error);
+			errorMessage = 'Impossible de modifier ce professionnel';
+		}
+	}
+</script>
+
+<div class="flex flex-col gap-6">
+	<div>
+		<h1>Mettre à jour les informations du professionnel</h1>
+	</div>
+
+	<Form
+		initialValues={{
+			firstname: professional.firstname,
+			lastname: professional.lastname,
+			email: professional.email,
+			mobileNumber: professional.mobileNumber,
+			position: professional.position,
+		}}
+		validationSchema={proAccountSchema}
+		onSubmit={editProfessionalAccountSubmitHandler}
+		let:isSubmitting
+		let:isSubmitted
+		let:isValid
+	>
+		<div class="max-w-sm">
+			<h2 class="text-france-blue fr-h4">Informations personnelles</h2>
+
+			<Input placeholder="Jean Baptiste" inputLabel="Prénom" name="firstname" required />
+			<Input placeholder="Poquelin" inputLabel="Nom" name="lastname" required />
+			<Input placeholder="b@poquelin.fr" inputLabel="Courriel" name="email" required />
+			<Input placeholder="0789542136" inputLabel="Numéro de téléphone" name="mobileNumber" />
+			<Input placeholder="Conseiller en insertion" inputLabel="Position" name="position" />
+			<div class="flex flex-row gap-6 mt-12">
+				<Button type="submit" disabled={(isSubmitted && !isValid) || isSubmitting}
+					>Enregistrer les modifications</Button
+				>
+				<Button outline={true} on:click={closeLayer}>Annuler</Button>
+			</div>
+		</div>
+	</Form>
+	{#if errorMessage}
+		<div class="mb-8">
+			<Alert type="error" description={errorMessage} />
+		</div>
+	{/if}
+</div>
--- app/src/lib/ui/ProfessionalList/List.svelte
+++ app/src/lib/ui/ProfessionalList/List.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
 	import type { GetProfessionalsForStructureQuery } from '$lib/graphql/_gen/typed-document-nodes';
-
+	import { Button } from '../base';
+	import { openComponent } from '$lib/stores';
+	import EditProfessionalAccountLayer from '$lib/ui/ProfessionalList/EditProfessionalAccountLayer.svelte';
 	import { displayFullName } from '../format';
+
 	type Professional = GetProfessionalsForStructureQuery['professional'][0];
 
 	export let professionals: Professional[];
+
+	function openEditProfessionalAccountLayer(professional: Professional) {
+		openComponent.open({
+			component: EditProfessionalAccountLayer,
+			props: { professional },
+		});
+	}
 </script>
 
 <table class="w-full fr-table fr-table--layout-fixed">
@@ -16,6 +26,7 @@
 			<th class="text-left">Email</th>
 			<th class="text-left">Onboarding</th>
 			<th class="text-right">BRSA suivis</th>
+			<th class="text-right">Gérer</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -38,10 +49,18 @@
 						{beneficiariesCount}
 					</a>
 				</td>
+				<td class="text-right">
+					<Button
+						classNames="fr-btn--sm fr-btn--secondary"
+						icon="fr-icon-edit-line"
+						title="Mettre à jour"
+						on:click={() => openEditProfessionalAccountLayer(professional)}>Mettre à jour</Button
+					>
+				</td>
 			</tr>
 		{/each}
 		{#if professionals.length === 0}
-			<tr><td colspan="5">Aucun professionel.</td></tr>
+			<tr><td colspan="6">Aucun professionel.</td></tr>
 		{/if}
 	</tbody>
 </table>
