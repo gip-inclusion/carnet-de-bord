928db267416890ca735ec114866ce517a96a3173
Guillaume Lagorce <guillaume@hey-gul.com>
2022-10-05 12:10:52 -0400

    ci: read .hasura_version file in hasura cli action
    precommit config is also adapted as there is no need anymore to check test.yml HASURA_VERSION

--- scripts/check-hasura-version.sh
+++ scripts/check-hasura-version.sh
@@ -4,23 +4,13 @@ HASURA_VERSION=$(cat hasura/.hasura_version)
 echo "--> Version from .hasura_version file is $HASURA_VERSION"
 
 # check that version in Dockerfile is the same as in .hasura_version file
-DOCKERFILE_VERSION_IS_THE_SAME=$(head -1 hasura/Dockerfile | grep -oP ${HASURA_VERSION})
+DOCKERFILE_HASURA_VERSION=$(< hasura/Dockerfile awk -F= '/^ARG HASURA_VERSION=/ {print $2}')
+echo "--> Version from hasura/Dockerfile file is $DOCKERFILE_HASURA_VERSION"
 
-if [ -z "$DOCKERFILE_VERSION_IS_THE_SAME" ]
+if [ "$HASURA_VERSION" = "$DOCKERFILE_HASURA_VERSION" ]
 then
+	echo "--> Version in hasura/Dockerfile and specified version in hasura/.hasura_version are consistent"
+else
 	echo "--> Error: Version in hasura/Dockerfile mismatches specified version in hasura/.hasura_version"
 	exit 1
-else
-	echo "--> Version in hasura/Dockerfile and specified version in hasura/.hasura_version are consistent"
-fi
-
-# check that version used by the CI is the same as in .hasura_version file
-CI_VERSION_IS_THE_SAME=$(grep "HASURA_VERSION: $HASURA_VERSION" '.github/workflows/test.yml')
-
-if [ -z "$CI_VERSION_IS_THE_SAME" ]
-then
-	echo "--> Error: Version in .github/workflows/test.yml mismatches specified version in hasura/.hasura_version"
-	exit 2
-else
-	echo "--> Version in .github/workflows/test.yml and specified version in hasura/.hasura_version are consistent"
 fi
