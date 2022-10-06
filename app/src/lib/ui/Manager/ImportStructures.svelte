777032ed4ea1d990b975ea703623df7ade1e0459
LionelB <lionel@lumographe.fr>
2022-10-06 19:35:49 +0200

    feat(structure): use python api to import structure

--- app/src/lib/ui/Manager/ImportStructures.svelte
+++ app/src/lib/ui/Manager/ImportStructures.svelte
@@ -1,237 +1,134 @@
-<script context="module" lang="ts">
-	import { ImportStructureDocument } from '$lib/graphql/_gen/typed-document-nodes';
-	import type {
-		AdminStructureInput,
-		ImportStructureMutation,
-		ImportStructureMutationVariables,
-		StructureInput,
-	} from '$lib/graphql/_gen/typed-document-nodes';
-	import { operationStore, mutation } from '@urql/svelte';
-	import type { OperationStore } from '@urql/svelte';
-</script>
-
 <script lang="ts">
+	import { session } from '$app/stores';
 	import Dropzone from 'svelte-file-dropzone';
-	import { Checkbox, GroupCheckbox } from '$lib/ui/base';
-	import { Text, ImportParserError } from '$lib/ui/utils';
-	import { Alert, Button } from '$lib/ui/base';
+	import { Button, Checkbox, GroupCheckbox } from '$lib/ui/base';
+	import { Text } from '$lib/ui/utils';
+	import { Alert } from '$lib/ui/base';
+	import { v4 as uuidv4 } from 'uuid';
 	import { pluralize } from '$lib/helpers';
-	import { parseEntities } from '$lib/utils/importFileParser';
 
-	type StructureWithAdminInput = StructureInput & AdminStructureInput;
-
-	type StructureImport = StructureWithAdminInput & {
-		valid: boolean;
-		uid: string;
-	};
-
-	let forceUpdate = false;
 	let sendAccountEmail = false;
+	let parsePromise;
+	let insertPromise;
+	let structurestoImport = [];
 
-	let files = [];
-	let structures: StructureImport[] = [];
-
-	let parseErrors = [];
-
-	$: structuresToImport = structures.filter(({ uid }) => toImport.includes(uid));
-
-	let toImport = [];
-
-	function handleFilesSelect(event: CustomEvent<{ acceptedFiles: Buffer[] }>): void {
-		files = event.detail.acceptedFiles;
-		for (let i = 0; i < files.length; i++) {
-			parseEntities(
-				files[i],
-				'StructureImport',
-				headers,
-				({ entities, idToImport }: Record<string, unknown>, errors: string[]): void => {
-					structures = entities as StructureImport[];
-					toImport = idToImport as string[];
-					parseErrors = errors;
-				}
-			);
+	export function translateError(error = ''): string {
+		if (/none is not an allowed value/.test(error)) {
+			return `Champ obligatoire manquant`;
 		}
+		if (/not a valid email address/.test(error)) {
+			return `Format d'adresse mail invalide`;
+		}
+		if (/not a valid siret/.test(error)) {
+			return `Numéro siret invalide`;
+		}
+		if (/not a valid phone/.test(error)) {
+			return `Numéro de téléphone invalide`;
+		}
+		if (/not a valid postal code/.test(error)) {
+			return `Code postal invalide`;
+		}
+		if (/URL/.test(error)) {
+			return `Format d'url invalide`;
+		}
+		if (/insert structure failed/.test(error)) {
+			return 'Création de la structure impossible';
+		}
+		if (/insert structure admin failed/.test(error)) {
+			return "Création de l'admin de structure impossible";
+		}
+		if (/add admin structure to structure failed"/.test(error)) {
+			return "Ajout de l'admin à la structure impossible";
+		}
+		console.error(error);
+		return `Une erreur s'est produite lors de la lecture du fichier.`;
 	}
 
-	const insertStore: OperationStore<
-		ImportStructureMutation,
-		ImportStructureMutationVariables,
-		StructureWithAdminInput
-	> = operationStore(ImportStructureDocument, null, { additionalTypenames: ['structure'] });
-	const inserter = mutation(insertStore);
-	let insertInProgress = false;
+	function handleFilesSelect(event: CustomEvent<{ acceptedFiles: FileList }>): void {
+		const file = event.detail.acceptedFiles[0];
+		const formData = new FormData();
+		formData.append('upload_file', file);
+		parsePromise = fetch(`${$session.backendAPI}/v1/convert-file/structures`, {
+			method: 'POST',
+			headers: {
+				'jwt-token': $session.token,
+				Accept: 'application/json; version=1.0',
+			},
+			body: formData,
+		})
+			.then(async (response) => {
+				if (response.ok) {
+					return response.json();
+				}
+				const errorMessage = await response.text();
+				console.error(errorMessage);
+				return Promise.reject(
+					new Error(
+						`api call failed (${response.status} - ${response.statusText})\n${errorMessage}`
+					)
+				);
+			})
+			.then((structures) => {
+				return structures.map((structure) => {
+					if (structure.valid) {
+						const uuid = uuidv4();
+						structurestoImport.push(uuid);
+						return { uuid, ...structure };
+					}
+					return { ...structure };
+				});
+			});
+	}
 
-	let insertResult: {
-		struct: StructureWithAdminInput;
-		error: string | null;
-	}[];
-
-	async function handleSubmit() {
-		insertInProgress = true;
-		insertResult = [];
-		for (const structure of structuresToImport) {
-			const { uid, valid, ...struct } = structure;
-			const result = await inserter({ ...struct, forceUpdate, sendAccountEmail });
-			let errorMessage = "Une erreur s'est produite, la structure n'a pas été importée.";
-			if (/admin_structure_structure relation failed/i.test(result.error?.message)) {
-				errorMessage =
-					"Une erreur s'est produite, le rattachement de l’admin de structure à échoué.";
+	async function handleSubmit(structures) {
+		console.log('insert', structures);
+		insertPromise = fetch(`${$session.backendAPI}/v1/structures/import`, {
+			method: 'POST',
+			headers: {
+				'jwt-token': $session.token,
+				Accept: 'application/json; version=1.0',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ structures: structures.map(({ data }) => data), sendAccountEmail }),
+		}).then(async (response) => {
+			if (response.ok) {
+				return response.json();
 			}
-			if (/uniqueness/i.test(result.error?.message)) {
-				errorMessage = 'Cette structure existe déjà.';
-			}
+			const errorMessage = await response.text();
+			console.error(errorMessage);
+			return Promise.reject(
+				new Error(`api call failed (${response.status} - ${response.statusText})\n${errorMessage}`)
+			);
+		});
+	}
 
-			insertResult = [
-				...insertResult,
-				{
-					struct,
-					...(result.error && { error: errorMessage }),
-				},
-			];
-		}
-		insertInProgress = false;
+	function backToFileSelect() {
+		structurestoImport = [];
+		parsePromise = [];
 	}
 
 	const headers = [
-		{ label: 'Nom*', key: 'name' },
-		{ label: 'Description', key: 'shortDesc' },
-		{ label: 'Téléphone', key: 'phone' },
-		{ label: 'Adresse', key: 'address1' },
-		{ label: 'Adresse (complément)', key: 'address2' },
-		{ label: 'Code postal* ', key: 'postalCode' },
-		{ label: 'Ville*', key: 'city' },
-		{ label: 'Site web', key: 'website' },
-		{ label: 'Courriel', key: 'email' },
-		{ label: 'SIRET', key: 'siret' },
-		{ label: "Courriel de l'administrateur*", key: 'adminEmail' },
-		{ label: 'Prénom', key: 'firstname' },
-		{ label: 'Nom', key: 'lastname' },
-		{ label: 'Fonction', key: 'position' },
-		{ label: 'Numéros de téléphone', key: 'phoneNumbers' },
+		{ label: 'Nom', csvName: 'Nom', mandatory: true },
+		{ label: 'Description', csvName: 'Description', mandatory: false },
+		{ label: 'Téléphones', csvName: 'Téléphones', mandatory: false },
+		{ label: 'Adresse', csvName: 'Adresse', mandatory: false },
+		{ label: 'Adresse (complément)', csvName: 'Adresse (complément)', mandatory: false },
+		{ label: 'Code postal', csvName: 'Code postal', mandatory: true },
+		{ label: 'Ville', csvName: 'Ville', mandatory: true },
+		{ label: 'Site web', csvName: 'Site web', mandatory: false },
+		{ label: 'Courriel', csvName: 'Courriel', mandatory: false },
+		{ label: 'Siret', csvName: 'Siret', mandatory: false },
+		{ label: 'Courriel responsable', csvName: 'Courriel responsable', mandatory: true },
+		{ label: 'Prénom', csvName: 'Prénom responsable', mandatory: false },
+		{ label: 'Nom', csvName: 'Nom responsable', mandatory: false },
+		{ label: 'Fonction', csvName: 'Fonction responsable', mandatory: false },
+		{ label: 'Numéros de téléphone', csvName: 'Téléphones responsable', mandatory: false },
 	];
-
-	function backToFileSelect() {
-		structures = [];
-		parseErrors = [];
-	}
-
-	$: successfulImports = (insertResult || []).filter(({ error }) => !error).length;
 </script>
 
 <div class="flex flex-col gap-6">
-	{#if insertResult === undefined}
-		{#if structures.length > 0}
-			<div class="border-b border-gray-200 shadow" style="overflow-x: auto;">
-				<table class="w-full divide-y divide-gray-300">
-					<thead class="px-2 py-2">
-						<th />
-						{#each headers as { label } (label)}
-							<th>{label}</th>
-						{/each}
-					</thead>
-					<tbody class="bg-white divide-y divide-gray-300">
-						{#each structures as structure}
-							<tr>
-								<td class="align-middle">
-									{#if structure.valid}
-										<GroupCheckbox
-											classNames="bottom-3 left-2"
-											bind:selectedOptions={toImport}
-											groupId={'toImport'}
-											option={{ name: structure.uid, label: '' }}
-											disabled={!structure.valid}
-											title={`${
-												toImport.includes(structure.uid) ? 'Ne pas importer' : 'Importer'
-											} la structure`}
-										/>
-									{:else}
-										<i
-											class="ri-alert-line text-error relative left-3"
-											title="La structure ne contient pas les informations obligatoires (marquées d'un astérisque)"
-										/>
-									{/if}
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.name} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.city} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.postalCode} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.address1} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.address2} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.phone} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.email} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.website} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.siret} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.shortDesc} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.adminEmail} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.firstname} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.lastname} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.position} />
-								</td>
-								<td class="px-2 py-2">
-									<Text value={structure.phoneNumbers} />
-								</td>
-							</tr>
-						{/each}
-					</tbody>
-				</table>
-			</div>
-			<ImportParserError {parseErrors} />
-			<p>
-				Vous allez importer les structures suivantes. Veuillez vérifier que les données sont
-				correctes et confirmer. Il est possible de mettre plusieurs fois la même structure pour
-				ajouter plusieurs administrateurs à une structure (et une seule structure sera crée).
-			</p>
-			<p>
-				<Checkbox
-					name="forceUpdate"
-					label="Écraser les informations des structures existantes. Dans le cas ou il y aurait plusieurs fois la même structure, seules les informations de la dernière ligne seront conservées."
-					bind:checked={forceUpdate}
-				/>
-				<Checkbox
-					name="sendConfirmEmail"
-					label="Envoyer un email de creation de compte aux nouveaux gestionnaires de structure"
-					bind:checked={sendAccountEmail}
-				/>
-			</p>
-			<div class="mt-6 flex justify-end flex-row gap-4">
-				<span>
-					{toImport.length || 'Aucune'}
-					{pluralize('structure', toImport.length)}
-					{pluralize('sélectionnée', toImport.length)}
-					sur {structures.length}
-				</span>
-			</div>
-			<div class="mt-6 flex justify-end flex-row gap-4">
-				<Button on:click={backToFileSelect} outline={true}>Retour</Button>
-				<Button on:click={handleSubmit} disabled={toImport.length < 1}>Confirmer</Button>
-			</div>
-		{:else}
+	{#if insertPromise === undefined}
+		{#if parsePromise === undefined}
 			<div>
 				Veuillez fournir un fichier au format EXCEL ou CSV.
 				<br />Vous pouvez
@@ -244,69 +141,157 @@
 			<Dropzone on:drop={handleFilesSelect} multiple={false} accept=".csv,.xls,.xlsx">
 				Déposez votre fichier ou cliquez pour le rechercher sur votre ordinateur.
 			</Dropzone>
-			<ImportParserError {parseErrors} />
-		{/if}
-	{:else}
-		<div class="flex flex-col gap-4">
-			{#if insertInProgress}
-				<Alert
-					type="info"
-					title={`Ajout ${pluralize('de la', toImport.length, 'des')} ${pluralize(
-						'structure',
-						toImport.length
-					)} en cours... ${insertResult.length}/${toImport.length}`}
-				/>
-			{:else}
-				<Alert
-					type={successfulImports ? 'success' : 'error'}
-					title={`${successfulImports || 'Aucune'}
-					${pluralize('structure', successfulImports)}
-					${pluralize('importée', successfulImports)}
-					sur ${toImport.length}
-					${pluralize('demandée', toImport.length)}.`}
-				/>
-			{/if}
-			{#key insertResult}
-				<div class="border-b border-gray-200 shadow">
-					<table class="w-full divide-y divide-gray-300">
-						<thead class="px-2 py-2">
-							<th>Nom</th>
-							<th>Ville</th>
-							<th>Résultat</th>
-						</thead>
-						<tbody class="bg-white divide-y divide-gray-300">
-							{#each insertResult as structure}
-								<tr>
-									<td class="px-2 py-2 ">
-										<Text value={structure.struct.name} />
-									</td>
-									<td class="px-2 py-2 ">
-										<Text value={structure.struct.city} />
-									</td>
-									<td class="px-2 py-2 ">
-										{#if structure.error}
-											<Text classNames="text-error" value={structure.error} />
+		{:else}
+			{#await parsePromise}
+				<Alert type="info" title={`Lecture du fichier en cours...`} />
+			{:then parsedStructures}
+				<p>
+					Vous allez importer les professionnels suivants. Veuillez vérifier que les données sont
+					correctes et confirmer.
+				</p>
+				<table class="w-full divide-y divide-gray-300">
+					<caption class="sr-only">Récapitulatif des structures à importer</caption>
+					<thead class="px-2 py-2">
+						<th />
+						{#each headers as { label }}
+							<th>{label}</th>
+						{/each}
+					</thead>
+					<tbody class="bg-white divide-y divide-gray-300">
+						{#each parsedStructures as structure}
+							{@const lineErrors = Object.fromEntries(
+								structure.errors?.map(({ key, error }) => [key, error]) || []
+							)}
+							<tr>
+								<td class="align-middle">
+									{#if structure.valid}
+										<GroupCheckbox
+											classNames="bottom-3 left-2"
+											bind:selectedOptions={structurestoImport}
+											groupId="structure_import"
+											option={{ name: structure.uuid, label: '' }}
+											title={`${
+												structurestoImport.includes(structure.uuid) ? 'Ne pas importer' : 'Importer'
+											} la structure`}
+										/>
+									{:else}
+										<i
+											class="ri-alert-line text-error relative left-3"
+											title="La structure ne contient pas les informations obligatoires (marquées d'un astérisque)"
+										/>
+									{/if}
+								</td>
+								{#each headers as { csvName }}
+									<td class="px-2 py-2">
+										{#if structure.valid}
+											<Text value={structure.data[csvName]} />
+										{:else if lineErrors[csvName]}
+											<p
+												class="text-error border-dashed border-b-1"
+												title={translateError(lineErrors[csvName])}
+											>
+												<Text value={structure.row[csvName]} />
+											</p>
 										{:else}
-											<span
-												class="fr-icon-success-fill text-success"
-												aria-hidden="true"
-												style="margin: 0 50%;"
-											/>
+											<Text value={structure.row[csvName]} />
 										{/if}
 									</td>
-								</tr>
-							{/each}
-							{#if insertInProgress}
-								<tr>
-									<td colspan="3">
-										<i class="ri-loader-2-fill" style="margin: 0 50%;" />
-									</td>
-								</tr>
-							{/if}
-						</tbody>
-					</table>
+								{/each}
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+				<Checkbox
+					name="sendAccountEmail"
+					label="Envoyer un email de creation de compte aux nouveaux gestionnaires de structure"
+					bind:checked={sendAccountEmail}
+				/>
+				<div class="mt-6 flex justify-end flex-row gap-4">
+					<span>
+						{structurestoImport.length || 'Aucun'}
+						{pluralize('structure', structurestoImport.length)}
+						{pluralize('sélectionnée', structurestoImport.length)}
+						sur {parsedStructures.length}
+					</span>
 				</div>
-			{/key}
-		</div>
+				<div class="mt-6 flex justify-end flex-row gap-4">
+					<Button on:click={backToFileSelect} outline={true}>Retour</Button>
+					<Button
+						on:click={() =>
+							handleSubmit(
+								parsedStructures.filter(({ uuid }) => structurestoImport.includes(uuid))
+							)}
+						disabled={structurestoImport.length < 1}
+						>Confirmer
+					</Button>
+				</div>
+			{:catch error}
+				<Alert type="error" title="lecture du fichier impossible, veuillez contacter le support." />
+				<details>
+					<summary>voir le detail</summary>
+					<pre>{error.message}</pre>
+				</details>
+			{/await}
+		{/if}
+	{:else}
+		{#await insertPromise}
+			<Alert type="info" title={`Création des structures en cours...`} />
+		{:then insertResults}
+			{@const nbStructureInserted = insertResults.filter(({ valid }) => valid).length}
+
+			<Alert
+				type={nbStructureInserted ? 'success' : 'error'}
+				title={`${nbStructureInserted || 'Aucun'}
+					${pluralize('structure', nbStructureInserted)}
+					${pluralize('importée', nbStructureInserted)}
+					sur ${insertResults.length}.`}
+			/>
+			<table class="w-full divide-y divide-gray-300">
+				<caption class="sr-only">Récapitulatif des structures crée</caption>
+				<thead class="px-2 py-2">
+					<th class="text-left">Nom</th>
+					<th class="text-left">Email du Responsable</th>
+					<th />
+				</thead>
+				<tbody class="bg-white divide-y divide-gray-300">
+					{#each insertResults as structure}
+						{@const lineErrors = structure.errors?.map(({ error }) => error) || []}
+						<tr>
+							<td class="px-2 py-2">
+								{#if structure.valid}
+									<Text value={structure.data['Nom']} />
+								{:else}
+									<Text value={structure.row['Nom']} />
+								{/if}
+							</td>
+							<td class="px-2 py-2">
+								{#if structure.valid}
+									<Text value={structure.data['Courriel responsable']} />
+								{:else}
+									<Text value={structure.row['Courriel responsable']} />
+								{/if}
+							</td>
+							<td class="px-2 py-2 ">
+								{#if structure.valid === false}
+									<Text classNames="text-error" value={translateError(lineErrors.join(','))} />
+								{:else}
+									<span
+										class="fr-icon-success-fill text-success"
+										aria-hidden="true"
+										style="margin: 0 50%;"
+									/>
+								{/if}
+							</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		{:catch error}
+			<Alert type="error" title="import du fichier impossible, veuillez contacter le support." />
+			<details>
+				<summary>voir le detail</summary>
+				<pre>{error.message}</pre>
+			</details>
+		{/await}
 	{/if}
 </div>
