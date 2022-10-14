557f2fe0cafc30f1cf3e20bde99f53fbb32784b6
Lionel <lionel.breduillieard@beta.gouv.fr>
2022-10-14 14:38:34 +0200

    fix(data): recupération correcte du dernier fichier PE (#1176)
    * fix(data): recupération du dernier fichier
    
    * fix: review

--- backend/scripts/pe-fetch.sh
+++ backend/scripts/pe-fetch.sh
@@ -15,7 +15,7 @@ chmod 600 *.pem
 echo "download files."
 # -1rt = one column, reverse order, time sort
 # tail -1 is used in case there are multiple files with the same name (I know, it's weird but it happened)
-$MAIN_FILE=$(echo "ls -1rt" | sftp -o "StrictHostKeyChecking accept-new"  -i ./pe_server.pem $PE_SERVER_URL:/OI33SPIE | grep principal | tail -1)
+MAIN_FILE=$(echo "ls -1rt" | sftp -o "StrictHostKeyChecking accept-new"  -i ./pe_server.pem $PE_SERVER_URL:/OI33SPIE | grep principal | tail -1)
 sftp -i ./pe_server.pem $PE_SERVER_URL:/OI33SPIE/$MAIN_FILE .
 
 # Processing of the "actions" file is disabled for now because it is too large to be decrypted
@@ -23,7 +23,7 @@ sftp -i ./pe_server.pem $PE_SERVER_URL:/OI33SPIE/$MAIN_FILE .
 
 # sftp -i ./pe_server.pem $PE_SERVER_URL:/OI33SPIE/actions .
 
-openssl smime -decrypt -in principal -binary -inform DEM -inkey pe_file.pem -out principal.csv
+openssl smime -decrypt -in $MAIN_FILE -binary -inform DEM -inkey pe_file.pem -out principal.csv
 # openssl smime -decrypt -in actions -binary -inform DEM -inkey pe_file.pem -out actions.csv
 
 echo "fichier principal: $(wc -l principal.csv) lignes"
