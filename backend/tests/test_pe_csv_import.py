a3613beedd0d4fab8324d2ff6aae3dcbd7cc4d72
Vincent Jousse <vincent@jousse.org>
2022-10-18 11:43:03 +0200

    test: add import_pe_referent test

--- backend/tests/test_pe_csv_import.py
+++ backend/tests/test_pe_csv_import.py
@@ -5,6 +5,7 @@ from uuid import UUID
 import httpx
 import respx
 from asyncpg.connection import Connection
+from dask.dataframe.core import DataFrame
 
 from api.core.settings import settings
 from api.db.crud.beneficiary import get_beneficiary_by_id
@@ -20,10 +21,12 @@ from api.db.crud.structure import get_structures
 from api.db.models.beneficiary import Beneficiary
 from api.db.models.external_data import ExternalSource
 from api.db.models.notebook import NotebookMember
+from api.db.models.professional import Professional
 from api.db.models.structure import Structure
 from cdb_csv.models.csv_row import PrincipalCsvRow
 from cdb_csv.pe import (
     import_beneficiaries,
+    import_pe_referent,
     insert_wanted_jobs_for_csv_row_and_notebook,
     map_principal_row,
 )
@@ -202,3 +205,25 @@ async def test_parse_principal_csv_exception(
         await import_beneficiaries(db_connection, pe_principal_csv_filepath)
 
         assert "Exception while processing CSV line: Parsing exception" in caplog.text
+
+
+async def test_import_pe_referent(
+    db_connection: Connection, pe_principal_csv_series: DataFrame
+):
+    iterator = pe_principal_csv_series.iterrows()
+    for i, (_, row_content) in enumerate(iterator):
+        # Get the 4th element
+        if i < 3:
+            continue
+
+        csv_row: PrincipalCsvRow = await map_principal_row(row_content)
+
+        professional: Professional | None = await import_pe_referent(
+            db_connection,
+            csv_row,
+            csv_row.identifiant_unique_de,
+            UUID("4dab8036-a86e-4d5f-9bd4-6ce88c1940d0"),
+            UUID("9c0e5236-a03d-4e5f-b945-c6bc556cf9f3"),
+        )
+
+        assert professional is not None
