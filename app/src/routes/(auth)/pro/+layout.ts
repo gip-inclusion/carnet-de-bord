9a0700bbb17836f65c7f98313e18a87567050ef9
Guillaume Lagorce <guillaume@hey-gul.com>
2022-10-21 16:19:30 -0400

    fix: do not redirect on /moncompte if url is already /moncompte

--- app/src/routes/(auth)/pro/+layout.ts
+++ app/src/routes/(auth)/pro/+layout.ts
@@ -4,7 +4,14 @@ import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async ({ parent, url }) => {
 	const parentData = await parent();
-	if (parentData.account && !parentData.account.onboardingDone && !/bienvenue/.test(url.pathname)) {
+	if (
+		parentData.account &&
+		!parentData.account.onboardingDone &&
+		!/bienvenue/.test(url.pathname) &&
+		!/moncompte/.test(url.pathname)
+	) {
+		console.log({ parentData, url });
+		console.count('pro/layout');
 		throw redirect(302, `${baseUrlForRole(parentData.account.type)}/moncompte`);
 	}
 };
