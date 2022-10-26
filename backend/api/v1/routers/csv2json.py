e919b36f01e7576d50dd9a022139cdfb8be8a57f
Benoit Chiquet <benoit.chiquet@beta.gouv.fr>
2022-10-26 11:25:20 +0200

    fix: make sure a nir was provided

--- backend/api/v1/routers/csv2json.py
+++ backend/api/v1/routers/csv2json.py
@@ -155,16 +155,17 @@ def parse_date(field: str):
 
 
 def nir_format(field: str):
-    nir = field.strip()
-    if len(nir) != 15:
-        return "The NIR must be 15 digit long"
-    if not all([char.isdigit() for char in nir]):
-        return "The NIR cannot contain letters"
-    payload = int(nir[:13])
-    check = int(nir[13:])
-    valid = 97 - payload % 97 == check
-    if not valid:
-        return "The NIR provided has invalid format"
+    if field:
+        nir = field.strip()
+        if len(nir) != 15:
+            return "The NIR must be 15 digit long"
+        if not all([char.isdigit() for char in nir]):
+            return "The NIR cannot contain letters"
+        payload = int(nir[:13])
+        check = int(nir[13:])
+        valid = 97 - payload % 97 == check
+        if not valid:
+            return "The NIR provided has invalid format"
 
 
 async def file_to_json(
