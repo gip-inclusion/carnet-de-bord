e3a77e53f78c12f5e1334650ecbff09be530f202
Lionel <lionel.breduillieard@beta.gouv.fr>
2022-11-09 17:35:12 +0100

    fix: redirection après connexion (#1225)
    Co-authored-by: Jonathan Perret <jonathan.perret@beta.gouv.fr>

--- app/src/routes/auth/jwt/[uuid]/+error.svelte
+++ app/src/routes/auth/jwt/[uuid]/+error.svelte
@@ -6,7 +6,7 @@
 	<title>Validation du token de connexion - Carnet de bord</title>
 </svelte:head>
 
-<div class="pt-28 flex flex-col items-center">
+<div class="pt-28 flex flex-col grow items-center">
 	<p class="pb-12 text-xl">Désolé, ce lien n'est plus valide...</p>
 	<Link classNames="fr-btn" href="/auth/login">Accéder à la page de connexion</Link>
 </div>
---
+++ app/src/routes/auth/login/+layout.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { Video } from '$lib/ui/base';
+
+	const videoOptions = {
+		sources: [
+			/**
+			 * Video with neither subtitles nor voiceover
+			 * {
+			 *   url: 'https://nextcloud.fabrique.social.gouv.fr/s/TtM2A7KkD55MqZ3/download/intro-cdb.mp4',
+			 *   type: 'video/mp4',
+			 * },
+			 */
+
+			/**
+			 * Video with voiceover
+			 */
+			{
+				url: 'https://nextcloud.fabrique.social.gouv.fr/s/KjFZxq4CFE76fyD/download/intro-cdb-voiceover.mp4',
+				type: 'video/mp4',
+			},
+
+			/**
+			 * Video with embedded subtitles
+			 * {
+			 *   url: 'https://nextcloud.fabrique.social.gouv.fr/s/Nx3wbt6LTMrNqGE/download/intro-cdb-with-subs.mp4',
+			 *   type: 'video/mp4',
+			 * },
+			 */
+		],
+		tracks: [
+			{
+				url: '/videos/intro-cdb.websrt',
+				label: 'Français',
+				lang: 'fr',
+			},
+		],
+		title: 'Découvrez Carnet de bord en vidéo !',
+		preload: 'none',
+		hideControls: true,
+		poster: '/videos/intro-cdb.png',
+	};
+</script>
+
+<div class="w-1/2 py-20 pr-10">
+	<Video {...videoOptions} />
+</div>
+<div class="w-1/2 py-20 pl-10">
+	<slot />
+</div>
