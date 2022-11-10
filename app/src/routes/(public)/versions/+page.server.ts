14da8783629fbd94ade3698691f39a9e4ae71e00
Jonathan Perret <jonathan.perret@beta.gouv.fr>
2022-11-10 22:47:23 +0100

    fix: attempt to copy CHANGELOG.md from root directory
    This is a quick and dirty attempt, mainly to check if it works on
    Scalingo. It would be much better to process the changelog at build
    time.

--- app/src/routes/(public)/versions/+page.server.ts
+++ app/src/routes/(public)/versions/+page.server.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-const changelogContent = fs.readFileSync('../CHANGELOG.md', { encoding: 'utf-8' });
+const changelogContent = fs.readFileSync('CHANGELOG.md', { encoding: 'utf-8' });
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
