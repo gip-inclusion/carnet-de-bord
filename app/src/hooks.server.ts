81cdb820b1d3c5088f5674003a680161e1d0b155
Guillaume Lagorce <guillaume@hey-gul.com>
2022-11-10 09:50:57 -0500

    chore: add request logging

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -10,21 +10,35 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 export const handle: Handle = async ({ event, resolve }) => {
-	return resolve(event, {
+	const requestStartTime = Date.now();
+
+	const response = await resolve(event, {
 		// this is needed to make it work on server side request
 		// in order to include Content-type header
 		// @see https://kit.svelte.dev/docs/hooks#server-hooks-handle
 		filterSerializedResponseHeaders: (name) => name.toLowerCase().startsWith('content'),
 	});
+
+	logger.info({
+		startTime: new Date(requestStartTime).toISOString(),
+		event: 'response',
+		method: event.request.method,
+		url: event.url,
+		duration: `${Date.now() - requestStartTime}ms`,
+		status: response.status,
+	});
+	return response;
 };
 
 export const handleError: HandleServerError = ({ error, event }) => {
 	const err = error as Error;
 	logger.error({
+		event: 'error',
 		err: {
 			message: err?.message ?? 'unknow error',
 			stack: err?.stack ?? null,
 		},
-		event,
+		method: event.request.method,
+		url: event.url,
 	});
 };
