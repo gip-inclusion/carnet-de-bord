6137145a29c765b4fb82208f12e3c72a3c0c77ed
LionelB <lionel@lumographe.fr>
2022-11-17 08:56:00 +0100

    fix: add role attribute to spinner

--- app/src/lib/ui/base/Spinner.svelte
+++ app/src/lib/ui/base/Spinner.svelte
@@ -2,7 +2,7 @@
 	export let label = 'Chargement...';
 </script>
 
-<div class={`flex items-center gap-2 text-gray-500 ${$$props.class}`}>
+<div class={`flex items-center gap-2 text-gray-500 ${$$props.class}`} role="status">
 	<span class="h-8 w-8 block rounded-full border-4 border-t-france-blue-500 animate-spin" />
 	{label}
 </div>
