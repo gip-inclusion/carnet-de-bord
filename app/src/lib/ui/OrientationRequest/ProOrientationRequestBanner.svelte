058bba23ede0f386308e57f94b037d11b7a881b1
Benoit Chiquet <benoit.chiquet@beta.gouv.fr>
2022-11-23 17:28:00 +0100

    feat: make notebook banner different for different reorientation outcomes

---
+++ app/src/lib/ui/OrientationRequest/ProOrientationRequestBanner.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { formatDateLocale } from '$lib/utils/date';
+
+	export let reorientationRequest;
+</script>
+
+<div class="flex flex-row mb-8 items-center">
+	<div class="text-france-blue fr-icon-info-fill" />
+	<div class="fr-container">
+		{#if reorientationRequest.status == 'pending'}
+			<div class="fr-text--bold text-france-blue">
+				Demande de réorientation envoyée le {formatDateLocale(reorientationRequest.createdAt)}
+			</div>
+		{:else if reorientationRequest.status == 'accepted'}
+			<div class="fr-text--bold text-france-green">
+				Demande de réorientation acceptée le {formatDateLocale(reorientationRequest.decidedAt)}
+			</div>
+		{:else if reorientationRequest.status == 'denied'}
+			<div class="fr-text--bold text-france-red">
+				Demande de réorientation refusée le {formatDateLocale(reorientationRequest.decidedAt)}
+			</div>
+		{/if}
+		<div>
+			Orientation recommandée : {reorientationRequest.requestedOrientationType.label}
+		</div>
+	</div>
+</div>
