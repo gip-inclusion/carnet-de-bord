bd1b34698f4228b18ff5a6a2e3670500ccf4b068
Vincent Jousse <vincent@jousse.org>
2022-11-28 16:34:57 +0100

    feat: don't import actions twice

--- backend/api/db/crud/notebook_event.py
+++ backend/api/db/crud/notebook_event.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from uuid import UUID
 
 from asyncpg import Record
@@ -35,6 +36,27 @@ def create_notebook_event(
     }
 
 
+async def get_notebook_event_pe(
+    connection: Connection, notebook_id: UUID, label: str, date: datetime
+) -> NotebookEvent | None:
+
+    record = await connection.fetchrow(
+        """
+        SELECT * from public.notebook_event WHERE notebook_id=$1
+        AND event->>'from'=$2
+        AND event->>'event_label'=$3
+        AND event_date=$4
+        """,
+        notebook_id,
+        EventFrom.pe,
+        label,
+        date,
+    )
+
+    if record:
+        return parse_notebook_event(record)
+
+
 async def insert_notebook_event(
     connection: Connection, notebook_event: NotebookEventInsert
 ) -> NotebookEvent | None:
@@ -66,4 +88,4 @@ async def get_notebook_event_by_id(
     )
 
     if record:
-        return NotebookEvent.parse_obj(record)
+        return parse_notebook_event(record)
