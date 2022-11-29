58296e398b06da8a654a193d9f9a77595f7864a9
LionelB <lionel@lumographe.fr>
2022-11-29 11:52:34 +0100

    fixup! feat: update orientation request from backend call
    fix: update crud

--- backend/api/db/crud/orientation_request.py
+++ backend/api/db/crud/orientation_request.py
@@ -12,11 +12,9 @@ logger = logging.getLogger(__name__)
 async def get_orientation_request_by_id(
     connection: Connection, orientation_request_id: UUID
 ) -> OrientationRequest | None:
-    orientation_requests = await connection.fetch(
+    orientation_request: Record = await connection.fetchrow(
         "SELECT * FROM orientation_request WHERE id = $1", orientation_request_id
     )
 
-    if orientation_requests[0] is None:
-        return None
-
-    return OrientationRequest.parse_obj(orientation_requests[0])
+    if orientation_request:
+        return OrientationRequest.parse_obj(orientation_request)
