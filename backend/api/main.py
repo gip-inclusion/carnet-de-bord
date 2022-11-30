db7c62cda845432e2f8bd45b96fbdce477afad1b
Guillaume Lagorce <guillaume@hey-gul.com>
2022-11-30 17:56:52 -0500

    chore: add python-json-logger

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,6 +1,17 @@
+import logging
+
 import sentry_sdk
+from pythonjsonlogger import jsonlogger
 
 from api.core.init import create_app
+from api.core.settings import settings
+
+logging.basicConfig(level=settings.LOG_LEVEL, format=settings.LOG_FORMAT)
+logger = logging.getLogger()
+logHandler = logging.StreamHandler()
+formatter = jsonlogger.JsonFormatter()
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
 
 sentry_sdk.init(attach_stacktrace=True)
 
