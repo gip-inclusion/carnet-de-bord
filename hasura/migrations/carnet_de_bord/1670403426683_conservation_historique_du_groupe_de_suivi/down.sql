bcd6f82c327113a0e3ac813347fa78c19df89657
Benoit Chiquet <benoit.chiquet@beta.gouv.fr>
2022-12-07 10:18:10 +0100

    feat: add end of notebook membership column

---
+++ hasura/migrations/carnet_de_bord/1670403426683_conservation_historique_du_groupe_de_suivi/down.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "public"."notebook_member"
+  DROP COLUMN membership_ends_at;
+
+DROP INDEX "notebook_member_notebook_id_account_id_if_active";
+
+ALTER TABLE ONLY public.notebook_member
+    ADD CONSTRAINT "notebook_member_notebook_id_account_id_key" UNIQUE (notebook_id, account_id);
---
+++ hasura/migrations/carnet_de_bord/1670403426683_conservation_historique_du_groupe_de_suivi/up.sql
@@ -0,0 +1,9 @@
+ALTER TABLE "public"."notebook_member"
+  ADD membership_ends_at date;
+
+ALTER TABLE ONLY "public"."notebook_member"
+  DROP CONSTRAINT "notebook_member_notebook_id_account_id_key";
+
+CREATE UNIQUE INDEX "notebook_member_notebook_id_account_id_if_active"
+  ON "public"."notebook_member" (notebook_id, account_id)
+  WHERE (active);
