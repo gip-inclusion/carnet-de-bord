a444a685f8b7c7428762a69cf0ae3f83d316a42a
Benoit Chiquet <benoit.chiquet@beta.gouv.fr>
2023-01-03 16:05:04 +0100

    feat: add targets for numerique theme

--- hasura/migrations/carnet_de_bord/1672741051729_migrate-add-numerique-theme/down.sql
+++ hasura/migrations/carnet_de_bord/1672741051729_migrate-add-numerique-theme/down.sql
@@ -2,3 +2,13 @@ update ref_situation set theme = 'difficulte_administrative'
 where description = 'Absence d''adresse de messagerie'
 or description = 'Absence d''équipement ou de connexion'
 or description = 'Accès ou utilisation difficile des outils numériques';
+
+with to_delete(description, theme) as (VALUES
+  ('Travailler l’accès à une prestation', 'numerique'),
+  ('Accompagnement dans les démarches numériques', 'numerique'),
+  ('Mise en place d''une mesure d''accompagnement adapté', 'numerique'))
+delete from ref_target where exists
+  (select * from to_delete
+   where ref_target.description = to_delete.description
+   and ref_target.theme = to_delete.theme
+  )
