0ef7709acf8031db4a37796bddaa6c9f5badc962
Guillaume Lagorce <guillaume@hey-gul.com>
2023-01-04 18:39:25 -0500

    chore: remove extra table

--- app/src/lib/ui/Beneficiary/NotebookMembers.svelte
+++ app/src/lib/ui/Beneficiary/NotebookMembers.svelte
@@ -70,47 +70,45 @@
 	{/if}
 
 	<table>
-		<table>
-			<thead>
-				<tr>
-					<th style="width: 40%">Structure</th>
-					<th style="width: 30%">Accompagnateur</th>
-					<th style="width: 30%">Fonction</th>
+		<thead>
+			<tr>
+				<th style="width: 40%">Structure</th>
+				<th style="width: 30%">Accompagnateur</th>
+				<th style="width: 30%">Fonction</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each members.filter(({ account }) => account.type === RoleEnum.Professional) as member}
+				<tr class:font-bold={member.memberType === 'referent'}>
+					<td>
+						<Text value={member.account?.professional.structure.name} />
+					</td>
+					<td>
+						<div class="flex flex-row gap-2">
+							<Text value={member.account?.professional.firstname} />
+							<Text value={member.account?.professional.lastname} />
+							{#if member.memberType === 'referent'}
+								(référent)
+							{/if}
+						</div>
+					</td>
+					<td>
+						<Text value={member.account?.professional.position} />
+					</td>
 				</tr>
-			</thead>
-			<tbody>
-				{#each members.filter(({ account }) => account.type === RoleEnum.Professional) as member}
-					<tr class:font-bold={member.memberType === 'referent'}>
-						<td>
-							<Text value={member.account?.professional.structure.name} />
-						</td>
-						<td>
-							<div class="flex flex-row gap-2">
-								<Text value={member.account?.professional.firstname} />
-								<Text value={member.account?.professional.lastname} />
-								{#if member.memberType === 'referent'}
-									(référent)
-								{/if}
-							</div>
-						</td>
-						<td>
-							<Text value={member.account?.professional.position} />
-						</td>
-					</tr>
-				{/each}
-				{#each members.filter(({ account }) => account.type === RoleEnum.OrientationManager) as member}
-					<tr class:font-bold={member.memberType === 'referent'}>
-						<td />
-						<td>
-							<div class="flex flex-row gap-2">
-								<Text value={member.account?.orientation_manager.firstname} />
-								<Text value={member.account?.orientation_manager.lastname} />
-							</div>
-						</td>
-						<td>Chargé d'orientation</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
+			{/each}
+			{#each members.filter(({ account }) => account.type === RoleEnum.OrientationManager) as member}
+				<tr class:font-bold={member.memberType === 'referent'}>
+					<td />
+					<td>
+						<div class="flex flex-row gap-2">
+							<Text value={member.account?.orientation_manager.firstname} />
+							<Text value={member.account?.orientation_manager.lastname} />
+						</div>
+					</td>
+					<td>Chargé d'orientation</td>
+				</tr>
+			{/each}
+		</tbody>
 	</table>
 </div>
