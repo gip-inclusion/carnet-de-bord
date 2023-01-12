451a5498e1e734bdf5fd78a0aa8a1140a23d1180
Guillaume Lagorce <guillaume@hey-gul.com>
2023-01-12 10:57:11 -0500

    chore: adjust max-width of the form

--- app/src/lib/ui/StructureEdit/StructureCreationForm.svelte
+++ app/src/lib/ui/StructureEdit/StructureCreationForm.svelte
@@ -22,33 +22,35 @@
 	let:isSubmitted
 	let:isValid
 >
-	<Input placeholder="Pole insertion" inputLabel="Nom" name="name" />
-	<Input placeholder="service d'insertion" inputLabel="Description" name="shortDesc" />
-	<Input placeholder="agence@cd08.fr" inputLabel="Courriel" name="email" />
-	<Input
-		placeholder=""
-		inputLabel="Téléphones"
-		name="phone"
-		class="max-w-max"
-		inputHint="Si plusieurs téléphones, utiliser une virgule pour les séparer"
-	/>
-	<Input placeholder="" inputLabel="Siret" name="siret" class="max-w-max" />
-	<Input placeholder="" inputLabel="Adresse" name="address1" />
-	<Input placeholder="10 rue des mésanges" inputLabel="Complément d'adresse" name="address2" />
-	<div class="fr-grid-row fr-grid-row--gutters fr-grid-row--top">
+	<div class="max-w-lg">
+		<Input placeholder="Pole insertion" inputLabel="Nom" name="name" />
+		<Input placeholder="service d'insertion" inputLabel="Description" name="shortDesc" />
+		<Input placeholder="agence@cd08.fr" inputLabel="Courriel" name="email" />
 		<Input
-			class="fr-col-3 max-w-max"
-			inputLabel="Code postal"
-			placeholder="75008"
-			name="postalCode"
+			placeholder=""
+			inputLabel="Téléphones"
+			name="phone"
+			inputHint="Si plusieurs téléphones, utiliser une virgule pour les séparer"
 		/>
-		<Input class="fr-col-9" inputLabel="Ville" placeholder="Paris" name="city" />
-	</div>
-	<Input placeholder="https://monsite.url" inputLabel="Site web" name="website" />
+		<Input placeholder="" inputLabel="Siret" name="siret" />
+		<Input placeholder="" inputLabel="Adresse" name="address1" />
+		<Input placeholder="10 rue des mésanges" inputLabel="Complément d'adresse" name="address2" />
+		<div class="fr-grid-row fr-grid-row--gutters fr-grid-row--top">
+			<Input
+				class="fr-col-3 max-w-max"
+				inputLabel="Code postal"
+				placeholder="75008"
+				name="postalCode"
+			/>
+			<Input class="fr-col-9" inputLabel="Ville" placeholder="Paris" name="city" />
+		</div>
+		<Input placeholder="https://monsite.url" inputLabel="Site web" name="website" />
 
-	<div class="flex flex-row gap-6 mt-12">
-		<Button type="submit" disabled={(isSubmitted && !isValid) || isSubmitting}>{submitLabel}</Button
-		>
-		{#if onCancel}<Button outline={true} on:click={onCancel}>Annuler</Button>{/if}
+		<div class="flex flex-row gap-6 mt-12">
+			<Button type="submit" disabled={(isSubmitted && !isValid) || isSubmitting}
+				>{submitLabel}</Button
+			>
+			{#if onCancel}<Button outline={true} on:click={onCancel}>Annuler</Button>{/if}
+		</div>
 	</div>
 </Form>
