d0398d77110e4ec976a09511ef8b980a7ab33efc
Jonathan Perret <jonathan.perret@beta.gouv.fr>
2023-01-23 19:56:50 +0100

    fix: do not create token without invalidating access key

--- app/src/routes/auth/jwt/[uuid]/+page.svelte
+++ app/src/routes/auth/jwt/[uuid]/+page.svelte
@@ -1,26 +1,10 @@
 <script lang="ts">
-	import { Button, Spinner } from '$lib/ui/base';
-
-	import type { PageServerData } from './$types';
-	export let data: PageServerData;
-
-	$: isLoading = false;
-
-	async function login() {
-		const body = new FormData();
-		body.set('token', data.token);
-		body.set('accountId', data.accountId);
-		await fetch(`/auth/jwt/${data.accessKey}`, { method: 'POST', body });
-
-		window.location.replace(data.redirectAfterLogin ?? '/');
-	}
+	import { Button } from '$lib/ui/base';
 </script>
 
 <div class="pt-28 flex flex-col grow items-center">
-	<p class="pb-12 text-xl">Votre lien magique est valide.</p>
-	{#if isLoading}
-		<Spinner />
-	{:else}
-		<Button on:click={login} title="Continuer">Continuer sur Carnet de bord</Button>
-	{/if}
+	<p class="pb-12 text-xl">Votre lien de connexion est valide.</p>
+	<form method="POST">
+		<Button title="Continuer" type="submit">Continuer sur Carnet de bord</Button>
+	</form>
 </div>
