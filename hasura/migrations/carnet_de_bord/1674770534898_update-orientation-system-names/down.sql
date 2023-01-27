268bc8e4224cb8c90caf65d2799ca47fd4b1f7cd
GUL <guillaume.lagorce@beta.gouv.fr>
2023-01-27 08:25:12 -0500

    fix: Mise à jour des dispostifs d'orientation (Pro -> Professionnel et Socio-pro -> Socio-professionnel). (#1479)

---
+++ hasura/migrations/carnet_de_bord/1674770534898_update-orientation-system-names/down.sql
@@ -0,0 +1,7 @@
+UPDATE orientation_system
+SET name='Pro'
+WHERE name='Professionnel';
+
+UPDATE orientation_system
+SET name='Socio-pro'
+WHERE name='socio-professionnel';
---
+++ hasura/migrations/carnet_de_bord/1674770534898_update-orientation-system-names/up.sql
@@ -0,0 +1,7 @@
+UPDATE orientation_system
+SET name='Professionnel'
+WHERE name='Pro';
+
+UPDATE orientation_system
+SET name='Socio-professionnel'
+WHERE name='Socio-pro';
