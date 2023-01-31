ef8c3e4381a9116a6d015ade5a298f2402f65708
GUL <guillaume.lagorce@beta.gouv.fr>
2023-01-31 13:30:24 -0500

    fix: Ne pas créer de token d'accès sans invalider l'access key. (#1478)
    fix: do not create token without invalidating access key
    
    Co-authored-by: Jonathan Perret <jonathan.perret@beta.gouv.fr>

--- app/src/routes/auth/jwt/[uuid]/+page.server.ts
+++ app/src/routes/auth/jwt/[uuid]/+page.server.ts
@@ -1,4 +1,5 @@
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
+import type { Actions, RequestEvent } from '@sveltejs/kit';
 import { createJwt } from '$lib/utils/getJwt';
 
 import { createClient } from '@urql/core';
@@ -22,24 +23,7 @@ const client = createClient({
 	requestPolicy: 'network-only',
 });
 
-export const actions = {
-	default: async ({ request, cookies, setHeaders }) => {
-		const data = await request.formData();
-		const accountId = data.get('accountId');
-		const token = data.get('token');
-
-		await client.mutation(ResetAccountAccessKeyDocument, { id: accountId }).toPromise();
-
-		cookies.set('jwt', token, { path: '/', httpOnly: true, sameSite: 'strict' });
-
-		setHeaders({ 'Cache-Control': 'private' });
-	},
-};
-
-export const load: PageServerLoad = async ({ url, params }) => {
-	const redirectAfterLogin = url.searchParams.get('url');
-	const accessKey = params.uuid;
-
+async function lookupAccessKey(accessKey: string): Promise<GetAccountInfoQuery['account'][0]> {
 	const getAccountResult = await client
 		.query<GetAccountInfoQuery>(GetAccountInfoDocument, { accessKey })
 		.toPromise();
@@ -55,48 +39,76 @@ export const load: PageServerLoad = async ({ url, params }) => {
 		throw error(401, `no account for key ${accessKey}`);
 	}
 
-	const {
-		id,
-		type,
-		username,
-		beneficiaryId,
-		managerId,
-		professionalId,
-		adminStructureId,
-		orientationManagerId,
-		professional,
-		manager,
-		adminStructure,
-		orientationManager,
-		beneficiary,
-	} = getAccountResult.data.account[0];
-	let deploymentId = null;
-	let structureId = null;
-	if (professional) {
-		deploymentId = professional.structure.deploymentId;
-		structureId = professional.structure.id;
-	} else if (manager) {
-		deploymentId = manager.deploymentId;
-	} else if (adminStructure) {
-		deploymentId = adminStructure.deploymentId;
-	} else if (orientationManager) {
-		deploymentId = orientationManager.deploymentId;
-	} else if (beneficiary) {
-		deploymentId = beneficiary.deploymentId;
-	}
+	return getAccountResult.data.account[0];
+}
 
-	const token = createJwt({
-		id,
-		type,
-		username,
-		professionalId,
-		managerId,
-		beneficiaryId,
-		deploymentId,
-		adminStructureId,
-		orientationManagerId,
-		structureId,
-	});
+export const actions: Actions = {
+	default: async ({ url, params, cookies, setHeaders }: RequestEvent) => {
+		const redirectAfterLogin = url.searchParams.get('url');
 
-	return { redirectAfterLogin, accessKey, token, accountId: id };
+		const account = await lookupAccessKey(params.uuid);
+
+		const {
+			id,
+			type,
+			username,
+			beneficiaryId,
+			managerId,
+			professionalId,
+			adminStructureId,
+			orientationManagerId,
+			professional,
+			manager,
+			adminStructure,
+			orientationManager,
+			beneficiary,
+		} = account;
+		let deploymentId = null;
+		let structureId = null;
+		if (professional) {
+			deploymentId = professional.structure.deploymentId;
+			structureId = professional.structure.id;
+		} else if (manager) {
+			deploymentId = manager.deploymentId;
+		} else if (adminStructure) {
+			deploymentId = adminStructure.deploymentId;
+		} else if (orientationManager) {
+			deploymentId = orientationManager.deploymentId;
+		} else if (beneficiary) {
+			deploymentId = beneficiary.deploymentId;
+		}
+
+		const token = createJwt({
+			id,
+			type,
+			username,
+			professionalId,
+			managerId,
+			beneficiaryId,
+			deploymentId,
+			adminStructureId,
+			orientationManagerId,
+			structureId,
+		});
+
+		await client
+			.mutation(ResetAccountAccessKeyDocument, {
+				id,
+			})
+			.toPromise();
+
+		cookies.set('jwt', token, {
+			path: '/',
+			httpOnly: true,
+			sameSite: 'strict',
+		});
+
+		setHeaders({ 'Cache-Control': 'private' });
+
+		throw redirect(303, redirectAfterLogin ?? '/');
+	},
+};
+
+export const load: PageServerLoad = async ({ params }: RequestEvent) => {
+	await lookupAccessKey(params.uuid);
 };
