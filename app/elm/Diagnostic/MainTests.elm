f2eee0680e10ee4109d40be36b5bbb47484cd9e7
Guillaume Lagorce <guillaume@hey-gul.com>
2023-02-07 07:55:28 -0500

    chore: add unit tests for beneficiaryRights

--- app/elm/Diagnostic/MainTests.elm
+++ app/elm/Diagnostic/MainTests.elm
@@ -1,7 +1,7 @@
 module Diagnostic.MainTests exposing (..)
 
 import Date
-import Diagnostic.Main exposing (workSituationDateFormat)
+import Diagnostic.Main exposing (beneficiaryRights, workSituationDateFormat)
 import Expect exposing (..)
 import Html
 import Html.Attributes exposing (..)
@@ -46,3 +46,27 @@ workSituationDateFormatTests =
                         |> Query.has [ text "Du 22/08/2021 au 22/08/2023", tag "span", classes [ "italic", "font-bold" ], text "(24 mois)" ]
             ]
         ]
+
+
+beneficiaryRightsTests : Test
+beneficiaryRightsTests =
+    describe "beneficiaryRights" <|
+        List.map
+            (\{ are, ass, bonus, testDescription, expected } ->
+                describe testDescription <|
+                    [ test "returns rights description" <|
+                        \_ ->
+                            beneficiaryRights are ass bonus
+                                |> Query.fromHtml
+                                |> Query.has [ text expected ]
+                    ]
+            )
+            [ { are = False, ass = False, bonus = False, testDescription = "when there are no rights", expected = "" }
+            , { are = True, ass = False, bonus = False, testDescription = "when there is only ARE", expected = "ARE" }
+            , { are = False, ass = True, bonus = False, testDescription = "when there is only ASS", expected = "ASS" }
+            , { are = False, ass = False, bonus = True, testDescription = "when there is only bonus", expected = "Prime d'activité" }
+            , { are = True, ass = True, bonus = False, testDescription = "when there are ARE and ASS", expected = "ARE, ASS" }
+            , { are = True, ass = False, bonus = True, testDescription = "when there are ARE and bonus", expected = "ARE, Prime d'activité" }
+            , { are = False, ass = True, bonus = True, testDescription = "when there are ASS and bonus", expected = "ASS, Prime d'activité" }
+            , { are = True, ass = True, bonus = True, testDescription = "when there are ARE, ASS and bonus", expected = "ARE, ASS, Prime d'activité" }
+            ]
