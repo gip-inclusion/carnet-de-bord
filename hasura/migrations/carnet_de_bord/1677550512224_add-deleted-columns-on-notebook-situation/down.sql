be73d1984936a41f073a88f1886f880dfb33d2bd
Guillaume Lagorce <guillaume@hey-gul.com>
2023-03-01 08:01:36 -0500

    fixup! feat: add deleted_at and deleted_by columns to soft delete situations

--- hasura/migrations/carnet_de_bord/1677550512224_add-deleted-columns-on-notebook-situation/down.sql
+++ hasura/migrations/carnet_de_bord/1677550512224_add-deleted-columns-on-notebook-situation/down.sql
@@ -1,5 +1,6 @@
 alter table "public"."notebook_situation" drop constraint "notebook_situation_notebook_id_situation_id_deleted_at_key";
 alter table "public"."notebook_situation" add constraint "notebook_situation_notebook_id_situation_id_key" unique ("notebook_id", "situation_id");
 
+alter table "public"."notebook_situation" drop constraint "notebook_situation_deleted_by_fkey";
 alter table "public"."notebook_situation" drop column "deleted_by";
 alter table "public"."notebook_situation" drop column "deleted_at";
