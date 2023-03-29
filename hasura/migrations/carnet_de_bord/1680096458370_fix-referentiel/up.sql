0045865ee23805d7ddb46245ad90718defc26733
LionelB <lionel@lumographe.fr>
2023-03-29 18:06:21 +0200

    fix: fix typo in referentiel
    add missing theme to action without theme
    fix typo in some target label
    fix #1590

---
+++ hasura/migrations/carnet_de_bord/1680096458370_fix-referentiel/up.sql
@@ -0,0 +1,3 @@
+UPDATE "public"."ref_action" SET  theme= 'mobilite' where description='Constitution d’une demande d''aide financière pour l’achat d’un véhicule';
+UPDATE "public"."ref_action" SET (description,theme)= ('Réalisation d''une demande de mesure de protection administrative ou juridique','sante') where description='Réalisation d''une demande de mesure de protection administrative ou juridique"';
+UPDATE "public"."ref_target" SET description = 'Accéder à des service en ligne' WHERE theme='numerique' AND description='Accéder a des service en ligne';
