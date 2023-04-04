3ab95b2422099356b92b44db8e6cb803dbffd41c
Vincent Jousse <vincent@jousse.org>
2023-04-04 17:55:01 +0200

    fix: bad down migration

--- hasura/migrations/carnet_de_bord/1680094738881_add_deleted_to_professionnal_project/down.sql
+++ hasura/migrations/carnet_de_bord/1680094738881_add_deleted_to_professionnal_project/down.sql
@@ -1,9 +1,6 @@
 
-alter table "public"."professional_project" drop constraint "professional_project_deleted_by_fkey";
 alter table "public"."professional_project" drop constraint "professional_project_created_by_fkey";
 alter table "public"."professional_project" drop constraint "professional_project_updated_by_fkey";
 
 alter table "public"."professional_project" drop column "created_by";
 alter table "public"."professional_project" drop column "updated_by";
-alter table "public"."professional_project" drop column "deleted_by";
-alter table "public"."professional_project" drop column "deleted_at";
