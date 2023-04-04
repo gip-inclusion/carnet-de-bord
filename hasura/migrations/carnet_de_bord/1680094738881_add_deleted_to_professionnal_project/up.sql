97e79bba5064e95db7ce296523e614527322daae
Vincent Jousse <vincent@jousse.org>
2023-04-04 17:50:53 +0200

    fix: rename history to audit

--- hasura/migrations/carnet_de_bord/1680094738881_add_deleted_to_professionnal_project/up.sql
+++ hasura/migrations/carnet_de_bord/1680094738881_add_deleted_to_professionnal_project/up.sql
@@ -14,7 +14,7 @@ alter table "public"."professional_project"
   references "public"."account"
   ("id") on update set null on delete set null;
 
-CREATE TABLE history (
+CREATE TABLE audit (
   "id" uuid NOT NULL DEFAULT gen_random_uuid(),
   "created_at" timestamptz NOT NULL DEFAULT now(),
   "schema_name" text NOT NULL,
@@ -38,23 +38,23 @@ BEGIN
   END IF;
   IF      TG_OP = 'INSERT'
   THEN
-    INSERT INTO history (table_name, schema_name, operation, new_val, created_by)
+    INSERT INTO audit (table_name, schema_name, operation, new_val, created_by)
       VALUES (TG_RELNAME, TG_TABLE_SCHEMA, TG_OP, row_to_json(NEW), creator_id);
     RETURN NEW;
   ELSIF   TG_OP = 'UPDATE'
   THEN
-    INSERT INTO history (table_name, schema_name, operation, new_val, old_val, created_by)
+    INSERT INTO audit (table_name, schema_name, operation, new_val, old_val, created_by)
       VALUES (TG_RELNAME, TG_TABLE_SCHEMA, TG_OP, row_to_json(NEW), row_to_json(OLD), creator_id);
     RETURN NEW;
   ELSIF   TG_OP = 'DELETE'
   THEN
-    INSERT INTO history (table_name, schema_name, operation, old_val, created_by)
+    INSERT INTO audit (table_name, schema_name, operation, old_val, created_by)
       VALUES (TG_RELNAME, TG_TABLE_SCHEMA, TG_OP, row_to_json(OLD), creator_id);
     RETURN OLD;
   END IF;
 END;
 $$ LANGUAGE 'plpgsql' SECURITY DEFINER;
 
-CREATE TRIGGER t
+CREATE TRIGGER log_audit
 AFTER INSERT OR UPDATE OR DELETE ON professional_project
   FOR EACH ROW EXECUTE PROCEDURE change_trigger();
