568367907c2907c4f7bf7e757fdb51aa30fea0ca
Jonathan Perret <jonathan.perret@beta.gouv.fr>
2023-04-24 17:09:04 +0200

    feat: get jwt from cookie

--- app/src/routes/graphql/+server.ts
+++ app/src/routes/graphql/+server.ts
@@ -3,9 +3,10 @@ import { getRealGraphqlAPI } from '$lib/config/variables/private';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const POST = (async ({ request }) => {
+export const POST = (async ({ request, cookies }) => {
 	const body = await request.text();
-	const authorization = request.headers.get('Authorization');
+	const jwt = cookies.get('jwt');
+	const authorization = `Bearer ${jwt}`;
 	const url = getRealGraphqlAPI();
 	const response = await fetch(url, {
 		method: 'POST',
