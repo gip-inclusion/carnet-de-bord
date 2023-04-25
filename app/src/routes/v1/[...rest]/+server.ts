a6c98fddda2e803af6afb2604c307600af1476fd
LionelB <lionel@lumographe.fr>
2023-04-25 12:11:35 +0200

    fix: utiliser blob pour forwarder le body vers le backend

--- app/src/routes/v1/[...rest]/+server.ts
+++ app/src/routes/v1/[...rest]/+server.ts
@@ -4,9 +4,8 @@ import type { RequestHandler } from './$types';
 
 export const POST = (async ({ request, cookies, params }) => {
 	const url = `${getBackendAPI()}/v1/${params.rest}`;
-	const body = await request.text();
+	const body = await request.blob();
 	const jwt = cookies.get('jwt');
-
 	return fetch(url, {
 		method: 'POST',
 		body,
@@ -14,7 +13,9 @@ export const POST = (async ({ request, cookies, params }) => {
 			'Content-Type': request.headers.get('content-type'),
 			accept: request.headers.get('accept'),
 			'jwt-token': jwt,
-			'Content-Disposition': request.headers.get('content-disposition') ?? null,
+			...(request.headers.get('content-disposition')
+				? { 'Content-Disposition': request.headers.get('content-disposition') }
+				: null),
 		},
 	});
 }) satisfies RequestHandler;
