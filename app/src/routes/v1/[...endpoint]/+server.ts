26c192cf0f1b2af515da2acd8f4a584a91e81df0
Jonathan Perret <jonathan.perret@beta.gouv.fr>
2023-04-26 16:52:11 +0200

    feat: support des envois de gros fichiers

--- app/src/routes/v1/[...endpoint]/+server.ts
+++ app/src/routes/v1/[...endpoint]/+server.ts
@@ -3,12 +3,13 @@ import { getBackendAPI } from '$lib/config/variables/private';
 import type { RequestHandler } from './$types';
 
 export const POST = (async ({ request, cookies, params }) => {
-	const url = `${getBackendAPI()}/v1/${params.rest}`;
-	const body = await request.blob();
+	const url = `${getBackendAPI()}/v1/${params.endpoint}`;
+	const body = request.body;
 	const jwt = cookies.get('jwt');
 	return fetch(url, {
 		method: 'POST',
 		body,
+		duplex: 'half',
 		headers: {
 			'Content-Type': request.headers.get('content-type'),
 			accept: request.headers.get('accept'),
@@ -17,5 +18,8 @@ export const POST = (async ({ request, cookies, params }) => {
 				? { 'Content-Disposition': request.headers.get('content-disposition') }
 				: null),
 		},
-	});
+		// A cast to RequestInit is required because TypeScript's idea of fetch is outdated
+		// and does not know about "duplex", while the "fetch" polyfill provided by
+		// SvelteKit requires "duplex" to be set when providing the body as a ReadableStream.
+	} as RequestInit);
 }) satisfies RequestHandler;
