3939e8d9b8d5162be15e7f0020ac93a13bee42c2
Guillaume Lagorce <guillaume@hey-gul.com>
2023-05-02 16:38:40 -0400

    fix: ensure there is no username/password in CSP URLs

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,6 +1,16 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-node';
 import { resolve } from 'path';
+
+function formatURLForCsp(originalUrl) {
+	if (!originalUrl) return;
+	const url = new URL(originalUrl);
+	url.username = '';
+	url.password = '';
+
+	return url.href;
+}
+
 const config = {
 	kit: {
 		env: {
@@ -18,6 +28,7 @@ const config = {
 					'self',
 					...(process.env.NODE_ENV === 'production' ? [''] : ['unsafe-eval']),
 					'https://client.crisp.chat/',
+					...(process.env.PUBLIC_MATOMO_URL ? [process.env.PUBLIC_MATOMO_URL] : []),
 				],
 				'connect-src': [
 					'self',
@@ -25,14 +36,18 @@ const config = {
 					...(process.env.NODE_ENV === 'production' ? [] : ['ws:', 'http:']),
 					'wss://client.relay.crisp.chat/',
 					'https://client.crisp.chat/static/',
-					// Note that in development, PUBLIC_MATOMO_URL(or PUBLIC_SENTRY_DSN) will not be read from .env
-					// (because dotenv has not been loaded at this point), you have to set it
-					// in the environment explicitly, e.g. `PUBLIC_MATOMO_URL=... npm run dev`
-					process.env?.GRAPHQL_API_URL,
-					process.env?.BACKEND_API_URL,
-					process.env?.PUBLIC_MATOMO_URL,
-					process.env?.PUBLIC_SENTRY_DSN,
-				].filter(Boolean),
+					...[
+						// Note that in development, environment variables will not be read from .env
+						// (because dotenv has not been loaded at this point), you have to set it
+						// in the environment explicitly, e.g. `PUBLIC_MATOMO_URL=... npm run dev`
+						// or use shdotenv script, e.g. `./scripts/shdotenv make start-app`
+						process.env.GRAPHQL_API_URL,
+						process.env.BACKEND_API_URL,
+						process.env.PUBLIC_SENTRY_DSN,
+					]
+						.map(formatURLForCsp)
+						.filter(Boolean),
+				],
 			},
 		},
 		adapter: adapter({ precompress: true }),
