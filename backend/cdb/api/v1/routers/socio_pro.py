d9342886ef50736cbf5dda8371bb2ab6ef656b2b
Vincent Jousse <vincent@jousse.org>
2023-05-02 13:45:29 +0200

    feat: execute gql query from python backend

--- backend/cdb/api/v1/routers/socio_pro.py
+++ backend/cdb/api/v1/routers/socio_pro.py
@@ -1,10 +1,13 @@
+import json
 from typing import Annotated, List
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, Header, Request
+from fastapi import APIRouter, Depends, Header
+from gql import Client, gql
+from gql.transport.aiohttp import AIOHTTPTransport
 from pydantic import BaseModel, Field
 
-from cdb.api.core.init import connection
+from cdb.api.core.settings import settings
 from cdb.api.v1.dependencies import verify_secret_token
 
 router = APIRouter(dependencies=[Depends(verify_secret_token)])
@@ -21,18 +24,43 @@ class ProfessionalProjectCommon(BaseModel):
     mobilityRadius: str | None
     romeCodeId: UUID | None
 
+    def gql_variables(self):
+        return {
+            "contractTypeId": self.contractTypeId,
+            "employmentTypeId": self.employmentTypeId,
+            "hourlyRate": self.employmentTypeId,
+            "mobilityRadius": self.mobilityRadius,
+            "romeCodeId": str(self.romeCodeId) if self.romeCodeId is not None else None,
+        }
+
 
 class ProfessionalProjectToAdd(ProfessionalProjectCommon):
     notebookId: UUID
+
+    def gql_variables(self):
+        return super().gql_variables() | {
+            "notebookId": str(self.notebookId),
+        }
 
 
 class ProfessionalProjectToUpdate(ProfessionalProjectCommon):
     id: UUID
 
+    def gql_variables(self):
+        return super().gql_variables() | {
+            "id": str(self.id),
+        }
+
 
 class SituationsToAdd(BaseModel):
     notebookId: UUID
     situationId: UUID
+
+    def gql_variables(self):
+        return {
+            "notebookId": str(self.notebookId),
+            "situationId": str(self.situationId),
+        }
 
 
 class Input(BaseModel):
@@ -58,19 +86,135 @@ class SessionVariables(BaseModel):
     x_hasura_user_id: str = Field(..., alias="x-hasura-user-id")
 
 
+def remove_none_from_dict(dictionary):
+    for key, value in list(dictionary.items()):
+        if value is None:
+            del dictionary[key]
+        elif isinstance(value, dict):
+            remove_none_from_dict(value)
+        elif isinstance(value, list):
+            for item in value:
+                if isinstance(item, dict):
+                    remove_none_from_dict(item)
+
+    return dictionary
+
+
 class UpdateSocioProMutation(BaseModel):
     action: Action
     input: Input
     request_query: str
     session_variables: SessionVariables
 
+    def gql_variables(self):
+        return {
+            "id": str(self.input.id),
+            "workSituation": self.input.workSituation,
+            "workSituationDate": self.input.workSituationDate,
+            "workSituationEndDate": self.input.workSituationEndDate,
+            "rightRqth": self.input.rightRqth,
+            "educationLevel": self.input.educationLevel,
+            "lastJobEndedAt": self.input.lastJobEndedAt,
+            "professionalProjectIdsToDelete": [
+                p.gql_variables() for p in self.input.professionalProjectIdsToDelete
+            ],
+            "professionalProjectsToAdd": [
+                p.gql_variables() for p in self.input.professionalProjectsToAdd
+            ],
+            "professionalProjectsToUpdate": [
+                p.gql_variables() for p in self.input.professionalProjectsToUpdate
+            ],
+            "situationsToAdd": [p.gql_variables() for p in self.input.situationsToAdd],
+            "situationIdsToDelete": [
+                p.gql_variables() for p in self.input.situationIdsToDelete
+            ],
+        }
+
+
+deny_orientation_gql = gql(
+    """
+mutation UpdateSocioPro(
+  $id: uuid!
+  $workSituation: String
+  $workSituationDate: date
+  $workSituationEndDate: date
+  $rightRqth: Boolean
+  $educationLevel: String
+  $lastJobEndedAt: date
+  $professionalProjectIdsToDelete: [uuid!]!
+  $professionalProjectsToAdd: [professional_project_insert_input!]!
+  $professionalProjectsToUpdate: [professional_project_updates!]!
+  $situationsToAdd: [notebook_situation_insert_input!]!
+  $situationIdsToDelete: [uuid!]!
+) {
+  update: update_notebook_by_pk(
+    pk_columns: { id: $id }
+    _set: {
+      workSituation: $workSituation
+      workSituationDate: $workSituationDate
+      workSituationEndDate: $workSituationEndDate
+      rightRqth: $rightRqth
+      educationLevel: $educationLevel
+      lastJobEndedAt: $lastJobEndedAt
+    }
+  ) {
+    id
+  }
+  delete_professional_project(where: { id: { _in: $professionalProjectIdsToDelete } }) {
+    affected_rows
+  }
+  update_professional_project_many(updates: $professionalProjectsToUpdate) {
+    affected_rows
+  }
+  insert_professional_project(objects: $professionalProjectsToAdd) {
+    affected_rows
+  }
+  update_notebook_situation(
+    where: {
+      _and: [
+        { notebookId: { _eq: $id } }
+        { situationId: { _in: $situationIdsToDelete } }
+        { deletedAt: { _is_null: true } }
+      ]
+    }
+  ) {
+    affected_rows
+  }
+  insert_notebook_situation(objects: $situationsToAdd) {
+    affected_rows
+  }
+}
+"""
+)
+
 
 @router.post("/update")
 async def update(
-    request: Request,
     mutation: UpdateSocioProMutation,
     authorization: Annotated[str | None, Header()] = None,
-    db=Depends(connection),
 ):
+    """
+    Update the socio pro information of a notebook.
+    It will create a notebook_event that will keep track of the modifications performed
+    """
+
     print(authorization)
     print(mutation)
+
+    transport = AIOHTTPTransport(
+        url=settings.graphql_api_url,
+        headers={"Authorization": authorization},
+    )
+
+    async with Client(
+        transport=transport, fetch_schema_from_transport=False, serialize_variables=True
+    ) as session:
+        variables = remove_none_from_dict(mutation.gql_variables())
+        print(json.dumps(variables))
+
+        update_socio_pro_response = await session.execute(
+            deny_orientation_gql,
+            variable_values=variables,
+        )
+
+        print(update_socio_pro_response)
