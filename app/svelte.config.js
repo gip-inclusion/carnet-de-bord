f966a55be8512eaa128264cf272e814894d36aa2
Jonathan Perret <jonathan.perret@beta.gouv.fr>
2023-05-03 19:52:30 +0200

    Gestion des variables d'environnement dynamiques dans la Content-Security-Policy (#1719)

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -2,15 +2,6 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-node';
 import { resolve } from 'path';
 
-function formatURLForCsp(originalUrl) {
-	if (!originalUrl) return;
-	const url = new URL(originalUrl);
-	url.username = '';
-	url.password = '';
-
-	return url.href;
-}
-
 const config = {
 	kit: {
 		env: {
@@ -18,17 +9,20 @@ const config = {
 		},
 		csp: {
 			mode: 'auto',
+			// The %…% delimited identifiers in these directives are processed by the 'handle' hook
+			// in hooks.server.ts to expand environment variables at runtime
 			directives: {
 				'base-uri': ['self'],
 				'default-src': ['self'],
 				'font-src': ['self', 'https://client.crisp.chat/static/'],
 				'img-src': ['self', 'data:', 'https://*.crisp.chat/'],
 				'style-src': ['self', '*.crisp.chat', 'unsafe-inline'],
+				'media-src': ['self', 'nextcloud.fabrique.social.gouv.fr'],
 				'script-src': [
 					'self',
 					...(process.env.NODE_ENV === 'production' ? [''] : ['unsafe-eval']),
 					'https://client.crisp.chat/',
-					...(process.env.PUBLIC_MATOMO_URL ? [process.env.PUBLIC_MATOMO_URL] : []),
+					'%PUBLIC_MATOMO_URL%',
 				],
 				'connect-src': [
 					'self',
@@ -36,17 +30,10 @@ const config = {
 					...(process.env.NODE_ENV === 'production' ? [] : ['ws:', 'http:']),
 					'wss://client.relay.crisp.chat/',
 					'https://client.crisp.chat/static/',
-					...[
-						// Note that in development, environment variables will not be read from .env
-						// (because dotenv has not been loaded at this point), you have to set it
-						// in the environment explicitly, e.g. `PUBLIC_MATOMO_URL=... npm run dev`
-						// or use shdotenv script, e.g. `./scripts/shdotenv make start-app`
-						process.env.GRAPHQL_API_URL,
-						process.env.BACKEND_API_URL,
-						process.env.PUBLIC_SENTRY_DSN,
-					]
-						.map(formatURLForCsp)
-						.filter(Boolean),
+					'%GRAPHQL_API_URL%',
+					'%BACKEND_API_URL%',
+					'%PUBLIC_MATOMO_URL%',
+					'%PUBLIC_SENTRY_DSN%',
 				],
 			},
 		},
