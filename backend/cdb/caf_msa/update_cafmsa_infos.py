36d16c82e2f25815841a57ce03e7403aac4ae7f9
Lionel <lionel.breduillieard@beta.gouv.fr>
2023-05-03 15:05:02 +0200

    chore: Améliore le logging de l'import de fichiers CAF/MSA. (#1714)
    Co-authored-by: Guillaume Lagorce <guillaume@hey-gul.com>

--- backend/cdb/caf_msa/update_cafmsa_infos.py
+++ backend/cdb/caf_msa/update_cafmsa_infos.py
@@ -23,6 +23,8 @@ from cdb.caf_msa.parse_infos_foyer_rsa import (
     transform_cafMsaFoyer_to_beneficiary,
 )
 
+logger = logging.getLogger(__name__)
+
 
 async def update_cafmsa_for_beneficiaries(
     account_id: UUID, authorization: str, file: SpooledTemporaryFile
@@ -30,68 +32,79 @@ async def update_cafmsa_for_beneficiaries(
     count = 0
     count_error = 0
     count_success = 0
-    time.time()
-
+    time_interval = 60 * 5
     infos: CafInfoFlux | None = None
 
     async with gql_client_backend_only(bearer_token=authorization) as session:
         start_time = time.time()
-
-        parsed = parse_caf_file(file)
-        maybe_infos = next(parsed)
-        if not isinstance(maybe_infos, CafInfoFlux):
-            raise CafXMLMissingNodeException
-
-        infos = maybe_infos
-
-        for node in parsed:
-            if not isinstance(node, CafMsaInfosFoyer) or not infos:
+        last_time = time.time()
+        try:
+            parsed = parse_caf_file(file)
+            maybe_infos = next(parsed)
+            if not isinstance(maybe_infos, CafInfoFlux):
                 raise CafXMLMissingNodeException
-            for beneficiary in node.beneficiaries:
-                try:
-                    result = await save_cafmsa_infos_for_beneficiary(
-                        gql_session=session,
-                        flux_info=infos,
-                        foyer=node,
-                        caf_beneficiary=beneficiary,
-                    )
-                    if result:
-                        count_success += 1
-                except Exception as error:
-                    logging.error(
-                        "Erreur lors du traitement du dossier %s",
-                        error,
-                    )
-                    count_error += 1
-                finally:
-                    count += 1
 
-        logging.info(
-            "Mise à jour de %s/%s dossiers reçus (%s erreurs) en %s",
-            count_success,
-            count,
-            count_error,
-            timedelta(seconds=time.time() - start_time),
-        )
+            infos = maybe_infos
 
-        manager = await get_manager_by_account_id(session, account_id)
-        if manager:
-            logging.info(
-                "Notification de la fin du traitement au manager %s",
-                manager.email,
-            )
-            notify_manager_after_cafmsa_import(
-                manager.email,
-                date.today().strftime("%d/%m/%Y"),
-                count,
+            for node in parsed:
+                if not isinstance(node, CafMsaInfosFoyer) or not infos:
+                    raise CafXMLMissingNodeException
+                for beneficiary in node.beneficiaries:
+                    if (time.time() - last_time) > time_interval:
+                        logger.info(
+                            "Traitement du fichier en cours "
+                            "%s/%s dossiers traités (%s erreurs)",
+                            count_success,
+                            count,
+                            count_error,
+                        )
+                        last_time = time.time()
+                    try:
+                        result = await save_cafmsa_infos_for_beneficiary(
+                            gql_session=session,
+                            flux_info=infos,
+                            foyer=node,
+                            caf_beneficiary=beneficiary,
+                        )
+                        if result:
+                            count_success += 1
+                    except Exception as error:
+                        logger.error(
+                            "Erreur lors du traitement du dossier %s",
+                            error,
+                        )
+                        count_error += 1
+                    finally:
+                        count += 1
+
+            logger.info(
+                "Mise à jour de %s/%s dossiers reçus (%s erreurs) en %s",
                 count_success,
+                count,
                 count_error,
+                timedelta(seconds=time.time() - start_time),
             )
-        else:
-            logging.warn(
-                "manager (id=%s) introuvable",
-                account_id,
-            )
+
+            manager = await get_manager_by_account_id(session, account_id)
+            if manager:
+                logger.info(
+                    "Notification de la fin du traitement au manager %s",
+                    manager.email,
+                )
+                notify_manager_after_cafmsa_import(
+                    manager.email,
+                    date.today().strftime("%d/%m/%Y"),
+                    count,
+                    count_success,
+                    count_error,
+                )
+            else:
+                logger.warn(
+                    "manager (id=%s) introuvable",
+                    account_id,
+                )
+        except Exception as error:
+            logger.error(error)
 
 
 async def save_cafmsa_infos_for_beneficiary(
@@ -119,9 +132,8 @@ async def save_cafmsa_infos_for_beneficiary(
             sha,
             external_data_to_save,
         )
-
     else:
-        logging.info("Bénéficiaire non trouvé.", caf_beneficiary.nir)
+        logging.warning("truc", caf_beneficiary.nir)
 
 
 class CafXMLMissingNodeException(Exception):
