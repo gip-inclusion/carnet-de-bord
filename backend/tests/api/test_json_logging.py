668003dce22ca68cae2d227c4c82820306301a74
LionelB <lionel@lumographe.fr>
2023-05-10 17:59:53 +0200

    test: remove unknown key

--- backend/tests/api/test_json_logging.py
+++ backend/tests/api/test_json_logging.py
@@ -8,11 +8,10 @@ async def test_json_logging_request(
     caplog: LogCaptureFixture, test_client: AsyncClient
 ):
     with caplog.at_level(logging.INFO):
-        response = await test_client.get(
+        await test_client.get(
             "/",
         )
         assert "http" in caplog.text
         assert "network" in caplog.text
         assert "duration" in caplog.text
         assert "response" in caplog.text
-        assert response.headers["X-Process-Time"]
