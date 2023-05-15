db76cc2a3b2d07cd68df2715a82a87d08c7763e9
LionelB <lionel@lumographe.fr>
2023-05-15 22:52:06 +0200

    fix: add Accept encoding header

--- app/src/routes/v1/[...endpoint]/+server.ts
+++ app/src/routes/v1/[...endpoint]/+server.ts
@@ -16,6 +16,14 @@ export const POST = (async ({ request, cookies, params }) => {
 			...(request.headers.get('content-disposition')
 				? { 'Content-Disposition': request.headers.get('content-disposition') }
 				: null),
+			// Prevent the fetch implementation (undici most likely) from requesting
+			// compressed data from the upstream server. Because the same fetch implementation
+			// automatically uncompresses the received data, it would make work for this proxy
+			// and require us to scrub the 'Content-Encoding' header from the response before
+			// forwarding it.
+			// The Scalingo router will compress our responses for the client if necessary in
+			// any case.
+			'Accept-Encoding': 'identity',
 		},
 		// A cast to RequestInit is required because TypeScript's idea of fetch is outdated
 		// and does not know about "duplex", while the "fetch" polyfill provided by
