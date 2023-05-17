21c420a363ffd0f1b7c576a09a1213c70ea64767
LionelB <lionel@lumographe.fr>
2023-05-17 16:45:26 +0200

    perf: ajout d'un index sur notebook_id dans la table notebook_member

---
+++ hasura/migrations/carnet_de_bord/1684334642154_create_index_notebook_member_notebook_id/down.sql
@@ -0,0 +1,1 @@
+DROP INDEX IF EXISTS "public"."notebook_member_notebook_id";
---
+++ hasura/migrations/carnet_de_bord/1684334642154_create_index_notebook_member_notebook_id/up.sql
@@ -0,0 +1,2 @@
+CREATE  INDEX "notebook_member_notebook_id" on
+  "public"."notebook_member" using btree ("notebook_id");
