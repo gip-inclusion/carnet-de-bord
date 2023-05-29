e561116b1ef2e860b7634fa5936006f398f94c12
Hadrien Mens-Pellen <hadrien.mens-pellen@beta.gouv.fr>
2023-05-29 11:48:04 +0200

    fix(actions): utiliser la date de début dans l'historique de carnet (#1770)
    fix(actions): use the start date in the notebook history

---
+++ hasura/migrations/carnet_de_bord/1684934249002_actions_events_are_listed_at_the_starting_at_date/down.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION public.record_notebook_action_event()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+  new_notebook_id uuid;
+  focus_theme text;
+BEGIN
+  SELECT notebook_focus.notebook_id, notebook_focus.theme
+  INTO new_notebook_id, focus_theme
+  FROM notebook_focus, notebook_target
+  WHERE notebook_focus.id = notebook_target.focus_id
+  AND notebook_target.id = NEW.target_id;
+
+  INSERT INTO notebook_event
+  (notebook_id, event_date, creator_id, event, event_type)
+  VALUES
+  (new_notebook_id, now(), NEW.creator_id, ('{ "category": "' || focus_theme || '", "status": "' || NEW.status || '", "event_label": "' || NEW.action || '"}')::jsonb, 'action');
+  RETURN NEW;
+END;
+$function$;
