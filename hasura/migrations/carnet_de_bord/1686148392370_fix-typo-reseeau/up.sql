aab4cf3ce9c86f4740ef571a536947e621951e3e
Vincent Jousse <vincent@jousse.org>
2023-06-08 14:35:10 +0200

    fix: migrate notebook_event and notebook_target

--- hasura/migrations/carnet_de_bord/1686148392370_fix-typo-reseeau/up.sql
+++ hasura/migrations/carnet_de_bord/1686148392370_fix-typo-reseeau/up.sql
@@ -1,1 +1,3 @@
 UPDATE public.ref_target SET description='Développer son réseau' WHERE description='Développer son réseeau';
+UPDATE public.notebook_target SET target='Développer son réseau' WHERE target='Développer son réseeau';
+UPDATE public.notebook_event SET event = event::jsonb || '{"event_label":"Développer son réseau"}' WHERE event->>'event_label' = 'Développer son réseeau' AND event_type='target';
