4c2061dcd8f2494ef1177f05d14a9b003803e3c4
Jonathan Perret <jonathan.perret@beta.gouv.fr>
2023-06-08 19:18:53 +0200

    chore: utilisation de graphql-to-elm pour RomeSelect

--- app/elm/DiagnosticEdit/RomeSelect.elm
+++ app/elm/DiagnosticEdit/RomeSelect.elm
@@ -1,22 +1,14 @@
 module DiagnosticEdit.RomeSelect exposing (Model, Msg, getSelected, init, update, view)
 
+import DiagnosticEdit.SearchRomes
 import Domain.ProfessionalProject exposing (Rome)
+import Extra.GraphQL
 import Html
-import Http
-import Json.Decode as Decode
-import Json.Encode as Json
 import UI.SearchSelect.SearchSelect as SearchSelect
 
 
 
 -- Init
-
-
-decoder : Decode.Decoder SearchSelect.Option
-decoder =
-    Decode.map2 SearchSelect.Option
-        (Decode.field "id" Decode.string)
-        (Decode.field "label" Decode.string)
 
 
 type alias Model =
@@ -43,41 +35,9 @@ init props =
 
 getRome : SearchSelect.SearchApi
 getRome { search, callbackMsg } =
-    let
-        query =
-            """
-            query searchRomes($searchString: String!) {
-              rome: search_rome_codes(args: {search: $searchString}, limit: 50) {
-                id
-                label
-              }
-            }
-            """
-    in
-    Http.request
-        { method = "POST"
-        , url = "/graphql"
-        , headers = []
-        , body =
-            Http.jsonBody
-                (Json.object
-                    [ ( "query", Json.string query )
-                    , ( "variables"
-                      , Json.object
-                            [ ( "searchString", Json.string search )
-                            ]
-                      )
-                    ]
-                )
-        , expect =
-            Http.expectJson
-                callbackMsg
-                (Decode.at [ "data", "rome" ]
-                    (Decode.list decoder)
-                )
-        , timeout = Nothing
-        , tracker = Nothing
-        }
+    Extra.GraphQL.postOperationSimple
+        (DiagnosticEdit.SearchRomes.query { searchString = search })
+        (Result.map .rome_codes >> callbackMsg)
 
 
 getSelected : Model -> Maybe Rome
