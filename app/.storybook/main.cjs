0880e3a61dd884863566f1f73f0f3313c804bad0
Guillaume Lagorce <guillaume@hey-gul.com>
2023-06-14 09:14:46 -0400

    chore: use mergeConfig

--- app/.storybook/main.cjs
+++ app/.storybook/main.cjs
@@ -1,6 +1,7 @@
-const Elm = require('vite-plugin-elm');
+const { mergeConfig } = require('vite');
 const path = require('path');
 const folder = path.resolve(__dirname, './elm-storybook');
+
 module.exports = {
 	stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)', '../elm/**/*.stories.@(js|jsx|ts|tsx)'],
 	addons: [
@@ -15,19 +16,14 @@ module.exports = {
 		options: {},
 	},
 	async viteFinal(config) {
-		// Allow .elm files to be imported
-		config.plugins.push(
-			Elm.plugin({
-				debug: false,
-				optimize: false,
-			})
-		);
-
 		// Allow `elm-storybook` to be imported in *.stories.js
-		config.resolve = config.resolve || {};
-		config.resolve.alias = config.resolve.alias || {};
-		config.resolve.alias['elm-storybook'] = folder;
-		return config;
+		return mergeConfig(config, {
+			resolve: {
+				alias: {
+					'elm-storybook': folder,
+				},
+			},
+		});
 	},
 	docs: {
 		autodocs: true,
