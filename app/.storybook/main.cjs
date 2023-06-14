8fd6303bd1e29bb75fb7932894dc3d92bc428f68
Jonathan Perret <jonathan.perret@beta.gouv.fr>
2023-06-14 16:13:07 +0200

    chore: correction du build storybook
    Les plugins de build SvelteKit doivent être désactivés pendant le
    build Storybook, comme le fait le framework @storybook/sveltekit.

--- app/.storybook/main.cjs
+++ app/.storybook/main.cjs
@@ -1,6 +1,37 @@
 const { mergeConfig } = require('vite');
 const path = require('path');
-const folder = path.resolve(__dirname, './elm-storybook');
+const { plugin: elm } = require('vite-plugin-elm');
+
+/**
+ * Recursively removes all plugins with the names given
+ * Resolves async plugins
+ */
+const withoutVitePlugins = async function (plugins, namesToRemove) {
+	const result = [];
+	const resolvedPlugins = await Promise.all(plugins);
+	// eslint-disable-next-line no-restricted-syntax -- we need to await in the loop
+	for (const plugin of resolvedPlugins) {
+		if (Array.isArray(plugin)) {
+			// eslint-disable-next-line no-await-in-loop
+			result.push(await withoutVitePlugins(plugin, namesToRemove));
+		}
+		if (plugin && 'name' in plugin && !namesToRemove.includes(plugin.name)) {
+			result.push(plugin);
+		}
+	}
+	return result;
+};
+
+function ssrDisablingPlugin() {
+	return {
+		// SvelteKit sets SSR, we need it to be false when building
+		name: 'storybook:sveltekit-overrides',
+		apply: 'build',
+		config: () => {
+			return { build: { ssr: false } };
+		},
+	};
+}
 
 module.exports = {
 	stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)', '../elm/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -17,13 +48,30 @@ module.exports = {
 	},
 	async viteFinal(config) {
 		// Allow `elm-storybook` to be imported in *.stories.js
-		return mergeConfig(config, {
+		config = mergeConfig(config, {
 			resolve: {
 				alias: {
-					'elm-storybook': folder,
+					'elm-storybook': path.resolve(__dirname, './elm-storybook'),
 				},
 			},
 		});
+
+		config.plugins = [
+			...(await withoutVitePlugins(config.plugins, [
+				// disable specific plugins that are not compatible with Storybook
+				'vite-plugin-sveltekit-compile',
+				'vite-plugin-sveltekit-guard',
+				// disable Elm plugin to re-add it without 'optimize' option
+				'vite-plugin-elm',
+			])),
+			ssrDisablingPlugin(),
+			elm({
+				debug: false,
+				optimize: false,
+			}),
+		];
+
+		return config;
 	},
 	docs: {
 		autodocs: true,
