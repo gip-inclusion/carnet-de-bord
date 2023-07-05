991521a0396b66983d7bce7b955006d6fcc8047a
LionelB <lionel@lumographe.fr>
2023-07-05 15:21:34 +0200

    feat: call getVisitLog

--- app/__tests__/triggers/logNotebookVisits.test.ts
+++ app/__tests__/triggers/logNotebookVisits.test.ts
@@ -1,20 +1,29 @@
 require('isomorphic-fetch');
-import { graphqlAdmin } from '../graphql';
+import { graphql } from '../graphql';
+import { getAccountAndJwtForUser } from '../login_as';
 
 const sofieTifourNotebookId = '9b07a45e-2c7c-4f92-ae6b-bc2f5a3c9a7d';
-const pierreChevalierAccountId = '17434464-5f69-40cc-8172-40160958a33d';
+
+let accountId;
+let graphqlAsPierreChevalier: ReturnType<typeof graphql>;
+
+beforeAll(async () => {
+	const { account, token } = await getAccountAndJwtForUser('pierre.chevalier');
+	accountId = account.id;
+	graphqlAsPierreChevalier = graphql({
+		authorization: `Bearer ${token}`,
+	});
+});
 
 test('it should add a new row in the table notebook_visit', async () => {
-	updateNotebookMemberLastVisit(
-		'2023-07-05T12:50:43.669Z',
-		sofieTifourNotebookId,
-		pierreChevalierAccountId
-	);
+	updateNotebookMemberLastVisit('2023-07-05T12:50:43.669Z', sofieTifourNotebookId, accountId);
+	const logs = await getVisitLogs();
+	expect(logs).toMatchInlineSnapshot('"toto"');
 });
 
 async function updateNotebookMemberLastVisit(date, notebookId, accountId) {
-	const response = await graphqlAdmin(
-		`mutation updateVisitDate($notebookId:uuid!, $accountId: uuid!, $date: timestamptz!){
+	const response = await graphqlAsPierreChevalier(
+		`mutation($notebookId:uuid!, $accountId: uuid!, $date: timestamptz!){
 			update_notebook_member(
 				where: { notebookId: {_eq: $notebookId}, accountId:{_eq: $accountId }}
 				_set: {lastVisitedAt: $date}
@@ -22,4 +31,8 @@ async function updateNotebookMemberLastVisit(date, notebookId, accountId) {
 		{ date, notebookId, accountId }
 	);
 	return response?.data.account[0]?.id;
+}
+
+async function getVisitLogs() {
+	return 'toto';
 }
