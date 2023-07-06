321fdf60bdce85e5ac57bdda80b2b11c115edd08
Hadrien Mens-Pellen <hadrien.mens-pellen@beta.gouv.fr>
2023-07-06 09:12:18 +0200

    feat(objectifs): ajout de deux nouveaux objectifs

---
+++ hasura/migrations/carnet_de_bord/1688627437320_add_new_targets/up.sql
@@ -0,0 +1,5 @@
+INSERT INTO "public".ref_target (id, description, theme)
+VALUES ('8047eb4c-8103-4e95-9ed6-f08abbd7bc5e', 'Prévenir une dégradation de la situation financière',
+        'difficulte_financiere'),
+       ('7931bc54-a031-4fce-80fe-e956040f821a', 'Constituer un dossier MDPH/invalidité',
+        'sante');
