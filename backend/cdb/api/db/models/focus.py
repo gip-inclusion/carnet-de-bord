d6c465cd1fcfc9c7a34242ab8bbcdc4401ec576a
LionelB <lionel@lumographe.fr>
2023-07-11 17:46:14 +0200

    fixup! chore: rend le creator nullable

--- backend/cdb/api/db/models/focus.py
+++ backend/cdb/api/db/models/focus.py
@@ -9,7 +9,7 @@ from cdb.api.db.models.target import Target
 class Focus(BaseModel):
     id: UUID
     theme: str
-    creator_id: UUID
+    creator_id: UUID | None
     notebook_id: UUID
     created_at: datetime
     updated_at: datetime
