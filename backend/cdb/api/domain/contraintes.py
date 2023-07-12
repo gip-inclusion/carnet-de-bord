462fd7cb55d917abeb5c7eb501333fd9c74aaba3
LionelB <lionel@lumographe.fr>
2023-07-12 15:44:30 +0200

    feat: regarde les différences entre les contraintes FT et Cdb

--- backend/cdb/api/domain/contraintes.py
+++ backend/cdb/api/domain/contraintes.py
@@ -1,21 +1,42 @@
+from datetime import datetime
 from typing import List
 from uuid import UUID
 
 from attr import dataclass
 
 from cdb.api.db.models.focus import Focus
-from cdb.pe.models.dossier_individu_api import Contrainte
+from cdb.pe.models.dossier_individu_api import Contrainte, Objectif
+
+
+@dataclass
+class TargetPayload:
+    target: str
 
 
 @dataclass
 class FocusToAdd:
     theme: str
+    created_at: datetime | None = None
+    targets: List[TargetPayload] = []
+
+
+@dataclass
+class TargetToAdd:
+    target: str
+    focus_id: UUID
+
+
+@dataclass
+class TargetDifferences:
+    targets_to_add: List[TargetToAdd]
+    target_ids_to_delete: List[UUID]
 
 
 @dataclass
 class FocusDifferences:
     focus_to_add: List[FocusToAdd]
-    focus_to_delete: List[UUID]
+    focus_ids_to_delete: List[UUID]
+    target_differences: TargetDifferences
 
 
 def getThemeFromContrainteId(id: str) -> str | None:
@@ -52,6 +73,41 @@ def getContrainteIdFromTheme(theme: str) -> str | None:
         return "29"
 
 
+def diff_objectifs(
+    focuses: List[Focus], contraintes: List[Contrainte]
+) -> TargetDifferences:
+    shared_contraintes_focuses = [
+        (focus, contrainte)
+        for focus in focuses
+        for contrainte in contraintes
+        if contrainte.valeur == "OUI"
+        and getContrainteIdFromTheme(focus.theme) == contrainte.code
+    ]
+    targets_to_add = []
+    target_ids_to_delete = []
+    for shared_focus, shared_contrainte in shared_contraintes_focuses:
+        targets = shared_focus.targets or []
+        objectifs = shared_contrainte.objectifs
+
+        for objectif in objectifs:
+            if objectif.valeur == "OUI" and objectif not in [
+                target.target for target in targets
+            ]:
+                targets_to_add.append(
+                    TargetToAdd(target=objectif.libelle, focus_id=shared_focus.id)
+                )
+
+        for target in targets or []:
+            if target.target not in [
+                objectif.libelle for objectif in objectifs if objectif.valeur == "OUI"
+            ]:
+                target_ids_to_delete.append(target.id)
+
+    return TargetDifferences(
+        targets_to_add=targets_to_add, target_ids_to_delete=target_ids_to_delete
+    )
+
+
 def contraintes_to_focus(contrainte: Contrainte) -> FocusToAdd:
     theme = getThemeFromContrainteId(contrainte.code)
     if not theme:
@@ -59,19 +115,20 @@ def contraintes_to_focus(contrainte: Contrainte) -> FocusToAdd:
     return FocusToAdd(theme=theme)
 
 
+# TODO : Il faut ajouter une entrée dans notebook_event quand on supprime un objectif
 def diff_contraintes(
     focuses: List[Focus], contraintes: List[Contrainte]
 ) -> FocusDifferences:
     return FocusDifferences(
         focus_to_add=[
-            FocusToAdd(getThemeFromContrainteId(contrainte.code))
+            contrainte_to_focus(contrainte)
             for contrainte in contraintes
             if contrainte.code
             not in [getContrainteIdFromTheme(focus.theme) for focus in focuses]
             and contrainte.valeur == "OUI"
             and getThemeFromContrainteId(contrainte.code)
         ],
-        focus_to_delete=[
+        focus_ids_to_delete=[
             focus.id
             for focus in focuses
             if getContrainteIdFromTheme(focus.theme)
@@ -81,4 +138,28 @@ def diff_contraintes(
                 if contrainte.valeur == "OUI"
             ]
         ],
+        target_differences=diff_objectifs(contraintes=contraintes, focuses=focuses),
     )
+
+
+def contrainte_to_focus(contrainte: Contrainte) -> FocusToAdd:
+    theme = getThemeFromContrainteId(contrainte.code)
+    if not theme:
+        raise Exception(
+            "error: unknonw contrainte.code %s. " "can't found a matching theme",
+            contrainte.code,
+        )
+
+    return FocusToAdd(
+        theme=theme,
+        created_at=contrainte.date,
+        targets=[
+            objectif_to_target(objectif)
+            for objectif in contrainte.objectifs
+            if objectif.valeur == "OUI"
+        ],
+    )
+
+
+def objectif_to_target(objectif: Objectif) -> TargetPayload:
+    return TargetPayload(target=objectif.libelle)
--- backend/tests/api/domain/test_contraintes.py
+++ backend/tests/api/domain/test_contraintes.py
@@ -5,47 +5,157 @@ from uuid import uuid4
 import pytest
 
 from cdb.api.db.models.focus import Focus
+from cdb.api.db.models.target import Target
 from cdb.api.domain.contraintes import (
     FocusToAdd,
+    TargetPayload,
+    TargetToAdd,
     diff_contraintes,
 )
-from cdb.pe.models.dossier_individu_api import Contrainte
+from cdb.pe.models.dossier_individu_api import Contrainte, Objectif
 
 
 def test_diff_empty_contraintes_and_empty_focus():
     result = diff_contraintes([], [])
 
     assert result.focus_to_add == []
-    assert result.focus_to_delete == []
+    assert result.focus_ids_to_delete == []
+    assert result.target_differences.targets_to_add == []
+    assert result.target_differences.target_ids_to_delete == []
 
 
 def test_diff_empty_contraintes_and_existing_focus(notebook_focuses: List[Focus]):
     result = diff_contraintes(notebook_focuses, [])
 
     assert result.focus_to_add == []
-    assert result.focus_to_delete == [focus.id for focus in notebook_focuses]
+    assert result.focus_ids_to_delete == [focus.id for focus in notebook_focuses]
+    assert result.target_differences.targets_to_add == []
+    assert result.target_differences.target_ids_to_delete == []
 
 
 def test_diff_existing_contraintes_and_empty_focus(contraintes: List[Contrainte]):
     result = diff_contraintes([], contraintes)
 
     assert result.focus_to_add == [
-        FocusToAdd(theme="mobilite"),
+        FocusToAdd(
+            theme="mobilite",
+            targets=[TargetPayload(target="Dépendant des transports en commun")],
+        ),
         FocusToAdd(theme="difficulte_administrative"),
     ]
-    assert result.focus_to_delete == []
+    assert result.focus_ids_to_delete == []
+    assert result.target_differences.targets_to_add == []
+    assert result.target_differences.target_ids_to_delete == []
 
 
 def test_diff_existing_contraintes_and_existing_focus(
     notebook_focuses: List[Focus], contraintes: List[Contrainte]
 ):
-    result = diff_contraintes(notebook_focuses, contraintes)
+    result = diff_contraintes(focuses=notebook_focuses, contraintes=contraintes)
 
     assert result.focus_to_add == [
         FocusToAdd(theme="difficulte_administrative"),
     ]
-    assert result.focus_to_delete == [
+    assert result.focus_ids_to_delete == [
         focus.id for focus in notebook_focuses if focus.theme == "logement"
+    ]
+    focus_ids = [focus.id for focus in notebook_focuses if focus.theme == "mobilite"]
+    assert result.target_differences.targets_to_add == [
+        TargetToAdd(
+            focus_id=focus_ids[0],
+            target="Dépendant des transports en commun",
+        )
+    ]
+    assert result.target_differences.target_ids_to_delete == []
+
+
+def test_shared_contrainte_with_no_objectif_and_no_target(
+    shared_focus_with_no_target: Focus, shared_contrainte_with_no_objectif: Contrainte
+):
+    result = diff_contraintes(
+        [shared_focus_with_no_target], [shared_contrainte_with_no_objectif]
+    )
+
+    assert result.focus_to_add == []
+    assert result.focus_ids_to_delete == []
+    assert result.target_differences.targets_to_add == []
+    assert result.target_differences.target_ids_to_delete == []
+
+
+def test_shared_contrainte_with_objectif_and_no_target(
+    shared_contrainte_with_objectifs: Contrainte,
+    shared_focus_with_no_target: Focus,
+):
+    result = diff_contraintes(
+        focuses=[shared_focus_with_no_target],
+        contraintes=[shared_contrainte_with_objectifs],
+    )
+
+    assert result.focus_to_add == []
+    assert result.focus_ids_to_delete == []
+    assert result.target_differences.targets_to_add == [
+        TargetToAdd(
+            focus_id=shared_focus_with_no_target.id,
+            target="Dépendant des transports en commun",
+        )
+    ]
+    assert result.target_differences.target_ids_to_delete == []
+
+
+def test_shared_contrainte_with_no_objectif_and_target(
+    shared_contrainte_with_no_objectif: Contrainte,
+    shared_focus_with_targets: Focus,
+):
+    result = diff_contraintes(
+        focuses=[shared_focus_with_targets],
+        contraintes=[shared_contrainte_with_no_objectif],
+    )
+
+    assert result.focus_to_add == []
+    assert result.focus_ids_to_delete == []
+    assert result.target_differences.targets_to_add == []
+    assert result.target_differences.target_ids_to_delete == [
+        target.id for target in (shared_focus_with_targets.targets or [])
+    ]
+
+
+def test_shared_contraintes_with_objectifs_and_targets(
+    shared_contrainte_with_objectifs: Contrainte,
+    shared_focus_with_targets: Focus,
+):
+    result = diff_contraintes(
+        focuses=[shared_focus_with_targets],
+        contraintes=[shared_contrainte_with_objectifs],
+    )
+
+    [target_id_to_delete] = [
+        target.id
+        for target in (shared_focus_with_targets.targets or [])
+        if target.target == "Permis non valide / suspension de permis"
+    ]
+    assert result.focus_to_add == []
+    assert result.focus_ids_to_delete == []
+    assert result.target_differences.targets_to_add == [
+        TargetToAdd(
+            focus_id=shared_focus_with_targets.id,
+            target="Dépendant des transports en commun",
+        )
+    ]
+    assert result.target_differences.target_ids_to_delete == [target_id_to_delete]
+
+
+@pytest.fixture(scope="session")
+def notebook_target() -> List[Target]:
+    return [
+        Target(
+            id=uuid4(),
+            focus_id=uuid4(),
+            target="Target 1",
+            creator_id=uuid4(),
+            created_at=datetime.now(),
+            updated_at=datetime.now(),
+            status="",
+        )
     ]
 
 
@@ -84,7 +194,16 @@ def contraintes() -> List[Contrainte]:
             valeur="OUI",
             libelle="Développer sa mobilité",
             situations=[],
-            objectifs=[],
+            objectifs=[
+                Objectif(
+                    code="6",
+                    libelle="Aucun moyen de transport à disposition",
+                    valeur="NON_ABORDEE",
+                ),
+                Objectif(
+                    code="7", libelle="Dépendant des transports en commun", valeur="OUI"
+                ),
+            ],
         ),
         Contrainte(
             code="27",
@@ -103,3 +222,84 @@ def contraintes() -> List[Contrainte]:
             objectifs=[],
         ),
     ]
+
+
+@pytest.fixture(scope="session")
+def shared_focus_with_no_target() -> Focus:
+    return Focus(
+        id=uuid4(),
+        creator_id=None,
+        linked_to=None,
+        targets=None,
+        theme="mobilite",
+        notebook_id=uuid4(),
+        created_at=datetime.now(),
+        updated_at=datetime.now(),
+    )
+
+
+@pytest.fixture(scope="session")
+def shared_contrainte_with_no_objectif() -> Contrainte:
+    return Contrainte(
+        code="23",
+        date=None,
+        valeur="OUI",
+        libelle="Développer sa mobilité",
+        situations=[],
+        objectifs=[],
+    )
+
+
+@pytest.fixture(scope="session")
+def shared_focus_with_targets() -> Focus:
+    focus_id = uuid4()
+    return Focus(
+        id=focus_id,
+        creator_id=None,
+        linked_to=None,
+        theme="mobilite",
+        notebook_id=uuid4(),
+        created_at=datetime.now(),
+        updated_at=datetime.now(),
+        targets=[
+            Target(
+                id=uuid4(),
+                focus_id=focus_id,
+                target="Dépendant des transports en commun",
+                creator_id=None,
+                created_at=datetime.now(),
+                updated_at=datetime.now(),
+                status="in_progress",
+            ),
+            Target(
+                id=uuid4(),
+                focus_id=focus_id,
+                target="Permis non valide / suspension de permis",
+                creator_id=None,
+                created_at=datetime.now(),
+                updated_at=datetime.now(),
+                status="in_progress",
+            ),
+        ],
+    )
+
+
+@pytest.fixture(scope="session")
+def shared_contrainte_with_objectifs() -> Contrainte:
+    return Contrainte(
+        code="23",
+        date=None,
+        valeur="OUI",
+        libelle="Développer sa mobilité",
+        situations=[],
+        objectifs=[
+            Objectif(
+                code="6",
+                libelle="Aucun moyen de transport à disposition",
+                valeur="NON_ABORDEE",
+            ),
+            Objectif(
+                code="7", libelle="Dépendant des transports en commun", valeur="OUI"
+            ),
+        ],
+    )
