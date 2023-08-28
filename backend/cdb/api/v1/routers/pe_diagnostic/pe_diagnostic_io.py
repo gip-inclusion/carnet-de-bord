f40d160d32255b443bbd700d18e148404fa421a6
Vincent Jousse <vincent@jousse.org>
2023-08-28 12:26:56 +0200

    feat: Ajout d'un dry-run pour la synchronisation Pôle emploi (#1958)
    Co-authored-by: Lionel <lionel.breduillieard@beta.gouv.fr>

--- backend/cdb/api/v1/routers/pe_diagnostic/pe_diagnostic_io.py
+++ backend/cdb/api/v1/routers/pe_diagnostic/pe_diagnostic_io.py
@@ -10,20 +10,13 @@ from cdb.api.core.settings import settings
 from cdb.api.db.crud.beneficiary import update_diagnostic_fetch_date_gql
 from cdb.api.db.crud.external_data import save_external_data_with_info
 from cdb.api.db.crud.notebook_focus import add_remove_notebook_focuses
-from cdb.api.db.crud.notebook_situation import (
-    save_notebook_situations,
-)
+from cdb.api.db.crud.notebook_situation import save_notebook_situations
 from cdb.api.db.models.external_data import ExternalSource
 from cdb.api.db.models.ref_situation import NotebookSituation
 from cdb.api.domain.contraintes import FocusDifferences
-from cdb.api.domain.situations import (
-    SituationDifferences,
-)
+from cdb.api.domain.situations import SituationDifferences
 from cdb.api.v1.payloads.socio_pro import SituationInsertInput
-from cdb.api.v1.routers.pe_diagnostic.pe_diagnostic_models import (
-    Focus,
-    Notebook,
-)
+from cdb.api.v1.routers.pe_diagnostic.pe_diagnostic_models import Focus, Notebook
 from cdb.cdb_csv.json_encoder import CustomEncoder
 from cdb.pe.models.dossier_individu_api import DossierIndividuData
 from cdb.pe.pole_emploi_client import PoleEmploiApiClient
@@ -89,7 +82,7 @@ async def find_notebook(session, notebook_id) -> Notebook | None:
     )
 
 
-async def get_dossier_pe(nir: str, birth_date: str):
+async def get_dossier_pe(nir: str, birth_date: str) -> DossierIndividuData | None:
     pe_client = PoleEmploiApiClient(
         auth_base_url=settings.PE_AUTH_BASE_URL,
         base_url=settings.PE_BASE_URL,
