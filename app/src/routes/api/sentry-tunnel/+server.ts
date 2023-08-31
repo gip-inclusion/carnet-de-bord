80e197017772f294a1b032237d6aceebb5288aba
Hadrien Mens-Pellen <hadrien.mens-pellen@beta.gouv.fr>
2023-08-31 11:40:17 +0200

    fix: pr

--- app/src/routes/api/sentry-tunnel/+server.ts
+++ app/src/routes/api/sentry-tunnel/+server.ts
@@ -1,8 +1,30 @@
 import { env } from '$env/dynamic/public';
-import { forwardToSentry } from './server.testable';
+import { error } from '@sveltejs/kit';
+
+export type Params = {
+	dsn: URL;
+	body: string;
+	fetch: typeof fetch;
+};
+export async function _forwardToSentry({ dsn, body, fetch }: Params) {
+	const parts = body.split('\n');
+	const first = JSON.parse(parts[0]);
+	const requestDsn = new URL(first.dsn);
+	const projectId = dsn.pathname.replaceAll('/', '');
+
+	if (dsn.toString() !== requestDsn.toString()) {
+		throw error(400, `${requestDsn} is not the sentry dsn we use, rejecting the request`);
+	}
+
+	return await fetch(`https://${dsn.hostname}/api/${projectId}/envelope/`, {
+		method: 'POST',
+		body,
+		headers: { 'Content-type': 'application/x-sentry-envelope' },
+	});
+}
 
 export async function POST({ request, fetch }) {
-	return await forwardToSentry({
+	return await _forwardToSentry({
 		dsn: new URL(env.PUBLIC_SENTRY_DSN),
 		body: await request.text(),
 		fetch,
--- app/src/routes/api/sentry-tunnel/server.test.ts
+++ app/src/routes/api/sentry-tunnel/server.test.ts
@@ -1,4 +1,4 @@
-import { forwardToSentry } from './server.testable';
+import { _forwardToSentry } from './+server';
 
 describe('Sentry tunnel', () => {
 	it('fowards to sentry', async () => {
@@ -7,7 +7,7 @@ describe('Sentry tunnel', () => {
 {"event_id":"6e193188f6fd4f9cb647a7e852e7c05b","sent_at":"2023-08-28T14:48:45.788Z","sdk":{"name":"sentry.javascript.svelte","version":"7.46.0"},"dsn":"https://1793e2fe81b04f5aad6e75e0372c9a12@sentry.fabrique.social.gouv.fr/63"}
 {"type":"event"}
 `.trim();
-		await forwardToSentry({
+		await _forwardToSentry({
 			dsn: new URL('https://1793e2fe81b04f5aad6e75e0372c9a12@sentry.fabrique.social.gouv.fr/63'),
 			body,
 			fetch: fakeFetch,
@@ -27,7 +27,7 @@ describe('Sentry tunnel', () => {
 		const fakeFetch = vi.fn();
 		const body = JSON.stringify({ dsn: 'https://unknown.fr' });
 		try {
-			await forwardToSentry({
+			await _forwardToSentry({
 				dsn: new URL('http://lol.com'),
 				body,
 				fetch: fakeFetch,
